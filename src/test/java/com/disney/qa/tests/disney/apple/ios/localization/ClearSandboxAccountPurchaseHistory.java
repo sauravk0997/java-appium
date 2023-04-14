474ca5b3f0996f4bbad433428a1bf91ab4f65637
Jose Silveti <jose.silveti@disneystreaming.com>
2023-04-14 18:13:20 -0400

    QTE-1841: Added assertions to clearDSSSandboxAccounts and removed util class.

--- src/test/java/com/disney/qa/tests/disney/apple/ios/localization/ClearSandboxAccountPurchaseHistory.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/localization/ClearSandboxAccountPurchaseHistory.java
@@ -1,12 +1,15 @@
 package com.disney.qa.tests.disney.apple.ios.localization;
 
+import com.disney.qa.api.appstoreconnect.AppStoreConnectApi;
+import com.disney.qa.api.pojos.sandbox.SandboxAccount;
 import com.disney.qa.tests.BaseTest;
-import com.disney.util.AppStoreUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.lang.invoke.MethodHandles;
+import java.util.List;
 
 public class ClearSandboxAccountPurchaseHistory extends BaseTest {
 
@@ -15,6 +18,13 @@ public class ClearSandboxAccountPurchaseHistory extends BaseTest {
     @Test(description = "Clears app purchase history", groups = {"clearAccounts"})
     public void clearDSSSandboxAccounts() {
         LOGGER.info("Clearing purchase history for dsqaaiap perfix accounts");
-        AppStoreUtil.clearPurchaseHistoryForSandboxAccounts("dsqaaiap");
+        AppStoreConnectApi appStoreConnectApi = new AppStoreConnectApi();
+
+        List<SandboxAccount> accounts = appStoreConnectApi.getSandboxAccounts("dsqaaiap");
+        Assert.assertNotNull(accounts);
+
+        for(SandboxAccount account : accounts) {
+            Assert.assertTrue(appStoreConnectApi.clearAccountPurchaseHistory(account.getId()).getStatusCode().is2xxSuccessful());
+        }
     }
 }
