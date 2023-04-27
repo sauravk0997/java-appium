41fb9ece7360df987d6779a6bd6c7faacbc47064
Jose Silveti <jose.silveti@disneystreaming.com>
2023-04-27 10:08:49 -0400

    QTE-1708: Updated DisneyPlusAudioSubtitleIOSPage.

--- src/main/java/com/disney/qa/disney/apple/pages/common/DisneyPlusAudioSubtitleIOSPageBase.java
+++ src/main/java/com/disney/qa/disney/apple/pages/common/DisneyPlusAudioSubtitleIOSPageBase.java
@@ -1,9 +1,9 @@
 package com.disney.qa.disney.apple.pages.common;
 
 import com.disney.qa.common.utils.IOSUtils;
-import com.qaprosoft.carina.core.foundation.utils.mobile.IMobileUtils;
-import com.qaprosoft.carina.core.foundation.webdriver.decorator.ExtendedWebElement;
-import com.qaprosoft.carina.core.foundation.webdriver.locator.ExtendedFindBy;
+import com.zebrunner.carina.utils.mobile.IMobileUtils;
+import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
+import com.zebrunner.carina.webdriver.locator.ExtendedFindBy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.FindBy;
 
