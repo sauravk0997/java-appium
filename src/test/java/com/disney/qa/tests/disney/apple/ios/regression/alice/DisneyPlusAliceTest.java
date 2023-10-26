0f4d721529d9e641cfb5d5f5d12746f156ccdb80
csolmaz <cristina.solmaz@hulu.com>
2023-10-26 11:55:03 -0700

    update ids, scheduling

--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/alice/DisneyPlusAliceTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/alice/DisneyPlusAliceTest.java
@@ -4,14 +4,16 @@ import com.disney.alice.AliceDriver;
 import com.disney.qa.disney.apple.pages.common.DisneyPlusWelcomeScreenIOSPageBase;
 import com.disney.qa.tests.disney.apple.ios.DisneyBaseTest;
 import com.disney.util.TestGroup;
+import com.zebrunner.agent.core.annotation.Maintainer;
 import com.zebrunner.agent.core.annotation.TestLabel;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 public class DisneyPlusAliceTest extends DisneyBaseTest {
 
-    @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-62687"})
-    @Test(description = "App Opens to Welcome Screen", groups = {"Smoke", TestGroup.PRE_CONFIGURATION})
+    @Maintainer("csolmaz")
+    @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-74625"})
+    @Test(description = "App Launches - Alice validates Disney logo present on Welcome screen", groups = {"Alice", TestGroup.PRE_CONFIGURATION})
     public void testAppLaunch() {
         SoftAssert sa = new SoftAssert();
         AliceDriver aliceDriver = new AliceDriver(getDriver());
