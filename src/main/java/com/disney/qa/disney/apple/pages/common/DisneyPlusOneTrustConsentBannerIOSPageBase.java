0eb123e3c9224e478025330734e528f817fe371e
Garima <garima.krishna@disneystreaming.com>
2023-10-30 16:24:26 -0700

    one trust tests

--- src/main/java/com/disney/qa/disney/apple/pages/common/DisneyPlusOneTrustConsentBannerIOSPageBase.java
+++ src/main/java/com/disney/qa/disney/apple/pages/common/DisneyPlusOneTrustConsentBannerIOSPageBase.java
@@ -11,11 +11,59 @@ public class DisneyPlusOneTrustConsentBannerIOSPageBase extends DisneyPlusAppleP
         super(driver);
     }
 
+    @ExtendedFindBy(accessibilityId = "bannerDPDDescriptions")
+    protected ExtendedWebElement bannerDPDDescriptions;
+
+    @ExtendedFindBy(accessibilityId = "bannerListOfVendors")
+    protected ExtendedWebElement bannerListOfVendors;
+
+    @ExtendedFindBy(accessibilityId = "bannerAllowAllButton")
+    protected ExtendedWebElement bannerAllowAllButton;
+
     @ExtendedFindBy(accessibilityId = "bannerRejectAllButton")
     protected ExtendedWebElement rejectAllButton;
 
+    @ExtendedFindBy(accessibilityId = "bannerPrivacySettingsButton")
+    protected ExtendedWebElement bannerPrivacySettingsButton;
+
+    @ExtendedFindBy(accessibilityId = "pcDescription")
+    protected ExtendedWebElement preferenceCenterDescription;
+
+    @Override
+    public boolean isOpened() {
+        return bannerDPDDescriptions.isPresent();
+    }
+
+    public boolean isListOfVendorsLinkPresent() {
+        return bannerListOfVendors.isPresent();
+    }
+
+    public boolean isAllowAllButtonPresent() {
+        return bannerAllowAllButton.isPresent();
+    }
+
+    public boolean isRejectAllButtonPresent() {
+        return rejectAllButton.isPresent();
+    }
+
+    public boolean isCustomizedChoicesButtonPresent() {
+        return bannerPrivacySettingsButton.isPresent();
+    }
+
+    public boolean isPrivacyPreferenceCenterOpen() {
+        return preferenceCenterDescription.isPresent();
+    }
+
+    public void tapAcceptAllButton() {
+        bannerAllowAllButton.click();
+    }
+
     public void tapRejectAllButton() {
-        rejectAllButton.clickIfPresent();
+        rejectAllButton.click();
+    }
+
+    public void tapCustomizedChoices() {
+        bannerPrivacySettingsButton.click();
     }
 
 }
