a91cc892adef0b9f8cab9c6dcffdb84cf6dd9144
csolmaz <cristina.solmaz@hulu.com>
2023-11-06 12:47:41 -0800

    improve getalicescreenshot

--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/alice/DisneyPlusAliceTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/alice/DisneyPlusAliceTest.java
@@ -1,8 +1,8 @@
 package com.disney.qa.tests.disney.apple.ios.regression.alice;
 
-import ch.qos.logback.classic.Logger;
 import com.disney.alice.AliceDriver;
 import com.disney.alice.labels.AliceLabels;
+import com.disney.qa.common.utils.UniversalUtils;
 import com.disney.qa.disney.apple.pages.common.DisneyPlusDetailsIOSPageBase;
 import com.disney.qa.disney.apple.pages.common.DisneyPlusHomeIOSPageBase;
 import com.disney.qa.disney.apple.pages.common.DisneyPlusSearchIOSPageBase;
@@ -11,12 +11,21 @@ import com.disney.qa.tests.disney.apple.ios.DisneyBaseTest;
 import com.disney.util.TestGroup;
 import com.zebrunner.agent.core.annotation.Maintainer;
 import com.zebrunner.agent.core.annotation.TestLabel;
+import com.zebrunner.carina.utils.R;
+import com.zebrunner.carina.utils.factory.DeviceType;
+import org.openqa.selenium.ScreenOrientation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
-import java.awt.image.BufferedImage;
+import java.lang.invoke.MethodHandles;
 
 public class DisneyPlusAliceTest extends DisneyBaseTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    protected ThreadLocal<String> baseDirectory = new ThreadLocal<>();
+    protected ThreadLocal<String> pathToZip = new ThreadLocal<>();
+    private static final String DETAILS_PAGE = "_Details_Page";
 
     @Maintainer("csolmaz")
     @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-74625"})
@@ -32,39 +41,37 @@ public class DisneyPlusAliceTest extends DisneyBaseTest {
     }
 
     @Maintainer("csolmaz")
-    @Test(description = "Details Page - validate images, create zip of images, validate part on alice", groups = {"Alice", TestGroup.PRE_CONFIGURATION})
-    public void testDetailsPageImageCompare() {
-
+    @Test(description = "Details Page - take screenshot, create zip of image", groups = {"Alice", TestGroup.PRE_CONFIGURATION})
+    public void testSeriesDetailsScreenshot() {
         DisneyPlusHomeIOSPageBase homePage = initPage(DisneyPlusHomeIOSPageBase.class);
         DisneyPlusDetailsIOSPageBase detailsPage = initPage(DisneyPlusDetailsIOSPageBase.class);
         DisneyPlusSearchIOSPageBase searchPage = initPage(DisneyPlusSearchIOSPageBase.class);
-        //        AliceDriver aliceDriver = new AliceDriver(getDriver());
-        SoftAssert sa = new SoftAssert();
-        //use areImagesTheSame
+        baseDirectory.set("Screenshots/");
         setAppToHomeScreen(disneyAccount.get());
 
         homePage.isOpened();
         homePage.clickSearchIcon();
-        searchPage.searchForMedia("Loki");
-        searchPage.getDisplayedTitles().get(0).click();
+        searchPage.clickSeriesTab();
+        searchPage.selectRandomTitle();
         detailsPage.isOpened();
+        String detailsTitle = detailsPage.getMediaTitle();
+        getAliceScreenshots(detailsTitle + DETAILS_PAGE, baseDirectory);
+    }
 
-        BufferedImage lokiDetailsImage = getElementImage(detailsPage.contentDetailsPage);
-        BufferedImage lokiDetailsImageCopy = getScaledImage(lokiDetailsImage, lokiDetailsImage.getWidth(), lokiDetailsImage.getHeight());
-
-        detailsPage.getBackArrow().click();
-        searchPage.searchForMedia("Ahsoka");
-        searchPage.getDisplayedTitles().get(0).click();
-        detailsPage.isOpened();
-
-        BufferedImage moonKnightDetailsImage = getElementImage(detailsPage.contentDetailsPage);
-        BufferedImage moonKnightImageCopy = getScaledImage(moonKnightDetailsImage, moonKnightDetailsImage.getWidth(), moonKnightDetailsImage.getHeight());
-//        LOGGER.info("Scrolling down to view all of 'Information and choices about your profile'");
-        System.out.println(areImagesTheSame(lokiDetailsImage, moonKnightDetailsImage, 31));
-        System.out.println(areImagesTheSame(lokiDetailsImageCopy, moonKnightImageCopy, 31));
-
-        sa.assertTrue(areImagesTheSame(lokiDetailsImage, moonKnightDetailsImage, 31));
-        sa.assertTrue(areImagesTheSame(lokiDetailsImageCopy, moonKnightImageCopy, 31));
-        sa.assertAll();
+    private void getAliceScreenshots(String fileName, ThreadLocal<String> directory) {
+        rotateScreen(ScreenOrientation.PORTRAIT);
+        if (getDevice().getDeviceType() == DeviceType.Type.IOS_TABLET) {
+            UniversalUtils.storeScreenshot(getDriver(), fileName + "_iPad_Portrait_" , directory.get());
+            rotateScreen(ScreenOrientation.LANDSCAPE);
+            dismissKeyboardByClicking(5, 3);
+            pause(2);
+            UniversalUtils.storeScreenshot(getDriver(), fileName + "_iPad_Landscape_" + getDate(), directory.get());
+            rotateScreen(ScreenOrientation.PORTRAIT);
+            pause(2);
+        } else {
+            UniversalUtils.storeScreenshot(getDriver(), fileName + "_iPhone_" + getDate(), directory.get());
+        }
+        pathToZip.set(String.format("Detail_Page_Images_%s_%s_%s.zip", R.CONFIG.get("locale"), R.CONFIG.get("language"), getDate()));
+        UniversalUtils.archiveAndUploadsScreenshots(baseDirectory.get(), pathToZip.get());
     }
 }
