1717fdd980d4aa8913a0d3ef094d080fe11d8fb0
Garima <garima.krishna@disneystreaming.com>
2023-11-08 12:50:22 -0800

    suppress max depth inherit warn

--- src/main/java/com/disney/qa/disney/apple/pages/common/DisneyPlusHuluIOSPageBase.java
+++ src/main/java/com/disney/qa/disney/apple/pages/common/DisneyPlusHuluIOSPageBase.java
@@ -4,6 +4,7 @@ import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
 import com.zebrunner.carina.webdriver.locator.ExtendedFindBy;
 import org.openqa.selenium.WebDriver;
 
+@SuppressWarnings("squid:MaximumInheritanceDepth")
 public class DisneyPlusHuluIOSPageBase extends DisneyPlusApplePageBase {
 
     public DisneyPlusHuluIOSPageBase(WebDriver driver) {
