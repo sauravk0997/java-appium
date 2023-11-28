a03a7332ebf07b5319b739bc7ee8fc8512c38e21
hiren <hiren.patel@globant.com>
2023-11-28 16:30:10 +0530

    modify the steps for alice validation

--- src/test/java/com/disney/qa/tests/disney/apple/tvos/regression/onboarding/DisneyPlusAppleTVLoginTests.java
+++ src/test/java/com/disney/qa/tests/disney/apple/tvos/regression/onboarding/DisneyPlusAppleTVLoginTests.java
@@ -610,12 +610,15 @@ public class DisneyPlusAppleTVLoginTests extends DisneyPlusAppleTVBaseTest {
 
         sa.assertEquals(disneyPlusAppleTVWhoIsWatchingPage.getCollectionHeadlineTitleText(), whoIsWatchingTitle);
 
-        aliceDriver.screenshotAndRecognize().assertLabelContainsCaption(sa, testProfile, ROUND_TILE.getText())
-                .assertLabelContainsCaption(sa, entitledUser.getProfiles().get(0).getProfileName(), ROUND_TILE_HOVERED.getText())
+        aliceDriver.screenshotAndRecognize().assertLabelContainsCaption(sa, entitledUser.getProfiles().get(0).getProfileName(), ROUND_TILE.getText())
+                .assertLabelContainsCaption(sa, testProfile, ROUND_TILE.getText())
                 .assertLabelContainsCaption(sa, addProfileBtn, ROUND_TILE.getText());
 
         sa.assertTrue(disneyPlusAppleTVWhoIsWatchingPage.isDynamicAccessibilityIDElementPresent(editProfileBtn),
                 "The following button text was not found " + editProfileBtn);
+
+        disneyPlusAppleTVWhoIsWatchingPage.clickRight();
+        aliceDriver.screenshotAndRecognize().assertLabelContainsCaption(sa, entitledUser.getProfiles().get(0).getProfileName(), ROUND_TILE_HOVERED.getText());
 
         disneyPlusAppleTVWhoIsWatchingPage.clickRight();
         aliceDriver.screenshotAndRecognize().assertLabelContainsCaption(sa, testProfile, ROUND_TILE_HOVERED.getText());
