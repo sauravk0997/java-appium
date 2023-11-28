d2917d6f639d659bf033a735d109f293f75a53d6
hiren <hiren.patel@globant.com>
2023-11-28 14:23:46 +0530

    Added step for home validation and removed loading validation

--- src/test/java/com/disney/qa/tests/disney/apple/tvos/regression/onboarding/DisneyPlusAppleTVLoginTests.java
+++ src/test/java/com/disney/qa/tests/disney/apple/tvos/regression/onboarding/DisneyPlusAppleTVLoginTests.java
@@ -473,15 +473,17 @@ public class DisneyPlusAppleTVLoginTests extends DisneyPlusAppleTVBaseTest {
         DisneyOffer offer = new DisneyOffer();
         DisneyAccount entitledUser = disneyAccountApi.createAccount(offer, country, language, SUB_VERSION);
         DisneyPlusApplePageBase applePageBase = new DisneyPlusApplePageBase(getDriver());
+        DisneyPlusAppleTVHomePage homePage = new DisneyPlusAppleTVHomePage(getDriver());
 
         selectAppleUpdateLaterAndDismissAppTracking();
         logInWithoutHomeCheck(entitledUser);
 
         //TODO: Temp fix in place. After QCE-1333 is fixed, replace below lines with original Alice validation (QAA-11371).
         sa.assertTrue(applePageBase.doesAttributeEqualTrue(applePageBase.getStaticTextByLabelContains("Validating"), "enabled"));
-        aliceDriver.screenshotAndRecognize().assertLabelContainsCaption(sa, "Validating", AliceLabels.TITLE.getText())
-                .isLabelPresent(sa, AliceLabels.LOADING_ANIMATION.getText());
         aliceDriver.screenshotAndRecognize().isLabelPresent(sa, AliceLabels.LOADING_ANIMATION.getText());
+
+        sa.assertTrue(homePage.isOpened(),
+                "Home page did not launch for single profile user after logging in");
 
         sa.assertAll();
     }
