80a7fc41d3b6a1df1531409bd479efc423df43fc
Jose Silveti <jose.silveti@disneystreaming.com>
2023-12-14 09:49:16 -0500

    Added V2 account creation to verifySubscriptionDetails_DisneyPlus

--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/moremenu/DisneyPlusMoreMenuAccountSettingsTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/moremenu/DisneyPlusMoreMenuAccountSettingsTest.java
@@ -73,19 +73,13 @@ public class DisneyPlusMoreMenuAccountSettingsTest extends DisneyBaseTest {
         initPage(DisneyPlusMoreMenuIOSPageBase.class).clickMenuOption(DisneyPlusMoreMenuIOSPageBase.MoreMenu.ACCOUNT);
     }
 
-    public void setAppToAccountSettings(DisneyAccount account) {
-        setAppToHomeScreen(account, account.getProfiles().get(0).getProfileName());
-        navigateToTab(DisneyPlusApplePageBase.FooterTabs.MORE_MENU);
-        initPage(DisneyPlusMoreMenuIOSPageBase.class).clickMenuOption(DisneyPlusMoreMenuIOSPageBase.MoreMenu.ACCOUNT);
-    }
-
     @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-61547"})
     @Test(description = "Verify the Account submenu display elements are present", groups = {"More Menu", TestGroup.PRE_CONFIGURATION})
     public void verifyAccountDisplay() {
-        DisneyOffer offer = getAccountApi().lookupOfferToUse(getCountry(), BUNDLE_PREMIUM);
-        DisneyAccount account = getAccountApi().createAccount(offer, getLocalizationUtils().getLocale(), getLocalizationUtils().getUserLanguage(), SUBSCRIPTION_V2);
+        DisneyAccount accountV2 = createV2Account();
+        setAccount(accountV2);
 
-        setAppToAccountSettings(account);
+        setAppToAccountSettings();
         SoftAssert sa = new SoftAssert();
         DisneyPlusAccountIOSPageBase disneyPlusAccountIOSPageBase = initPage(DisneyPlusAccountIOSPageBase.class);
         pause(3);
@@ -126,6 +120,9 @@ public class DisneyPlusMoreMenuAccountSettingsTest extends DisneyBaseTest {
     @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-61571", "XMOBQA-61569"})
     @Test(description = "Verify that the correct description for D+ Premium displayed", groups = {"More Menu", TestGroup.PRE_CONFIGURATION})
     public void verifySubscriptionDetails_DisneyPlus() {
+        DisneyAccount accountV2 = createV2Account();
+        setAccount(accountV2);
+
         setAppToAccountSettings();
         DisneyPlusAccountIOSPageBase disneyPlusAccountIOSPageBase = new DisneyPlusAccountIOSPageBase(getDriver());
 
