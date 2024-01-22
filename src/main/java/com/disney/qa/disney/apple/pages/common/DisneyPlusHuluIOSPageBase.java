e9150a25677960e2e12cf8dcea951c70bd624a66
Mauricio Alejandro Parra Ramirez <>
2024-01-22 16:09:57 -0500

    [ADD] - Test verifyHuluBrandPage added

--- src/main/java/com/disney/qa/disney/apple/pages/common/DisneyPlusHuluIOSPageBase.java
+++ src/main/java/com/disney/qa/disney/apple/pages/common/DisneyPlusHuluIOSPageBase.java
@@ -3,6 +3,8 @@ package com.disney.qa.disney.apple.pages.common;
 import com.disney.qa.common.constant.CollectionConstant;
 import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
 import com.zebrunner.carina.webdriver.locator.ExtendedFindBy;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 
 @SuppressWarnings("squid:MaximumInheritanceDepth")
@@ -11,14 +13,77 @@ public class DisneyPlusHuluIOSPageBase extends DisneyPlusApplePageBase {
     @ExtendedFindBy(accessibilityId = "brandLandingView")
     protected ExtendedWebElement brandLandingView;
 
+    @ExtendedFindBy(iosClassChain = "**/XCUIElementTypeOther[`name == \"brandLandingView\"`]/XCUIElementTypeOther[2]/XCUIElementTypeOther/XCUIElementTypeImage")
+    protected ExtendedWebElement huluBrandImageExpanded;
+
+    @ExtendedFindBy(iosClassChain = "**/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther[1]/XCUIElementTypeOther/XCUIElementTypeOther[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther[2]/XCUIElementTypeOther/XCUIElementTypeImage")
+    protected ExtendedWebElement huluBrandImageCollapsed;
+
+    @ExtendedFindBy(iosClassChain = "**/XCUIElementTypeButton[`label == \"iconNavBack24Dark\"`]")
+    protected ExtendedWebElement backButton;
+
+    @ExtendedFindBy(iosClassChain = "**/XCUIElementTypeOther[`name == \"brandLandingView\"`]/XCUIElementTypeImage[1]")
+    protected ExtendedWebElement artworkBackground;
+
+    @ExtendedFindBy(iosClassChain = "**/XCUIElementTypeCollectionView[`name == \"917351f3-45cf-4251-b425-c8fd1b18434d\"`]")
+    private ExtendedWebElement studiosAndNetworkCollection;
+
     public DisneyPlusHuluIOSPageBase(WebDriver driver) {
         super(driver);
     }
 
     @Override
     public boolean isOpened() {
-        ExtendedWebElement studiosLabel = staticTextByLabel.format("Studios and Networks");
+        return isStudiosAndNetworkPresent();
+    }
+
+    public ExtendedWebElement getStudiosAndNetwork() {
+        return staticTextByLabel.format("Studios and Networks");
+    }
+
+    public boolean isStudiosAndNetworkPresent() {
+        ExtendedWebElement studiosLabel = getStudiosAndNetwork();
         swipePageTillElementPresent(studiosLabel, 3, brandLandingView, Direction.UP, 500);
         return studiosLabel.isPresent();
+    }
+
+    public boolean isHuluBrandImageExpanded() {
+        return huluBrandImageExpanded.isPresent() && !huluBrandImageCollapsed.isPresent(SHORT_TIMEOUT);
+    }
+
+    public boolean isHuluBrandImageCollapsed() {
+        return huluBrandImageCollapsed.isPresent() && !huluBrandImageExpanded.isPresent(SHORT_TIMEOUT);
+    }
+    public boolean isBackButtonPresent() {
+        return backButton.isPresent();
+    }
+
+    public boolean isArtworkBackgroundPresent() {
+        return artworkBackground.isPresent();
+    }
+
+    public void swipeInHuluBrandPage (Direction direction){
+        swipeInContainer(brandLandingView, direction, 500);
+    }
+
+    public void clickOnBackButton() {
+        backButton.click();
+    }
+
+    public boolean isNetworkLogoPresent(String logoName){
+        if (!typeCellLabelContains.format(logoName).isPresent(SHORT_TIMEOUT)){
+            // studiosAndNetworkCollection element has not visible attribute in false. This is a workaround
+            // swipeInContainer(studiosAndNetworkCollection, Direction.LEFT, 500);
+            Point elementLocation = studiosAndNetworkCollection.getLocation();
+            Dimension elementDimensions = studiosAndNetworkCollection.getSize();
+
+            int endY;
+            int startY = endY = elementLocation.getY() + Math.round((float)elementDimensions.getHeight() / 2.0F);
+            int startX = (int)((long)elementLocation.getX() + Math.round(0.8 * (double)elementDimensions.getWidth()));
+            int endX = (int)((long)elementLocation.getX() + Math.round(0.25 * (double)elementDimensions.getWidth()));
+
+            this.swipe(startX, startY, endX, endY, 500);
+        }
+        return typeCellLabelContains.format(logoName).isPresent(SHORT_TIMEOUT);
     }
 }
--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/Hulk/DisneyPlusHulkHomeTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/Hulk/DisneyPlusHulkHomeTest.java
@@ -10,7 +10,19 @@ import com.zebrunner.agent.core.annotation.TestLabel;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class DisneyPlusHulkHomeTest extends DisneyBaseTest {
+
+    private List<String> networkLogos = new ArrayList<String>(
+            Arrays.asList("A&E", "ABC", "ABC News", "Adult Swim", "Andscape", "Aniplex", "BBC Studios",
+                    "Cartoon Network", "CBS", "Discovery", "Disney XD", "FOX", "Freeform", "FX", "FYI", "HGTV",
+                    "Hulu Original Series", "Lifetime", "Lionsgate", "LMN", "Magnolia", "Moonbug Entertainment ",
+                    "MTV", "National Geographic", "Nickelodeon", "Saban Films", "Samuel Goldwyn Films",
+                    "Searchlight Pictures", "Paramount+", "Sony Pictures Television", "The HISTORY Channel",
+                    "TLC", "TV Land", "Twentieth Century Studios", "Vertical Entertainment", "Warner Bros"));
 
     @Maintainer("gkrishna1")
     @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-74645"})
@@ -26,5 +38,43 @@ public class DisneyPlusHulkHomeTest extends DisneyBaseTest {
         homePage.tapHuluBrandTile();
         sa.assertTrue(huluPage.isOpened(), "Hulu page didn't open after navigating via brand tile");
         sa.assertAll();
+    }
+
+    @Maintainer("mparra5")
+    @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-74814", "XMOBQA-74829"})
+    @Test(description = "Validate of the UI and functional items of the Hulu brand page", groups = {"Hulk", TestGroup.PRE_CONFIGURATION})
+    public void verifyHuluBrandPage() {
+        SoftAssert sa = new SoftAssert();
+        DisneyPlusHomeIOSPageBase homePage = initPage(DisneyPlusHomeIOSPageBase.class);
+        DisneyPlusHuluIOSPageBase huluPage = initPage(DisneyPlusHuluIOSPageBase.class);
+
+        setAccount(createAccountWithSku(DisneySkuParameters.DISNEY_VERIFIED_HULU_ESPN_BUNDLE, getLocalizationUtils().getLocale(), getLocalizationUtils().getUserLanguage()));
+        setAppToHomeScreen(getAccount(), getAccount().getProfiles().get(0).getProfileName());
+        sa.assertTrue(homePage.isHuluTileVisible(), "Hulu tile is not visible on home page");
+        homePage.tapHuluBrandTile();
+
+        sa.assertTrue(huluPage.isHuluBrandImageExpanded(), "Hulu brand logo is not expanded");
+        sa.assertTrue(huluPage.isBackButtonPresent(), "Back button is not present");
+        sa.assertTrue(huluPage.isArtworkBackgroundPresent(), "Artwork images is not present");
+
+        huluPage.swipeInHuluBrandPage(Direction.UP);
+        sa.assertTrue(huluPage.isHuluBrandImageCollapsed(), "Hulu brand logo is not collapsed");
+        sa.assertTrue(huluPage.isBackButtonPresent(), "Back button is not present");
+
+        huluPage.swipeInHuluBrandPage(Direction.DOWN);
+        sa.assertTrue(huluPage.isHuluBrandImageExpanded(), "Hulu brand logo is not expanded");
+
+        huluPage.clickOnBackButton();
+        sa.assertTrue(homePage.isHuluTileVisible(), "Hulu tile is not visible on home page");
+
+        homePage.tapHuluBrandTile();
+        sa.assertTrue(huluPage.isStudiosAndNetworkPresent(), "Network logos are not present");
+
+        networkLogos.forEach(item ->
+                sa.assertTrue(huluPage.isNetworkLogoPresent(item), String.format("%s Network logo is not present", item)));
+
+        sa.assertAll();
+
+
     }
 }
