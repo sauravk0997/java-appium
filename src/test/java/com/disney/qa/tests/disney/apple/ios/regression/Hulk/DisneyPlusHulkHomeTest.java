2ef75cd289b0e85e17203fed4613970ef005d406
Mauricio Alejandro Parra Ramirez <>
2024-02-06 16:41:03 -0500

    [MODIFY] - Move test case to a new class to be executed isolated

--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/Hulk/DisneyPlusHulkHomeTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/Hulk/DisneyPlusHulkHomeTest.java
@@ -1,9 +1,5 @@
 package com.disney.qa.tests.disney.apple.ios.regression.Hulk;
 
-import com.disney.hatter.api.alice.AliceApiManager;
-import com.disney.hatter.api.alice.model.ImagesRequestS3;
-import com.disney.hatter.api.alice.model.ImagesResponse360;
-import com.disney.hatter.core.utils.FileUtil;
 import com.disney.qa.api.utils.DisneySkuParameters;
 import com.disney.qa.disney.apple.pages.common.DisneyPlusHomeIOSPageBase;
 import com.disney.qa.disney.apple.pages.common.DisneyPlusHuluIOSPageBase;
@@ -12,34 +8,14 @@ import com.disney.util.TestGroup;
 import com.zebrunner.agent.core.annotation.Maintainer;
 import com.zebrunner.agent.core.annotation.TestLabel;
 import com.zebrunner.carina.utils.R;
-import org.json.JSONObject;
-import org.openqa.selenium.OutputType;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.util.*;
 
 public class DisneyPlusHulkHomeTest extends DisneyBaseTest {
-
-    private static final String S3_BASE_PATH = "bamtech-qa-alice/disney/recognition/alice/";
-    double imageSimilarityPercentageThreshold = 90.0;
-
-    private String buildS3BucketPath(String title) {
-        if ("Tablet".equalsIgnoreCase(R.CONFIG.get(DEVICE_TYPE))) {
-            return String.format(
-                    S3_BASE_PATH + "apple-tablet/default/hulu-network-logos/%s", title);
-        } else {
-            return String.format(
-                    S3_BASE_PATH + "apple-handset/default/hulu-network-logos/%s", title);
-        }
-    }
-
-    private AliceApiManager getAliceApiManager() {
-        return new AliceApiManager(MULTIVERSE_STAGING_ENDPOINT);
-    }
 
     private List<String> networkLogos = new ArrayList<String>(
             Arrays.asList("A&E", "ABC", "ABC News", "Adult Swim", "Andscape", "Aniplex", "BBC Studios",
@@ -167,47 +143,6 @@ public class DisneyPlusHulkHomeTest extends DisneyBaseTest {
 
         homePage.getUnavailableOkButton().click();
         sa.assertTrue(homePage.isOpened(), "Home page not present");
-
-        sa.assertAll();
-    }
-
-    @Maintainer("mparra5")
-    @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-74598"})
-    @Test(description = "Validate of the UI and functional items of the Collection and Network page", groups = {"Hulk", TestGroup.PRE_CONFIGURATION})
-    public void verifyHulkCollectionPagesNetworkPageUI() {
-        SoftAssert sa = new SoftAssert();
-        DisneyPlusHomeIOSPageBase homePage = initPage(DisneyPlusHomeIOSPageBase.class);
-        DisneyPlusHuluIOSPageBase huluPage = initPage(DisneyPlusHuluIOSPageBase.class);
-
-        setAccount(createAccountWithSku(DisneySkuParameters.DISNEY_VERIFIED_HULU_ESPN_BUNDLE, getLocalizationUtils().getLocale(), getLocalizationUtils().getUserLanguage()));
-        setAppToHomeScreen(getAccount());
-        homePage.tapHuluBrandTile();
-        sa.assertTrue(huluPage.isHuluBrandImageExpanded(), "Hulu brand logo is not expanded");
-        sa.assertTrue(huluPage.isBackButtonPresent(), "Back button is not present");
-        sa.assertTrue(huluPage.isArtworkBackgroundPresent(), "Artwork images is not present");
-        sa.assertTrue(huluPage.isStudiosAndNetworkPresent(), "Network and studios section are not present");
-
-        networkLogos.forEach(item -> {
-            sa.assertTrue(huluPage.isNetworkLogoPresent(item), String.format("%s Network logo is not present", item));
-            huluPage.clickOnNetworkLogo(item);
-            sa.assertTrue(homePage.isNetworkLogoImageVisible(), "Network logo page are not present");
-            pause(3);
-            String s3BucketPath = buildS3BucketPath(String.format("%s.png", item.replace(' ', '_')));
-            File srcFile = homePage.getNetworkLogoImage().getElement().getScreenshotAs(OutputType.FILE);;
-            ImagesRequestS3 imagesComparisonRequest = new ImagesRequestS3(srcFile.getName(), FileUtil.encodeBase64File(srcFile), s3BucketPath);
-            ImagesResponse360 imagesResponse360 = getAliceApiManager().compareImages360S3(imagesComparisonRequest);
-            JSONObject jsonResponse = new JSONObject(imagesResponse360.getData().toString());
-            LOGGER.info("Raw JSON response: " + jsonResponse);
-            double imageSimilarityPercentage = imagesResponse360.getSummary().getImageSimilarityPercentage();
-
-            LOGGER.info("Similarity Percentage is: " + imageSimilarityPercentage);
-
-            sa.assertTrue(
-                    imageSimilarityPercentage >= imageSimilarityPercentageThreshold,
-                    String.format("Similarity Percentage score was %,.2f or lower in %s Network logo {%,.2f}.", imageSimilarityPercentageThreshold, item, imageSimilarityPercentage));
-            huluPage.clickOnNetworkBackButton();
-            sa.assertTrue(huluPage.isStudiosAndNetworkPresent(), "Network and studios section are not present");
-        });
 
         sa.assertAll();
     }
