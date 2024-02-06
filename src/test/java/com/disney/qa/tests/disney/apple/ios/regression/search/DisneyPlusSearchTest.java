4fbcc422333517f72b7a67892460a4508c850d90
hiren <hiren.patel@globant.com>
2024-02-06 14:14:11 +0530

    Add testcase for clear recent search

--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/search/DisneyPlusSearchTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/search/DisneyPlusSearchTest.java
@@ -1,17 +1,24 @@
 package com.disney.qa.tests.disney.apple.ios.regression.search;
 
 import com.disney.alice.AliceDriver;
+import com.disney.qa.disney.apple.pages.common.DisneyPlusDetailsIOSPageBase;
 import com.disney.qa.disney.apple.pages.common.DisneyPlusHomeIOSPageBase;
 import com.disney.qa.disney.apple.pages.common.DisneyPlusSearchIOSPageBase;
 import com.disney.qa.tests.disney.apple.ios.DisneyBaseTest;
 import com.disney.util.TestGroup;
 import com.zebrunner.agent.core.annotation.Maintainer;
 import com.zebrunner.agent.core.annotation.TestLabel;
+import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
+import java.util.List;
+
 public class DisneyPlusSearchTest extends DisneyBaseTest {
+
+    private static final String BLUEY = "Bluey";
+
     @Maintainer("dconyers")
     @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-61083"})
     @Test(description = "Search: Navigate to search page and verify search icon", groups = {"Search", TestGroup.PRE_CONFIGURATION })
@@ -25,5 +32,35 @@ public class DisneyPlusSearchTest extends DisneyBaseTest {
         homePage.clickSearchIcon();
         aliceDriver.screenshotAndRecognize().isLabelPresent(sa, "search_button_selected");
         Assert.assertTrue(searchPage.isOpened(), "Search page did not open");
+    }
+
+    @Maintainer("hpatel7")
+    @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-62552"})
+    @Test(description = "Search - Recent Searches - Clear Recent Search by clicking on the X Icon", groups = {"Search", TestGroup.PRE_CONFIGURATION })
+    public void clearRecentSearchs() {
+        SoftAssert sa = new SoftAssert();
+        DisneyPlusHomeIOSPageBase homePage = initPage(DisneyPlusHomeIOSPageBase.class);
+        DisneyPlusSearchIOSPageBase searchPage = initPage(DisneyPlusSearchIOSPageBase.class);
+        DisneyPlusDetailsIOSPageBase detailsPage = initPage(DisneyPlusDetailsIOSPageBase.class);
+        setAppToHomeScreen(getAccount());
+
+        homePage.clickSearchIcon();
+        sa.assertTrue(searchPage.isOpened(), "Search page did not open");
+        searchPage.searchForMedia(BLUEY);
+        List<ExtendedWebElement> results = searchPage.getDisplayedTitles();
+        results.get(0).click();
+        sa.assertTrue(detailsPage.isOpened(), "Detail page did not open");
+        detailsPage.getBackArrow().click();
+        //Empty string to clear the keys
+        searchPage.searchForMedia("");
+        searchPage.getCancelButton().click();
+        homePage.clickSearchIcon();
+        searchPage.getSearchBar().click();
+        sa.assertTrue(searchPage.isRecentSearchDisplayed(), "recent search was displayed");
+
+        //click x button to clear recent searches
+        searchPage.tapRecentSearchClearButton();
+        searchPage.getSearchBar().click();
+        sa.assertFalse(searchPage.isRecentSearchDisplayed(), "recent search was displayed");
     }
 }
