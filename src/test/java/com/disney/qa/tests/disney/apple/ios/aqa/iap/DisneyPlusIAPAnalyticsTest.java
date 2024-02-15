cc9e49898d373fb95ee8cb1d41d62a6beef9ab36
insung.song <insung.song@disneystreaming.com>
2024-02-15 14:27:59 -0500

    change basic plan button

--- src/test/java/com/disney/qa/tests/disney/apple/ios/aqa/iap/DisneyPlusIAPAnalyticsTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/aqa/iap/DisneyPlusIAPAnalyticsTest.java
@@ -62,7 +62,8 @@ public class DisneyPlusIAPAnalyticsTest extends DisneyBaseTest {
 
 //        paywallIOSPageBase.clickBasicPlan();
 
-        paywallIOSPageBase.clickBasicPlanButton();
+        paywallIOSPageBase.clickBasicPlan();
+//                clickBasicPlanButton();
         paywallIOSPageBase.isOpened();
         paywallIOSPageBase.clickPurchaseButton();
 //        paywallIOSPageBase.isSusbscribed();
