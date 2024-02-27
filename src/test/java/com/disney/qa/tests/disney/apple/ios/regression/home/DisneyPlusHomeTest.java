f025ef66d861a80cdcf773f88a041f7cca54615c
csolmaz <cristina.solmaz@hulu.com>
2024-02-27 13:24:03 -0800

    revert

--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/home/DisneyPlusHomeTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/home/DisneyPlusHomeTest.java
@@ -30,7 +30,7 @@ import static com.disney.qa.tests.disney.apple.tvos.DisneyPlusAppleTVBaseTest.SU
 
 public class DisneyPlusHomeTest extends DisneyBaseTest {
     private static final String RECOMMENDED_FOR_YOU = "Recommended For You";
-    private static final String NEW_TO_DISNEY = "New To Disney+";
+    private static final String NEW_TO_DISNEY = "New To Disney";
     private static final String COLLECTIONS = "Collections";
     private static final String DISNEY_PLUS = "Disney Plus";
     private static final String PERSONALIZED_COLLECTION = "PersonalizedCollection";
@@ -52,98 +52,38 @@ public class DisneyPlusHomeTest extends DisneyBaseTest {
     public void verifyHomeUIElements() {
         SoftAssert sa = new SoftAssert();
         DisneyPlusHomeIOSPageBase homePage = initPage(DisneyPlusHomeIOSPageBase.class);
-        DisneyPlusSearchIOSPageBase searchPage = initPage(DisneyPlusSearchIOSPageBase.class);
-        DisneyPlusDetailsIOSPageBase detailsPage = initPage(DisneyPlusDetailsIOSPageBase.class);
-        DisneyPlusVideoPlayerIOSPageBase videoPlayer = initPage(DisneyPlusVideoPlayerIOSPageBase.class);
         DisneyOffer offer = new DisneyOffer();
         DisneyAccount entitledUser = getAccountApi().createAccount(offer, getCountry(), getLanguage(), SUB_VERSION);
         setAppToHomeScreen(entitledUser);
 
-        List<ContentSet> allSets = getSearchApi().getAllSetsInHomeCollection(entitledUser, getCountry(), getLanguage(), PERSONALIZED_COLLECTION);
-        CollectionRequest collectionRequest = CollectionRequest.builder().region(getCountry()).collectionType(PERSONALIZED_COLLECTION).
-                account(entitledUser).language(getLanguage()).slug(DisneyStandardCollection.HOME.getSlug()).
-                contentClass(DisneyStandardCollection.HOME.getSlug()).build();
-        ContentCollection collection = getSearchApi().getCollection(collectionRequest);
-        LOGGER.info("Collection json node to string: " + collection.getJsonNode().toString());
-//        List<String> collections = DisneySearchApi.parseValueFromJson(collection.getJsonNode().toString());
-        for (int i=1; i<allSets.size(); i++) {
-            var shelfTitle = allSets.get(i).getSetName();
-            var getSetAssets = allSets.get(i).getTitles();
-            LOGGER.info("What is shelf tile: " + shelfTitle);
-            LOGGER.info("What are set assets: " + getSetAssets);
-        }
-
-        homePage.clickSearchIcon();
-        searchPage.searchForMedia("Bluey");
-        searchPage.getDisplayedTitles().get(0).click();
-        detailsPage.isOpened();
-        detailsPage.clickPlayButton();
-        videoPlayer.waitForVideoToStart();
-        videoPlayer.scrubToPlaybackPercentage(90);
-        videoPlayer.clickBackButton();
-        detailsPage.clickHomeIcon();
-
+        //Validate top of home
         sa.assertTrue(homePage.getImageLabelContains(DISNEY_PLUS).isPresent(), "`Disney Plus` image was not found.");
-        sa.assertTrue(homePage.getTypeOtherContainsLabel(RECOMMENDED_FOR_YOU).isPresent(),
-                "'Recommend For You' collection was not found.");
-        sa.assertTrue(homePage.getTypeOtherContainsLabel(NEW_TO_DISNEY).isPresent(), "'New to Disney+' image was not found.");
+        sa.assertTrue(homePage.getTypeOtherContainsName(RECOMMENDED_FOR_YOU).isPresent(), "'Recommend For You' collection was not found.");
 
-
-        //Get top of home image
         BufferedImage topOfHome = getCurrentScreenView();
 
-        //Validate Collections is present after swiping to end of home.
-        swipePageTillElementPresent(homePage.getTypeOtherContainsLabel(COLLECTIONS), 10,null, Direction.UP, 300);
-        sa.assertTrue(homePage.getTypeOtherContainsLabel(COLLECTIONS).isPresent(), "'Collections' container was not found.");
+        //Validate bottom of home
+        swipePageTillElementPresent(homePage.getTypeOtherContainsName(COLLECTIONS), 10,null, Direction.UP, 300);
+        sa.assertTrue(homePage.getTypeOtherContainsName(COLLECTIONS).isPresent(), "'Collections' container was not found.");
         sa.assertFalse(homePage.getImageLabelContains(DISNEY_PLUS).isPresent(), "Disney Plus image was found after swiping up.");
 
-        //Get bottom of image with first `collections` tile
         BufferedImage bottomOfHomeWithFirstCollectionsTile = getCurrentScreenView();
-
-        //Validate swiping left through 'Collections'
         sa.assertTrue(homePage.isAllCollectionsPresent(), "`All Collections` was not found after swiping to end of Collection");
 
-        //Get bottom of home with last 'collections' tile
         BufferedImage bottomOfHomeWithLastCollectionsTile = getCurrentScreenView();
-
-        //Validate swiping right through 'Collections'
         sa.assertTrue(homePage.isBlackStoriesCollectionPresent(),
                 "`Black Stories Collection` was not found after swiping to beginning of Collection");
 
-        //Validate Recommended For You is present after swiping back to top of home.
-        swipePageTillElementPresent(homePage.getTypeOtherContainsLabel(RECOMMENDED_FOR_YOU), 10,null, Direction.DOWN, 300);
-        sa.assertTrue(homePage.getTypeOtherContainsLabel(RECOMMENDED_FOR_YOU).isPresent(),
-                "'Recommend For You' collection was not found.");
+        //Validate back at top of home
+        swipePageTillElementPresent(homePage.getTypeOtherContainsName(RECOMMENDED_FOR_YOU), 10,null, Direction.DOWN, 300);
+        sa.assertTrue(homePage.getTypeOtherContainsName(RECOMMENDED_FOR_YOU).isPresent(), "'Recommend For You' collection was not found.");
+        sa.assertTrue(homePage.getImageLabelContains(DISNEY_PLUS).isPresent(), "`Disney Plus` image was not found after return to top of home.");
 
         //Validate images are different
         sa.assertTrue(areImagesDifferent(bottomOfHomeWithFirstCollectionsTile, bottomOfHomeWithLastCollectionsTile),
                 "Bottom of image with first `collections` tile is the same bottom of home with last 'collections' tile");
         sa.assertTrue(areImagesDifferent(topOfHome, bottomOfHomeWithFirstCollectionsTile),
                 "Top of home image is the same as bottom of home image.");
-        sa.assertTrue(homePage.getImageLabelContains(DISNEY_PLUS).isPresent(), "`Disney Plus` image was not found after return to top of home.");
         sa.assertAll();
-    }
-
-    private void collectionBuild() {
-        CollectionRequest collectionRequest = CollectionRequest.builder()
-                .region(getLocalizationUtils().getLocale())
-                .audience("false")
-                .language(getLocalizationUtils().getUserLanguage())
-                .slug(DisneyStandardCollection.ORIGINALS.getSlug())
-                .contentClass(DisneyStandardCollection.ORIGINALS.getContentClass())
-                .account(testAccount)
-                .build();
-
-        collectionRequest = CollectionRequest.builder()
-                .region(getLocalizationUtils().getLocale())
-                .audience("false")
-                .language(getLocalizationUtils().getUserLanguage())
-                .slug(DisneyStandardCollection.MOVIES.getSlug())
-                .contentClass(DisneyStandardCollection.MOVIES.getContentClass())
-                .account(testAccount)
-                .build();
-
-        ContentCollection contentCollection = getSearchApi().getCollection(collectionRequest);
-        List<DisneyCollectionSet> setInfo = contentCollection.getCollectionSetsInfo();
     }
 }
