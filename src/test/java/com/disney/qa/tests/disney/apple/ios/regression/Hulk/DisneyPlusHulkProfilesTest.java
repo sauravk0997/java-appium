acde0a13df2fb6fc72b6d6602f1aadce1465b159
csolmaz <cristina.solmaz@hulu.com>
2024-03-07 09:37:28 -0800

    add movie playback test, improve stability

--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/Hulk/DisneyPlusHulkProfilesTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/Hulk/DisneyPlusHulkProfilesTest.java
@@ -62,7 +62,7 @@ public class DisneyPlusHulkProfilesTest extends DisneyBaseTest {
             swipeInContainer(null, Direction.UP, 2500);
         }
         detailsPage.getEpisodeToDownload("1", "1").click();
-        detailsPage.waitForOneEpisodeDownloadToComplete(45, 3);
+        detailsPage.waitForOneEpisodeDownloadToComplete(90, 6);
         searchPage.clickSearchIcon();
         searchPage.clearText();
         searchPage.searchForMedia(PREY);
@@ -72,7 +72,7 @@ public class DisneyPlusHulkProfilesTest extends DisneyBaseTest {
             swipeInContainer(null, Direction.UP, 2500);
         }
         detailsPage.getMovieDownloadButton().click();
-        detailsPage.waitForMovieDownloadComplete(150, 15);
+        detailsPage.waitForMovieDownloadComplete(250, 25);
         navigateToTab(DisneyPlusApplePageBase.FooterTabs.DOWNLOADS);
         downloadsPage.isOpened();
         sa.assertTrue(downloadsPage.getDownloadAssetFromListView(ONLY_MURDERS_IN_THE_BUILDING).isPresent(),
