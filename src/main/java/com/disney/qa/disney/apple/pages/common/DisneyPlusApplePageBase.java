72dfedb585678b24721df03e5968ed47ec7e36f8
csolmaz <cristina.solmaz@hulu.com>
2024-03-21 11:33:08 -0700

    add profile pin access test

--- src/main/java/com/disney/qa/disney/apple/pages/common/DisneyPlusApplePageBase.java
+++ src/main/java/com/disney/qa/disney/apple/pages/common/DisneyPlusApplePageBase.java
@@ -1340,4 +1340,24 @@ public class DisneyPlusApplePageBase extends DisneyAbstractPage implements IRemo
             return iPadKeyboardDelete;
         }
     }
+
+public ExtendedWebElement getPinProtectedProfileIcon(String name) {
+    return getDynamicAccessibilityId(
+            getDictionary().formatPlaceholderString(
+                    getDictionary().getDictionaryItem(DisneyDictionaryApi.ResourceKeys.PCON, DictionaryKeys.ACCESS_PIN_PROFILE.getText()), Map.of(USER_PROFILE, name)));
+}
+
+    public ExtendedWebElement getCellPinProtectedProfileIcon(String name) {
+        return dynamicCellByLabel.format(
+                getDictionary().formatPlaceholderString(
+                        getDictionary().getDictionaryItem(DisneyDictionaryApi.ResourceKeys.PCON, DictionaryKeys.ACCESS_PIN_PROFILE.getText()), Map.of(USER_PROFILE, name)));
+    }
+
+    public boolean isPinProtectedProfileIconPresent(String name) {
+        if (getCellPinProtectedProfileIcon(name).isPresent(SHORT_TIMEOUT)) {
+            return getCellPinProtectedProfileIcon(name).isPresent();
+        } else {
+            return getPinProtectedProfileIcon(name).isPresent();
+        }
+    }
 }
