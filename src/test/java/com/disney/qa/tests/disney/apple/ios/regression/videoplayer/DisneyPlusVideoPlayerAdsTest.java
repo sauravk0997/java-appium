ad850db8aeaa5701173d07dd0747f4e1bf0dbc10
csolmaz <cristina.solmaz@hulu.com>
2024-04-18 15:52:26 -0700

    cleanup

--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/videoplayer/DisneyPlusVideoPlayerAdsTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/videoplayer/DisneyPlusVideoPlayerAdsTest.java
@@ -49,7 +49,6 @@ public class DisneyPlusVideoPlayerAdsTest extends DisneyBaseTest {
         sa.assertTrue(videoPlayer.isAdBadgeLabelPresent(), "Ad badge label was not found during first ad.");
 
         videoPlayer.waitForPreRollAdToComplete(90, 6);
-//        videoPlayer.waitForCertainAmountOfPlayback(10, 2);
         videoPlayer.scrubToPlaybackPercentage(50);
         sa.assertFalse(videoPlayer.isAdBadgeLabelPresent(), "Ad badge label was found after scrubbing forward past new ad pod.");
         sa.assertAll();
