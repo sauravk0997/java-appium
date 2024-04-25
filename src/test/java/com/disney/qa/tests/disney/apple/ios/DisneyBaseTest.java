4df1d3cb9664c2f8b45d4633eea03eee33a38385
ckim1 <choon.kim@disneystreaming.com>
2024-04-24 18:21:20 -0700

    custom lang/locale logic

--- src/test/java/com/disney/qa/tests/disney/apple/ios/DisneyBaseTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/DisneyBaseTest.java
@@ -28,6 +28,7 @@ import org.slf4j.LoggerFactory;
 import org.testng.ITestContext;
 import org.testng.SkipException;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import org.testng.asserts.SoftAssert;
 
 import com.disney.jarvisutils.pages.apple.JarvisAppleBase;
@@ -71,17 +72,22 @@ public class DisneyBaseTest extends DisneyAppleBaseTest {
     public static final String MULTIVERSE_STAGING_ENDPOINT = "https://multiverse-alice-client-staging.qateam.bamgrid.com";
     private static final String S3_BASE_PATH = "bamtech-qa-alice/disney/recognition/alice/";
 
-    @BeforeMethod(alwaysRun = true, onlyForGroups = TestGroup.CUSTOM_LANG_LOCALE)
+    @BeforeSuite(alwaysRun = true)
     public void customLangLocale(ITestContext context) {
-        localContext.set(context);
         String testXmlLocale = context.getCurrentXmlTest().getParameter("locale");
         String testXmlLanguage = context.getCurrentXmlTest().getParameter("language");
+
+        if (testXmlLocale == null || testXmlLanguage == null) {
+            return;
+        }
+        LOGGER.info("CHOON!");
+        localContext.set(context);
         R.CONFIG.put("locale", testXmlLocale, true);
-        R.CONFIG.put("capabilities." + WebDriverConfiguration.Parameter.LANGUAGE_TAG, testXmlLocale, true);
+        //R.CONFIG.put("capabilities." + WebDriverConfiguration.Parameter.LANGUAGE_TAG, testXmlLocale, true);
         R.CONFIG.put("language", testXmlLanguage, true);
     }
 
-    @BeforeMethod(alwaysRun = true, onlyForGroups = TestGroup.NO_RESET, dependsOnMethods = "customLangLocale")
+    @BeforeMethod(alwaysRun = true, onlyForGroups = TestGroup.NO_RESET)
     public void enableNoTestReset() {
         R.CONFIG.put("capabilities." + SupportsNoResetOption.NO_RESET_OPTION, "true", true);
         R.CONFIG.put("capabilities." + SupportsFullResetOption.FULL_RESET_OPTION, "false", true);
