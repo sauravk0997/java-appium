7da44766db3aae7de4e3b22272aa3541d8832996
ckim1 <choon.kim@disneystreaming.com>
2024-04-24 18:19:35 -0700

    test

--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/ratings/DisneyPlusNonUSRatingsKCCTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/ratings/DisneyPlusNonUSRatingsKCCTest.java
@@ -8,7 +8,7 @@ import org.testng.annotations.Test;
 public class DisneyPlusNonUSRatingsKCCTest extends DisneyPlusRatingsBase {
 
     @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-75186"})
-    @Test(description = "Ratings-South Korea 12+ KCC", groups = {"NonUS-Ratings"})
+    @Test(description = "Ratings-South Korea 12+ KCC", groups = {"NonUS-Ratings", TestGroup.CUSTOM_LANG_LOCALE})
     public void verifyRatingSystemSouthKoreaKCC12() {
         ratingsSetup(KCC_12, KOREAN_LANG, KOREA_LOCALE);
         confirmRegionalRatingsDisplays(KCC_12, DictionaryKeys.RATING_KCC_12.getText());
