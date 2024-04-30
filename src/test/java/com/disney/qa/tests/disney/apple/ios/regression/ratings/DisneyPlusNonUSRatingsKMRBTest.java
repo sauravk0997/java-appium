c08bfb511ddba52b6a3619debe3ef1cc04671b98
Garima <garima.krishna@disneystreaming.com>
2024-04-30 16:27:29 -0700

    adding test for KMRB 12, 15 ratings

--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/ratings/DisneyPlusNonUSRatingsKMRBTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/ratings/DisneyPlusNonUSRatingsKMRBTest.java
@@ -8,8 +8,15 @@ public class DisneyPlusNonUSRatingsKMRBTest extends DisneyPlusRatingsBase {
 
     @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-75173"})
     @Test(description = "Ratings-South Korea 12+ KCC", groups = {"NonUS-Ratings"})
-    public void verifyRatingSystemSouthKoreaKCC7() {
-        ratingsSetup(KCC_7, KOREAN_LANG, KOREA_LOCALE);
-        confirmRegionalRatingsDisplays(KCC_7, DictionaryKeys.RATING_KCC_7.getText());
+    public void verifyRatingSystemSouthKoreaKMRB12() {
+        ratingsSetup(KMRB_12, KOREAN_LANG, KOREA_LOCALE);
+        confirmRegionalRatingsDisplays(KMRB_12, DictionaryKeys.RATING_KMRB_12.getText());
+    }
+
+    @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-75174"})
+    @Test(description = "Ratings-South Korea 15+ KCC", groups = {"NonUS-Ratings"})
+    public void verifyRatingSystemSouthKoreaKMRB15() {
+        ratingsSetup(KMRB_15, KOREAN_LANG, KOREA_LOCALE);
+        confirmRegionalRatingsDisplays(KMRB_15, DictionaryKeys.RATING_KMRB_15.getText());
     }
 }
