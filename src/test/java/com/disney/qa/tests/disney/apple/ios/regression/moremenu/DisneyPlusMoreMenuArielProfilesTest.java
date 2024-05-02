d99f8694808ec4b6e87dfe65e390470809eb80d0
hiren <hiren.patel@globant.com>
2024-05-02 16:22:15 +0530

    Add TC to verify Max profile name char

--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/moremenu/DisneyPlusMoreMenuArielProfilesTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/moremenu/DisneyPlusMoreMenuArielProfilesTest.java
@@ -911,6 +911,39 @@ public class DisneyPlusMoreMenuArielProfilesTest extends DisneyBaseTest {
         sa.assertAll();
     }
 
+    /*
+     * This test is implemented as per https://jira.disneystreaming.com/browse/DMGZ-1710
+     * This has bug ticket IOS-10586 as user able to enter and save with more than 12 character
+     */
+    @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-66811"})
+    @Test(description = "Edit Profile - Edit Profile Name - Maximum Profile Character Count", groups = {"Ariel-More Menu", TestGroup.PRE_CONFIGURATION})
+    public void verifyMaxProfileCharacterCount() {
+        String profileNameWithMoreThanMaxCharLimit = "TestTestTest1";
+        String profileNameWithMaxAllowedCharLimit = "TestTestTest";
+        DisneyPlusMoreMenuIOSPageBase moreMenu = initPage(DisneyPlusMoreMenuIOSPageBase.class);
+        DisneyPlusEditProfileIOSPageBase editProfilePage = initPage(DisneyPlusEditProfileIOSPageBase.class);
+        DisneyPlusHomeIOSPageBase homePage = initPage(DisneyPlusHomeIOSPageBase.class);
+        SoftAssert sa = new SoftAssert();
+        setAppToHomeScreen(getAccount());
+
+        moreMenu.clickMoreTab();
+        moreMenu.clickEditProfilesBtn();
+        editProfilePage.clickEditModeProfile(getAccount().getFirstName());
+        editProfilePage.enterProfileName(profileNameWithMoreThanMaxCharLimit);
+
+        sa.assertTrue(editProfilePage.getTextEntryField().getText().equals(profileNameWithMoreThanMaxCharLimit), "User ia not able to enter more than 12 character");
+        //Once we get error message details/copy, need to modify below assertion with actual error message validation
+        sa.assertTrue(editProfilePage.isProfileNameCharacterLimitErrorPresent(), "Profile Name Max character limit error was not found");
+
+        editProfilePage.enterProfileName(profileNameWithMaxAllowedCharLimit);
+        editProfilePage.getDoneButton().click();
+        homePage.waitForHomePageToOpen();
+        sa.assertTrue(homePage.isOpened(), "HomePage is not opened after saving profile with new name");
+        moreMenu.clickMoreTab();
+        sa.assertTrue(moreMenu.isProfileSwitchDisplayed(profileNameWithMaxAllowedCharLimit), "Profile name not saved with Max allowed character limit");
+        sa.assertAll();
+    }
+
     private void setAppToAccountSettings() {
         setAppToHomeScreen(getAccount(), getAccount().getProfiles().get(0).getProfileName());
         navigateToTab(DisneyPlusApplePageBase.FooterTabs.MORE_MENU);
