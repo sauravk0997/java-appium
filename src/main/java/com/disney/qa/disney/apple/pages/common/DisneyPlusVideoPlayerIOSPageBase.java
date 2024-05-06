3f9329134e5c04650219a3fbbe91ce86249462e8
csolmaz <cristina.solmaz@hulu.com>
2024-05-06 16:43:58 -0700

    remove unused

--- src/main/java/com/disney/qa/disney/apple/pages/common/DisneyPlusVideoPlayerIOSPageBase.java
+++ src/main/java/com/disney/qa/disney/apple/pages/common/DisneyPlusVideoPlayerIOSPageBase.java
@@ -7,8 +7,6 @@ import com.zebrunner.carina.webdriver.locator.ExtendedFindBy;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.interactions.PointerInput;
-import org.openqa.selenium.interactions.Sequence;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.slf4j.Logger;
