62d0b754e5e923e02444db112d30cf444b8a1a90
hiren <hiren.patel@globant.com>
2024-05-07 23:27:48 +0530

    removed extra steps

--- src/main/java/com/disney/qa/disney/apple/pages/common/DisneyPlusPasswordIOSPageBase.java
+++ src/main/java/com/disney/qa/disney/apple/pages/common/DisneyPlusPasswordIOSPageBase.java
@@ -112,7 +112,6 @@ public class DisneyPlusPasswordIOSPageBase extends DisneyPlusApplePageBase {
     public void submitPasswordForLogin(String userPassword) {
         //To hide the keyboard, passing \n at the end of password value
         enterLogInPassword(userPassword + "\n");
-        Screenshot.capture(getDriver(), ScreenshotType.EXPLICIT_VISIBLE);
         clickLogInButton();
     }
 
