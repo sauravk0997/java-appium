04f145183c186378c2dd38fe63662a771144ceed
csolmaz <cristina.solmaz@hulu.com>
2024-05-13 09:32:18 -0700

    wip

--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/moremenu/DisneyPlusMoreMenuProfilesTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/moremenu/DisneyPlusMoreMenuProfilesTest.java
@@ -762,6 +762,36 @@ public class DisneyPlusMoreMenuProfilesTest extends DisneyBaseTest {
         sa.assertAll();
     }
 
+
+    @Maintainer("csolmaz")
+    @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-66790"})
+    @Test(description = "Edit Profile - Tap Edit Profile", groups = {"More Menu", TestGroup.PRE_CONFIGURATION})
+    public void verifyEditProfileChangeAvatar() {
+        DisneyPlusEditProfileIOSPageBase editProfile = initPage(DisneyPlusEditProfileIOSPageBase.class);
+        DisneyPlusWhoseWatchingIOSPageBase whoIsWatching = initPage(DisneyPlusWhoseWatchingIOSPageBase.class);
+        DisneyPlusMoreMenuIOSPageBase moreMenu = initPage(DisneyPlusMoreMenuIOSPageBase.class);
+        SoftAssert sa = new SoftAssert();
+        setAppToHomeScreen(getAccount());
+        moreMenu.clickMoreTab();
+        BufferedImage moreMenuAvatar = getElementImage(moreMenu.getProfileAvatar(DEFAULT_PROFILE));
+        moreMenu.clickEditProfilesBtn();
+        editProfile.clickEditModeProfile(getAccount().getFirstName());
+        pause(1);
+        System.out.println(getDriver().getPageSource());
+
+
+        editProfile.getAddProfileAvatar().click();
+        pause(1);
+        System.out.println(getDriver().getPageSource());
+
+//Choose avatar
+//        ExtendedWebElement[] avatars = editProfile.getCellsWithLabels().toArray(new ExtendedWebElement[0]);
+//        BufferedImage selectedAvatar = getElementImage(avatars[0]);
+//        avatars[0].click();
+
+        sa.assertAll();
+    }
+
     private void verifyAutoPlayStateForProfile(String profile, String autoPlayState, SoftAssert sa) {
         DisneyPlusEditProfileIOSPageBase editProfile = initPage(DisneyPlusEditProfileIOSPageBase.class);
         DisneyPlusWhoseWatchingIOSPageBase whoIsWatching = initPage(DisneyPlusWhoseWatchingIOSPageBase.class);
