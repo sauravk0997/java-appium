6b616407df20839ae5f127538389673017925f73
hiren <hiren.patel@globant.com>
2024-05-15 17:38:03 +0530

    Revert continue button locator for ATV

--- src/main/java/com/disney/qa/disney/apple/pages/tv/DisneyPlusAppleTVLoginPage.java
+++ src/main/java/com/disney/qa/disney/apple/pages/tv/DisneyPlusAppleTVLoginPage.java
@@ -156,7 +156,7 @@ public class DisneyPlusAppleTVLoginPage extends DisneyPlusLoginIOSPageBase {
 
     public void clickContinueBtn() {
         Screenshot.capture(getDriver(), ScreenshotType.EXPLICIT_VISIBLE);
-        continueButton.click();
+        primaryButton.click();
     }
 
     public void clickTryAgainBtn() {
