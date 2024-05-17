72b059a6c106d76554eaadcdfd6073f5c18c8d53
hiren <hiren.patel@globant.com>
2024-05-17 21:03:17 +0530

    Removed unused var

--- src/main/java/com/disney/qa/disney/apple/pages/common/DisneyPlusVerifyAgeDOBCollectionIOSPageBase.java
+++ src/main/java/com/disney/qa/disney/apple/pages/common/DisneyPlusVerifyAgeDOBCollectionIOSPageBase.java
@@ -3,14 +3,9 @@ package com.disney.qa.disney.apple.pages.common;
 import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
 import com.zebrunner.carina.webdriver.locator.ExtendedFindBy;
 import org.openqa.selenium.WebDriver;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.lang.invoke.MethodHandles;
 
 @SuppressWarnings("squid:MaximumInheritanceDepth")
 public class DisneyPlusVerifyAgeDOBCollectionIOSPageBase extends DisneyPlusApplePageBase {
-    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     //LOCATORS
     @ExtendedFindBy(accessibilityId = "birthdateEntryView")
