b648f1a9f07286ba3d12baedd7c49d6d140b2a60
csolmaz <cristina.solmaz@hulu.com>
2024-05-22 17:31:11 -0700

    revert

--- src/main/java/com/disney/qa/disney/apple/pages/common/DisneyPlusVideoPlayerIOSPageBase.java
+++ src/main/java/com/disney/qa/disney/apple/pages/common/DisneyPlusVideoPlayerIOSPageBase.java
@@ -708,7 +708,7 @@ public class DisneyPlusVideoPlayerIOSPageBase extends DisneyPlusApplePageBase {
         REWIND
     }
 
-        public void validateRatingsOnPlayer(String rating, String ratingsDictionaryKey, SoftAssert sa, DisneyPlusDetailsIOSPageBase detailsPage) {
+    public void validateRatingsOnPlayer(String rating, String ratingsDictionaryKey, SoftAssert sa, DisneyPlusDetailsIOSPageBase detailsPage) {
         detailsPage.getPlayButton().click();
         sa.assertTrue(isRatingPresent(ratingsDictionaryKey), rating + " Rating was not found on movie video player.");
         clickBackButton();
