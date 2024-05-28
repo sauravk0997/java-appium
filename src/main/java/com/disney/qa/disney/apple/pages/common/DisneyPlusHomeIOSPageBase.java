df896995eec22485da353f0e569d2262f412efdd
kpalanisamy <kiruthika.palanisamy.-ND@disney.com>
2024-05-28 17:40:11 +0530

    updated the changes in comment

--- src/main/java/com/disney/qa/disney/apple/pages/common/DisneyPlusHomeIOSPageBase.java
+++ src/main/java/com/disney/qa/disney/apple/pages/common/DisneyPlusHomeIOSPageBase.java
@@ -10,7 +10,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.invoke.MethodHandles;
-import java.nio.charset.StandardCharsets;
 import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.List;
