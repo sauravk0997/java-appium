aa07afd80dd2f7e8f55c744c73fd951dd28adfb5
kpalanisamy <kiruthika.palanisamy.-ND@disney.com>
2024-05-30 15:12:23 +0530

    removed the conflict

--- src/main/java/com/disney/qa/disney/dictionarykeys/DictionaryKeys.java
+++ src/main/java/com/disney/qa/disney/dictionarykeys/DictionaryKeys.java
@@ -293,7 +293,6 @@ public enum DictionaryKeys {
     MY_DISNEY_CHANGE_EMAIL_HINT("mydisney_change_email_hint"),
     MY_DISNEY_CHANGE_EMAIL_SUCCESS_HEADER("mydisney_change_email_success_header"),
     MY_DISNEY_CHANGE_PASSWORD_BODY("mydisney_change_password_body"),
-    MY_DISNEY_ENTER_PASSWORD_HEADER("mydisney_enter_password_header"),
     MY_DISNEY_CHANGE_PASSWORD_HINT("mydisney_change_password_hint"),
     MY_DISNEY_CONTINUE_BTN("mydisney_continue_btn"),
     MY_DISNEY_CREATE_PASSWORD_HEADER("mydisney_create_password_header"),
