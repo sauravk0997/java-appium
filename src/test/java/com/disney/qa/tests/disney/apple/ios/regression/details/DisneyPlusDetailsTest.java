d23667f5d9cea96419ee60690ee8fde475391f10
hiren <hiren.patel@globant.com>
2024-06-04 22:37:20 +0530

    modify scrub percentage

--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/details/DisneyPlusDetailsTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/details/DisneyPlusDetailsTest.java
@@ -346,7 +346,7 @@ public class DisneyPlusDetailsTest extends DisneyBaseTest {
 
         detailsPage.clickContinueButton();
         sa.assertTrue(videoPlayerPage.isOpened(), "Video player Page is not opened");
-        videoPlayerPage.scrubToPlaybackPercentage(100);
+        videoPlayerPage.scrubToPlaybackPercentage(99.9);
         disneyPlusUpNextIOSPageBase.waitForUpNextUIToAppear();
         videoPlayerPage.clickPauseButton();
         videoPlayerPage.clickBackButton();
