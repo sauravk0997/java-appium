0b7f750fa311641e146104702880b8a06ce4b09a
hiren <hiren.patel@globant.com>
2024-06-12 20:33:29 +0530

    Added add label verification step

--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/videoplayer/DisneyPlusVideoPlayerAdsTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/videoplayer/DisneyPlusVideoPlayerAdsTest.java
@@ -61,6 +61,7 @@ public class DisneyPlusVideoPlayerAdsTest extends DisneyBaseTest {
         loginAndStartPlayback(MS_MARVEL, sa);
         videoPlayer.waitForVideoToStart();
         int remainingTimeBeforeAd = videoPlayer.getRemainingTime();
+        Assert.assertTrue(videoPlayer.isAdBadgeLabelPresent(), AD_BADGE_NOT_PRESENT_ERROR_MESSAGE);
         sa.assertTrue(videoPlayer.isAdTimeDurationPresent(), "Ad time duration wasn't shown when video controls were not present");
         sa.assertTrue(videoPlayer.isAdTimeDurationPresentWithVideoControls(), "Ad time duration wasn't shown when video controls were present");
         videoPlayer.waitForAdToCompleteIfPresent(3);
