8d07af56a30461c6c40c9a69504dd3054b60b799
csolmaz <cristina.solmaz@hulu.com>
2024-06-14 15:10:32 -0700

    update

--- src/main/java/com/disney/qa/api/disney/DisneyContentIds.java
+++ src/main/java/com/disney/qa/api/disney/DisneyContentIds.java
@@ -1,6 +1,9 @@
 package com.disney.qa.api.disney;
 
-@Deprecated
+@Deprecated(forRemoval = true)
+/**
+ * @deprecated - Deprecated as of Jun 14 2024. Change to DisneyEntityIds.
+ */
 public enum DisneyContentIds {
     END_GAME("Marvel Studios' Avengers: Endgame", "211b146d-13a0-4c71-b2f1-75de907781e9", DisneyContentIds.PROGRAM_ID),
     END_GAME_QA("Marvel Studios' Avengers: Endgame", "4193d521-c460-4a98-a3a1-3786e4b21631", DisneyContentIds.PROGRAM_ID),
