2822939ec70b9d205cee9ab0fbce731ed1ebbc76
kpalanisamy <kiruthika.palanisamy.-ND@disney.com>
2024-06-24 13:02:38 +0530

    addressed comments

--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/onboarding/DisneyPlusLoginTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/onboarding/DisneyPlusLoginTest.java
@@ -60,6 +60,7 @@ public class DisneyPlusLoginTest extends DisneyBaseTest {
         DisneyPlusChangeEmailIOSPageBase disneyPlusChangeEmailIOSPageBase = new DisneyPlusChangeEmailIOSPageBase(getDriver());
 
         new DisneyPlusWelcomeScreenIOSPageBase(getDriver()).clickLogInButton();
+
         disneyPlusLoginIOSPageBase.submitEmail(getAccount().getEmail());
         softAssert.assertTrue(disneyPlusPasswordIOSPageBase.isBackButtonPresent(), "Back Arrow should be present");
         softAssert.assertTrue(disneyPlusPasswordIOSPageBase.isDisneyLogoDisplayed(), "Disney+ logo image should be displayed");
