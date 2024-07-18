629cfc1a8b36d2cc81d34bb03529782483012062
Blake Watson <blake.watson@bamtechmedia.com>
2024-07-18 16:23:15 -0400

    QAA-15092 add pause back to see if this helps with failure

--- src/main/java/com/disney/qa/disney/apple/pages/common/DisneyplusLegalIOSPageBase.java
+++ src/main/java/com/disney/qa/disney/apple/pages/common/DisneyplusLegalIOSPageBase.java
@@ -78,6 +78,7 @@ public class DisneyplusLegalIOSPageBase extends DisneyPlusApplePageBase {
         String expandedHeader = localizationObj.getLegalDocumentBody(legalSection).split("\\n")[0];
         LOGGER.info("This is the expanded header text " + expandedHeader);
         getStaticTextByName(legalSection).click();
+        pause(10);
         waitForPresenceOfAnElement(getDynamicAccessibilityId(expandedHeader));
         sa.assertTrue(getStaticTextByName(legalSection).getAttribute(IOSUtils.Attributes.VALUE.getAttribute()).equals(EXPANDED),
                 legalSection + " was not expanded");
