e6cd7011723578c1555bd54917bb5ed88817ad85
Blake Watson <blake.watson@bamtechmedia.com>
2024-07-18 16:54:16 -0400

    QAA-15092 remove Logger as runs are now successful

--- src/main/java/com/disney/qa/disney/apple/pages/common/DisneyplusLegalIOSPageBase.java
+++ src/main/java/com/disney/qa/disney/apple/pages/common/DisneyplusLegalIOSPageBase.java
@@ -76,8 +76,6 @@ public class DisneyplusLegalIOSPageBase extends DisneyPlusApplePageBase {
     public void clickAndCollapseLegalScreenSection(SoftAssert sa, String legalSection, DisneyLocalizationUtils localizationObj) {
         LOGGER.info("Validating functions for: {}", legalSection);
         String expandedHeader = localizationObj.getLegalDocumentBody(legalSection).split("\\n")[0];
-        LOGGER.info("This is the entire legal section text: " + localizationObj.getLegalDocumentBody(legalSection));
-        LOGGER.info("This is the expanded header text " + expandedHeader);
         getStaticTextByName(legalSection).click();
         waitUntil(ExpectedConditions.visibilityOfElementLocated(getDynamicAccessibilityId(expandedHeader).getBy()), DEFAULT_EXPLICIT_TIMEOUT);
         sa.assertTrue(getStaticTextByName(legalSection).getAttribute(IOSUtils.Attributes.VALUE.getAttribute()).equals(EXPANDED),
