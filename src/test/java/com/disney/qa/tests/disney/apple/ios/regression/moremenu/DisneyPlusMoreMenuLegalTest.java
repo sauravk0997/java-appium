c9a0c8620f4d06b793882e760dbbcde3eba80b54
Blake Watson <blake.watson@bamtechmedia.com>
2024-07-18 17:27:40 -0400

    QAA-15092 move Soft Assert to match other test methods and delete DisneyOffer unneeded code

--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/moremenu/DisneyPlusMoreMenuLegalTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/moremenu/DisneyPlusMoreMenuLegalTest.java
@@ -118,14 +118,10 @@ public class DisneyPlusMoreMenuLegalTest extends DisneyBaseTest {
     @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-68063"})
     @Test(description = "Verify Legal and return to More Menu", groups = {TestGroup.MORE_MENU, TestGroup.PRE_CONFIGURATION})
     public void testMoreMenuLegalDisplay() {
+        SoftAssert sa = new SoftAssert();
         DisneyPlusMoreMenuIOSPageBase moreMenuIOSPageBase = initPage(DisneyPlusMoreMenuIOSPageBase.class);
         DisneyplusLegalIOSPageBase legalIOSPageBase = initPage(DisneyplusLegalIOSPageBase.class);
         DisneyPlusOneTrustIOSPageBase oneTrustPage = initPage(DisneyPlusOneTrustIOSPageBase.class);
-
-        DisneyOffer offer = getAccountApi().lookupOfferToUse(getCountry(), BUNDLE_PREMIUM);
-        setAccount(getAccountApi().createAccount(offer, "US", "en", SUBSCRIPTION_V2));
-        SoftAssert sa = new SoftAssert();
-      
         setAppToHomeScreen(getAccount());
 
         handleAlert(IOSUtils.AlertButtonCommand.ACCEPT);
