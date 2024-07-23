049c198c26120e6ebc31e7c5d1c803c6ed9c30b9
Alejandro Cadavid Correa <alejandro.cadavid.correa.-nd@disney.com>
2024-07-23 15:32:24 -0500

    comments attended

--- src/test/java/com/disney/qa/tests/disney/apple/DisneyAppleBaseTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/DisneyAppleBaseTest.java
@@ -54,8 +54,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
-import static com.disney.qa.common.constant.IConstantHelper.CONTENT_ENTITLEMENT_DISNEY;
-import static com.disney.qa.common.constant.IConstantHelper.CONTENT_ENTITLEMENT_HULU;
+import static com.disney.qa.common.constant.IConstantHelper.*;
 
 /**
  * Base class for both DisneyBaseTest (mobile) and DisneyPlusAppleTVBaseTest (TVOS)
