e2e6794de0ea824c21ec3e0d38892130b744314e
RUIZV035 <Victoria.Ruiz.Martinez.-ND@disney.com>
2024-07-29 11:09:25 -0600

    QAA-15539 unused import removed

--- src/main/java/com/disney/qa/disney/apple/pages/common/DisneyPlusBrandIOSPageBase.java
+++ src/main/java/com/disney/qa/disney/apple/pages/common/DisneyPlusBrandIOSPageBase.java
@@ -5,15 +5,11 @@ import com.disney.qa.disney.dictionarykeys.DictionaryKeys;
 import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
 import com.zebrunner.carina.webdriver.locator.ExtendedFindBy;
 import org.openqa.selenium.WebDriver;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.testng.asserts.SoftAssert;
 import java.awt.image.BufferedImage;
-import java.lang.invoke.MethodHandles;
 
 public class DisneyPlusBrandIOSPageBase extends DisneyPlusApplePageBase {
     private static final String IMAGES_ARE_THE_SAME_ERROR_MESSAGE = "Images are the same";
-    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     @ExtendedFindBy(iosClassChain = "**/XCUIElementTypeOther[`name == \"brandLandingView\"`]")
     private ExtendedWebElement brandLandingView;
