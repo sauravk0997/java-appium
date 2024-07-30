2ab182a7d4b4d53d756f2c61a8f3648d9dc6d3b3
hiren <hiren.patel@globant.com>
2024-07-30 13:08:05 +0530

    Add EMEA rating TC for 6, 9, 14, 16 & 18

--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/ratings/DisneyPlusEMEARatingsTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/ratings/DisneyPlusEMEARatingsTest.java
@@ -12,6 +12,11 @@ import static com.disney.qa.common.constant.RatingConstant.HAITI;
 import static com.disney.qa.common.constant.RatingConstant.MAURITIUS;
 import static com.disney.qa.common.constant.RatingConstant.MAYOTTE;
 import static com.disney.qa.common.constant.RatingConstant.REUNION;
+import static com.disney.qa.common.constant.RatingConstant.Rating.EIGHTEEN_PLUS;
+import static com.disney.qa.common.constant.RatingConstant.Rating.FOURTEEN_PLUS;
+import static com.disney.qa.common.constant.RatingConstant.Rating.NINE_PLUS;
+import static com.disney.qa.common.constant.RatingConstant.Rating.SIXTEEN_PLUS;
+import static com.disney.qa.common.constant.RatingConstant.Rating.SIX_PLUS;
 import static com.disney.qa.common.constant.RatingConstant.Rating.TWELVE_PLUS;
 import static com.disney.qa.common.constant.RatingConstant.UNITED_KINGDOM;
 
@@ -24,6 +29,51 @@ public class DisneyPlusEMEARatingsTest extends DisneyPlusRatingsBase {
         ratingsSetup(TWELVE_PLUS.getContentRating(), getEMEACountryLanguage(locale), locale);
         handleOneTrustPopUp();
         confirmRegionalRatingsDisplays(TWELVE_PLUS.getContentRating());
+    }
+
+    @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-73147"})
+    @Test(groups = {TestGroup.NON_US_RATINGS})
+    public void verifyRatingSystemEMEA6() {
+        String locale = getEMEACountryCode();
+        ratingsSetup(SIX_PLUS.getContentRating(), getEMEACountryLanguage(locale), locale);
+        handleOneTrustPopUp();
+        confirmRegionalRatingsDisplays(SIX_PLUS.getContentRating());
+    }
+
+    @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-73148"})
+    @Test(groups = {TestGroup.NON_US_RATINGS})
+    public void verifyRatingSystemEMEA9() {
+        String locale = getEMEACountryCode();
+        ratingsSetup(NINE_PLUS.getContentRating(), getEMEACountryLanguage(locale), locale);
+        handleOneTrustPopUp();
+        confirmRegionalRatingsDisplays(NINE_PLUS.getContentRating());
+    }
+
+    @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-73150"})
+    @Test(groups = {TestGroup.NON_US_RATINGS})
+    public void verifyRatingSystemEMEA14() {
+        String locale = getEMEACountryCode();
+        ratingsSetup(FOURTEEN_PLUS.getContentRating(), getEMEACountryLanguage(locale), locale);
+        handleOneTrustPopUp();
+        confirmRegionalRatingsDisplays(FOURTEEN_PLUS.getContentRating());
+    }
+
+    @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-73151"})
+    @Test(groups = {TestGroup.NON_US_RATINGS})
+    public void verifyRatingSystemEMEA16() {
+        String locale = getEMEACountryCode();
+        ratingsSetup(SIXTEEN_PLUS.getContentRating(), getEMEACountryLanguage(locale), locale);
+        handleOneTrustPopUp();
+        confirmRegionalRatingsDisplays(SIXTEEN_PLUS.getContentRating());
+    }
+
+    @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-73152"})
+    @Test(groups = {TestGroup.NON_US_RATINGS})
+    public void verifyRatingSystemEMEA18() {
+        String locale = getEMEACountryCode();
+        ratingsSetup(EIGHTEEN_PLUS.getContentRating(), getEMEACountryLanguage(locale), locale);
+        handleOneTrustPopUp();
+        confirmRegionalRatingsDisplays(EIGHTEEN_PLUS.getContentRating());
     }
 
     private String getEMEACountryCode() {
