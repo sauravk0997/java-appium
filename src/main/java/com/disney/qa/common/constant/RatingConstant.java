e59ede4475bc4f181e7713afaad1ccb2638337a6
hiren <hiren.patel@globant.com>
2024-08-05 17:37:52 +0530

    Add TC class and method for LATAM rating TC

--- src/main/java/com/disney/qa/common/constant/RatingConstant.java
+++ src/main/java/com/disney/qa/common/constant/RatingConstant.java
@@ -2,18 +2,33 @@ package com.disney.qa.common.constant;
 
 public class RatingConstant {
 
+    public static final String ARGENTINA = "AR";
+    public static final String BOLIVIA = "BO";
     public static final String BRAZIL = "BR";
+    public static final String CHILE = "CL";
+    public static final String COLOMBIA = "CO";
+    public static final String COSTA_RICA = "CR";
+    public static final String DOMINICAN_REPUBLIC = "DO";
+    public static final String ECUADOR = "EC";
+    public static final String EL_SALVADOR = "SV";
     public static final String GERMANY = "DE";
+    public static final String GUATEMALA = "GT";
+    public static final String HAITI = "HT";
+    public static final String HONDURAS = "HN";
     public static final String JAPAN = "JP";
     public static final String KOREA = "KR";
-    public static final String NEW_ZEALAND = "NZ";
-    public static final String SINGAPORE = "SG";
-    public static final String TURKEY = "TR";
-    public static final String USA = "US";
-    public static final String HAITI = "HT";
     public static final String MAURITIUS = "MU";
     public static final String MAYOTTE = "YT";
+    public static final String MEXICO = "MX";
+    public static final String NEW_ZEALAND = "NZ";
+    public static final String NICARAGUA = "NI";
+    public static final String PANAMA = "PA";
+    public static final String PARAGUAY = "PY";
+    public static final String PERU = "PE";
     public static final String REUNION = "RE";
+    public static final String SINGAPORE = "SG";
+    public static final String TURKEY = "TR";
+    public static final String URUGUAY = "UY";
     public static final String UNITED_KINGDOM = "GB";
 
     public enum Rating {
@@ -29,6 +44,7 @@ public class RatingConstant {
         TWELVE("12"),
         FOURTEEN("14"),
         SIXTEEN("16"),
+        ZERO_PLUS("0"),
         SIX_PLUS("6+"),
         SEVEN_PLUS("7+"),
         NINE_PLUS("9+"),
@@ -71,6 +87,22 @@ public class RatingConstant {
             case "YT":
             case "RE":
             case "GB":
+            case "AR":
+            case "BO":
+            case "CL":
+            case "CO":
+            case "CR":
+            case "DO":
+            case "EC":
+            case "SV":
+            case "GT":
+            case "HN":
+            case "MX":
+            case "NI":
+            case "PA":
+            case "PY":
+            case "PE":
+            case "UY":
                 return "1850";
             default:
                 throw new IllegalArgumentException(String.format("Max maturity rating for %s locale is not found", locale));
@@ -107,6 +139,38 @@ public class RatingConstant {
                 return "23101";
             case "GB":
                 return "23136";
+            case "AR":
+                return "23034";
+            case "BO":
+                return "23042";
+            case "CL":
+                return "23028";
+            case "CO":
+                return "23040";
+            case "CR":
+                return "23033";
+            case "DO":
+                return "23038";
+            case "EC":
+                return "23039";
+            case "SV":
+                return "23043";
+            case "GT":
+                return "23031";
+            case "HN":
+                return "23041";
+            case "MX":
+                return "23045";
+            case "NI":
+                return "23035";
+            case "PA":
+                return "23029";
+            case "PY":
+                return "23036";
+            case "PE":
+                return "23032";
+            case "UY":
+                return "23030";
             default:
                 throw new IllegalArgumentException(String.format("Max maturity rating for %s locale is not found", locale));
         }
---
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/ratings/DisneyPlusLATAMRatingsTest.java
@@ -0,0 +1,130 @@
+package com.disney.qa.tests.disney.apple.ios.regression.ratings;
+
+import com.disney.util.TestGroup;
+import com.zebrunner.agent.core.annotation.TestLabel;
+import org.testng.annotations.Test;
+
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.disney.qa.common.constant.RatingConstant.ARGENTINA;
+import static com.disney.qa.common.constant.RatingConstant.BOLIVIA;
+import static com.disney.qa.common.constant.RatingConstant.CHILE;
+import static com.disney.qa.common.constant.RatingConstant.COLOMBIA;
+import static com.disney.qa.common.constant.RatingConstant.COSTA_RICA;
+import static com.disney.qa.common.constant.RatingConstant.DOMINICAN_REPUBLIC;
+import static com.disney.qa.common.constant.RatingConstant.ECUADOR;
+import static com.disney.qa.common.constant.RatingConstant.EL_SALVADOR;
+import static com.disney.qa.common.constant.RatingConstant.GUATEMALA;
+import static com.disney.qa.common.constant.RatingConstant.HONDURAS;
+import static com.disney.qa.common.constant.RatingConstant.MEXICO;
+import static com.disney.qa.common.constant.RatingConstant.NICARAGUA;
+import static com.disney.qa.common.constant.RatingConstant.PANAMA;
+import static com.disney.qa.common.constant.RatingConstant.PARAGUAY;
+import static com.disney.qa.common.constant.RatingConstant.PERU;
+import static com.disney.qa.common.constant.RatingConstant.Rating.FOURTEEN_PLUS;
+import static com.disney.qa.common.constant.RatingConstant.Rating.SEVEN_PLUS;
+import static com.disney.qa.common.constant.RatingConstant.Rating.SIXTEEN_PLUS;
+import static com.disney.qa.common.constant.RatingConstant.Rating.TEN_PLUS;
+import static com.disney.qa.common.constant.RatingConstant.Rating.THIRTEEN_PLUS;
+import static com.disney.qa.common.constant.RatingConstant.Rating.TWELVE_PLUS;
+import static com.disney.qa.common.constant.RatingConstant.Rating.ZERO_PLUS;
+import static com.disney.qa.common.constant.RatingConstant.URUGUAY;
+
+public class DisneyPlusLATAMRatingsTest extends DisneyPlusRatingsBase {
+
+    @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-68362"})
+    @Test(groups = {TestGroup.RATINGS, TestGroup.RATING_SYSTEM_LATAM})
+    public void verifyRatingSystemLATAM0() {
+        String locale = getLATAMCountryCode();
+        ratingsSetup(ZERO_PLUS.getContentRating(), getLATAMCountryLanguage(locale), locale);
+        handleOneTrustPopUp();
+        confirmRegionalRatingsDisplays(ZERO_PLUS.getContentRating());
+    }
+
+    @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-73172"})
+    @Test(groups = {TestGroup.RATINGS, TestGroup.RATING_SYSTEM_LATAM})
+    public void verifyRatingSystemLATAM7() {
+        String locale = getLATAMCountryCode();
+        ratingsSetup(SEVEN_PLUS.getContentRating(), getLATAMCountryLanguage(locale), locale);
+        handleOneTrustPopUp();
+        confirmRegionalRatingsDisplays(SEVEN_PLUS.getContentRating());
+    }
+
+    @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-73173"})
+    @Test(groups = {TestGroup.RATINGS, TestGroup.RATING_SYSTEM_LATAM})
+    public void verifyRatingSystemLATAM10() {
+        String locale = getLATAMCountryCode();
+        ratingsSetup(TEN_PLUS.getContentRating(), getLATAMCountryLanguage(locale), locale);
+        handleOneTrustPopUp();
+        confirmRegionalRatingsDisplays(TEN_PLUS.getContentRating());
+    }
+
+    @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-73174"})
+    @Test(groups = {TestGroup.RATINGS, TestGroup.RATING_SYSTEM_LATAM})
+    public void verifyRatingSystemLATAM12() {
+        String locale = getLATAMCountryCode();
+        ratingsSetup(TWELVE_PLUS.getContentRating(), getLATAMCountryLanguage(locale), locale);
+        handleOneTrustPopUp();
+        confirmRegionalRatingsDisplays(TWELVE_PLUS.getContentRating());
+    }
+
+    @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-73175"})
+    @Test(groups = {TestGroup.RATINGS, TestGroup.RATING_SYSTEM_LATAM})
+    public void verifyRatingSystemLATAM13() {
+        String locale = getLATAMCountryCode();
+        ratingsSetup(THIRTEEN_PLUS.getContentRating(), getLATAMCountryLanguage(locale), locale);
+        handleOneTrustPopUp();
+        confirmRegionalRatingsDisplays(THIRTEEN_PLUS.getContentRating());
+    }
+
+    @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-73176"})
+    @Test(groups = {TestGroup.RATINGS, TestGroup.RATING_SYSTEM_LATAM})
+    public void verifyRatingSystemLATAM14() {
+        String locale = getLATAMCountryCode();
+        ratingsSetup(FOURTEEN_PLUS.getContentRating(), getLATAMCountryLanguage(locale), locale);
+        handleOneTrustPopUp();
+        confirmRegionalRatingsDisplays(FOURTEEN_PLUS.getContentRating());
+    }
+
+    @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-73177"})
+    @Test(groups = {TestGroup.RATINGS, TestGroup.RATING_SYSTEM_LATAM})
+    public void verifyRatingSystemLATAM16() {
+        String locale = getLATAMCountryCode();
+        ratingsSetup(SIXTEEN_PLUS.getContentRating(), getLATAMCountryLanguage(locale), locale);
+        handleOneTrustPopUp();
+        confirmRegionalRatingsDisplays(SIXTEEN_PLUS.getContentRating());
+    }
+
+    private String getLATAMCountryCode() {
+        List<String> countryCodeList = Arrays.asList(ARGENTINA, BOLIVIA, CHILE, COLOMBIA, COSTA_RICA, DOMINICAN_REPUBLIC, ECUADOR,
+                EL_SALVADOR, GUATEMALA, HONDURAS, MEXICO, NICARAGUA, PANAMA, PARAGUAY, PERU, URUGUAY);
+        LOGGER.info("Selecting random Country code");
+        return countryCodeList.get(new SecureRandom().nextInt(countryCodeList.size() - 1));
+    }
+
+    private String getLATAMCountryLanguage(String countryCode) {
+        switch (countryCode.toUpperCase()) {
+            case ARGENTINA:
+            case BOLIVIA:
+            case CHILE:
+            case COLOMBIA:
+            case COSTA_RICA:
+            case DOMINICAN_REPUBLIC:
+            case ECUADOR:
+            case EL_SALVADOR:
+            case GUATEMALA:
+            case HONDURAS:
+            case MEXICO:
+            case NICARAGUA:
+            case PANAMA:
+            case PARAGUAY:
+            case PERU:
+            case URUGUAY:
+                return "es";
+            default:
+                throw new IllegalArgumentException(String.format("Country language for %s is not found", countryCode));
+        }
+    }
+}
