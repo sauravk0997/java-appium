66dbce1762966f67edf84283ce9510fe0d7df660
Garima <garima.krishna@disneystreaming.com>
2024-08-21 12:45:39 -0700

    repair test

--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/onboarding/DisneyPlusWelcomeScreenTabletTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/onboarding/DisneyPlusWelcomeScreenTabletTest.java
@@ -4,6 +4,7 @@ import com.zebrunner.carina.utils.factory.DeviceType;
 
 import com.disney.util.TestGroup;
 import org.openqa.selenium.ScreenOrientation;
+import org.testng.*;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
@@ -13,31 +14,17 @@ import com.zebrunner.agent.core.annotation.TestLabel;
 public class DisneyPlusWelcomeScreenTabletTest extends DisneyPlusWelcomeScreenTest {
 
     @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-67821"})
-    @Test(description = "Verify the Welcome screen elements in Landscape mode (Tablet only)", groups = {TestGroup.ONBOARDING, TestGroup.PRE_CONFIGURATION}, enabled = false)
+    @Test(groups = {TestGroup.ONBOARDING,
+            TestGroup.PRE_CONFIGURATION, TestGroup.SMOKE})
     public void verifyWelcomeScreenLandscape() {
         SoftAssert sa = new SoftAssert();
-        DisneyPlusWelcomeScreenIOSPageBase disneyPlusWelcomeScreenIOSPageBase = new DisneyPlusWelcomeScreenIOSPageBase(getDriver());
-
+        DisneyPlusWelcomeScreenIOSPageBase welcomeScreen = initPage(DisneyPlusWelcomeScreenIOSPageBase.class);
         setToNewOrientation(DeviceType.Type.IOS_TABLET, ScreenOrientation.PORTRAIT, ScreenOrientation.LANDSCAPE);
-
-
-        sa.assertTrue(disneyPlusWelcomeScreenIOSPageBase.isOpened(),
-                "XMOBQA-62330/62333 - 'Welcome' screen was not displayed on launch/Sign Up button was not displayed as expected");
-
-//        sa.assertTrue(disneyPlusWelcomeScreenIOSPageBase.isDisneyPlusLogoDisplayed(), //Disney image logo identifier is missing
-//                "XMOBQA-62333 - Disney+ Logo was not displayed as expected");
-
-        sa.assertTrue(disneyPlusWelcomeScreenIOSPageBase.isMainTextDisplayed(),
-                "XMOBQA-62333 - 'Marketing Copy' was not displayed as expected");
-
-        swipePageTillElementPresent(disneyPlusWelcomeScreenIOSPageBase.getSubCopyDirectText(), 5, null, Direction.UP, 1);
-
-        sa.assertTrue(disneyPlusWelcomeScreenIOSPageBase.isSubCopyDirectTextPresent(),
-                "XMOBQA-62333 - 'Sign Up CTA' text was not displayed as expected");
-
-        sa.assertTrue(disneyPlusWelcomeScreenIOSPageBase.isLogInButtonDisplayed(),
-                "XMOBQA-62333 - 'Log In' button was not displayed as expected");
-
+        Assert.assertTrue(welcomeScreen.isOpened(), "'Welcome' screen was not displayed on launch");
+        sa.assertTrue(welcomeScreen.isMainTextDisplayed(), "'Marketing Copy' was not displayed as expected");
+        sa.assertTrue(welcomeScreen.isSignUpButtonDisplayed(), "'Sign Up CTA' text was not displayed as expected");
+        sa.assertTrue(welcomeScreen.isSubCopyDirectTextPresent(), "Sub copy is not displayed as expected");
+        sa.assertTrue(welcomeScreen.isLogInButtonDisplayed(), "'Log In' button was not displayed as expected");
         setToNewOrientation(DeviceType.Type.IOS_TABLET, ScreenOrientation.LANDSCAPE, ScreenOrientation.PORTRAIT);
         sa.assertAll();
     }
--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/onboarding/DisneyPlusWelcomeScreenTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/onboarding/DisneyPlusWelcomeScreenTest.java
@@ -11,28 +11,17 @@ import org.testng.asserts.SoftAssert;
 public class DisneyPlusWelcomeScreenTest extends DisneyBaseTest {
 
     @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-67821"})
-    @Test(description = "Verify the Welcome screen elements in Portrait mode", groups = {TestGroup.ONBOARDING, TestGroup.PRE_CONFIGURATION }, enabled = false)
+    @Test(groups = {TestGroup.ONBOARDING,
+            TestGroup.PRE_CONFIGURATION, TestGroup.SMOKE})
     public void verifyWelcomeScreenPortrait() {
         SoftAssert sa = new SoftAssert();
-        DisneyPlusWelcomeScreenIOSPageBase welcomeScreen = new DisneyPlusWelcomeScreenIOSPageBase(getDriver());
+        DisneyPlusWelcomeScreenIOSPageBase welcomeScreen = initPage(DisneyPlusWelcomeScreenIOSPageBase.class);
 
-        Assert.assertTrue(welcomeScreen.isOpened(),
-                "XMOBQA-62330/62332 - 'Welcome' screen was not displayed on launch/Sign Up button was not displayed as expected");
-
-//        sa.assertTrue(welcomeScreen.isDisneyPlusLogoDisplayed(), //Disney image logo identifier is missing
-//                "XMOBQA-62332 - Disney+ Logo was not displayed as expected");
-
-        sa.assertTrue(welcomeScreen.isMainTextDisplayed(),
-                "XMOBQA-62332 - 'Marketing Copy' was not displayed as expected");
-
-        sa.assertTrue(welcomeScreen.isSignUpButtonDisplayed(),
-                "XMOBQA-62332 - 'Sign Up CTA' text was not displayed as expected");
-        sa.assertTrue(welcomeScreen.isSubCopyDirectTextPresent(),
-                "XMOBQA-62332 - Sub copy is not displayed as expected");
-
-        sa.assertTrue(welcomeScreen.isLogInButtonDisplayed(),
-                "XMOBQA-62332 - 'Log In' button was not displayed as expected");
-
+        Assert.assertTrue(welcomeScreen.isOpened(), "'Welcome' screen was not displayed on launch");
+        sa.assertTrue(welcomeScreen.isMainTextDisplayed(), "'Marketing Copy' was not displayed as expected");
+        sa.assertTrue(welcomeScreen.isSignUpButtonDisplayed(), "'Sign Up CTA' text was not displayed as expected");
+        sa.assertTrue(welcomeScreen.isSubCopyDirectTextPresent(), "Sub copy is not displayed as expected");
+        sa.assertTrue(welcomeScreen.isLogInButtonDisplayed(), "'Log In' button was not displayed as expected");
         sa.assertAll();
     }
 }
