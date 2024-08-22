6569f042be0225622c3c54f354a76b81cf7b7ea5
Garima <garima.krishna@disneystreaming.com>
2024-08-22 12:29:03 -0700

    add test for home

--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/home/DisneyPlusHomeTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/home/DisneyPlusHomeTest.java
@@ -148,7 +148,11 @@ public class DisneyPlusHomeTest extends DisneyBaseTest {
         DisneyPlusHomeIOSPageBase homePage = initPage(DisneyPlusHomeIOSPageBase.class);
         DisneyPlusDetailsIOSPageBase detailsPage = initPage(DisneyPlusDetailsIOSPageBase.class);
         setAppToHomeScreen(getAccount());
-
+        homePage.swipeInContainerTillElementIsPresent(
+                homePage.getCollection(CollectionConstant.Collection.RECOMMENDED_FOR_YOU),
+                homePage.getTypeOtherContainsName(RECOMMENDED_FOR_YOU),
+                3,
+                Direction.UP);
         homePage.clickCollectionTile(CollectionConstant.Collection.RECOMMENDED_FOR_YOU, 1);
         Assert.assertTrue(detailsPage.isOpened(), DETAILS_PAGE_DID_NOT_OPEN);
         detailsPage.clickCloseButton();
