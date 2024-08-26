6fadf319393e84577c50d55cf9cfdf90f7df2654
csolmaz <cristina.solmaz@hulu.com>
2024-08-26 15:53:55 -0700

    move login test

--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/onboarding/DisneyPlusArielLoginTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/onboarding/DisneyPlusArielLoginTest.java
@@ -1,17 +1,55 @@
 package com.disney.qa.tests.disney.apple.ios.regression.onboarding;
 
 import com.disney.qa.api.client.requests.CreateDisneyAccountRequest;
-import com.disney.qa.disney.apple.pages.common.DisneyPlusEdnaDOBCollectionPageBase;
-import com.disney.qa.disney.apple.pages.common.DisneyPlusLoginIOSPageBase;
-import com.disney.qa.disney.apple.pages.common.DisneyPlusPasswordIOSPageBase;
-import com.disney.qa.disney.apple.pages.common.DisneyPlusWelcomeScreenIOSPageBase;
+import com.disney.qa.disney.apple.pages.common.*;
 import com.disney.qa.tests.disney.apple.ios.DisneyBaseTest;
 import com.disney.util.TestGroup;
 import com.zebrunner.agent.core.annotation.TestLabel;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class DisneyPlusArielLoginTest extends DisneyBaseTest {
+
+    @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-72231"})
+    @Test(groups = {TestGroup.ONBOARDING, TestGroup.LOG_IN, TestGroup.PRE_CONFIGURATION })
+    public void testLoginDobUnder18() {
+        SoftAssert softAssert = new SoftAssert();
+        DisneyPlusEdnaDOBCollectionPageBase ednaDOBCollectionPage = new DisneyPlusEdnaDOBCollectionPageBase(getDriver());
+        DisneyPlusDOBCollectionPageBase disneyPlusDOBCollectionPageBase = new DisneyPlusDOBCollectionPageBase(getDriver());
+        DisneyPlusLoginIOSPageBase disneyPlusLoginIOSPageBase = new DisneyPlusLoginIOSPageBase(getDriver());
+        DisneyPlusPasswordIOSPageBase disneyPlusPasswordIOSPageBase = new DisneyPlusPasswordIOSPageBase(getDriver());
+        DisneyPlusWelcomeScreenIOSPageBase disneyPlusWelcomeScreenIOSPageBase = new DisneyPlusWelcomeScreenIOSPageBase(getDriver());
+        DisneyPlusAccountIsMinorIOSPageBase disneyPlusAccountIsMinorIOSPageBase = new DisneyPlusAccountIsMinorIOSPageBase(getDriver());
+        CreateDisneyAccountRequest createDisneyAccountRequest = new CreateDisneyAccountRequest();
+        DisneyPlusMoreMenuIOSPageBase moreMenu = initPage(DisneyPlusMoreMenuIOSPageBase.class);
+
+        createDisneyAccountRequest
+                .setDateOfBirth(null)
+                .setGender(null)
+                .setCountry(getLocalizationUtils().getLocale())
+                .setLanguage(getLocalizationUtils().getUserLanguage());
+
+        setAccount(getAccountApi().createAccount(createDisneyAccountRequest));
+
+        disneyPlusWelcomeScreenIOSPageBase.clickLogInButton();
+        disneyPlusLoginIOSPageBase.submitEmail(getAccount().getEmail());
+        disneyPlusPasswordIOSPageBase.submitPasswordForLogin(getAccount().getUserPass());
+
+        disneyPlusDOBCollectionPageBase.isOpened();
+        ednaDOBCollectionPage.enterDOB(Person.MINOR.getMonth(), Person.MINOR.getDay(), Person.MINOR.getYear());
+        ednaDOBCollectionPage.tapSaveAndContinueButton();
+
+        softAssert.assertTrue(disneyPlusAccountIsMinorIOSPageBase.isOpened(),
+                "Contact CS screen did not appear.");
+        disneyPlusAccountIsMinorIOSPageBase.clickHelpCenterButton();
+        moreMenu.goBackToDisneyAppFromSafari();
+
+        disneyPlusAccountIsMinorIOSPageBase.clickDismissButton();
+        Assert.assertTrue(disneyPlusWelcomeScreenIOSPageBase.isSignUpButtonDisplayed(),
+                "User was not logged out and returned to the Welcome screen");
+        softAssert.assertAll();
+    }
 
     @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-74265"})
     @Test(groups = {TestGroup.ONBOARDING, TestGroup.LOG_IN, TestGroup.PRE_CONFIGURATION})
