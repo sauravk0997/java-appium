9c816405dbe33e40fb66a4bc252ac90719c5b369
RUIZV035 <Victoria.Ruiz.Martinez.-ND@disney.com>
2024-09-11 10:51:49 -0600

    QAA-15846 scenario id updated

--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/moremenu/DisneyPlusMoreMenuProfilesTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/moremenu/DisneyPlusMoreMenuProfilesTest.java
@@ -871,7 +871,7 @@ public class DisneyPlusMoreMenuProfilesTest extends DisneyBaseTest {
     }
 
 
-    @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-75399"})
+    @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-75398"})
     @Test(groups = {TestGroup.PROFILES, TestGroup.PRE_CONFIGURATION})
     public void verifyKidProofExitJuniorProfileCorrectCode() {
         DisneyPlusMoreMenuIOSPageBase moreMenu = initPage(DisneyPlusMoreMenuIOSPageBase.class);
