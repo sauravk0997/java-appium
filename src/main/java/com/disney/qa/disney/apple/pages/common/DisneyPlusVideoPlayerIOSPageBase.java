206434c4c5058a3ca2349b7e4daaf35426049896
RUIZV035 <Victoria.Ruiz.Martinez.-ND@disney.com>
2024-09-20 11:11:05 -0600

    QAA-15812 testing video

--- src/main/java/com/disney/qa/disney/apple/pages/common/DisneyPlusVideoPlayerIOSPageBase.java
+++ src/main/java/com/disney/qa/disney/apple/pages/common/DisneyPlusVideoPlayerIOSPageBase.java
@@ -201,7 +201,9 @@ public class DisneyPlusVideoPlayerIOSPageBase extends DisneyPlusApplePageBase {
         LOGGER.info("Activating video player controls...");
         //Check is due to placement of PlayPause, which will pause the video if clicked
         Dimension size = getDriver().manage().window().getSize();
-        tapAtCoordinateNoOfTimes((size.width * 35), (size.height * 50), 1);
+       // tapAtCoordinateNoOfTimes((size.width * 35), (size.height * 50), 1);
+        DisneyPlusVideoPlayerIOSPageBase videoPlayer = initPage(DisneyPlusVideoPlayerIOSPageBase.class);
+        videoPlayer.getPlayerView().click();
         fluentWait(getDriver(), FIFTEEN_SEC_TIMEOUT, FIVE_SEC_TIMEOUT, "Seek bar is present").until(it -> !seekBar.isPresent(ONE_SEC_TIMEOUT));
         int attempts = 0;
         do {
