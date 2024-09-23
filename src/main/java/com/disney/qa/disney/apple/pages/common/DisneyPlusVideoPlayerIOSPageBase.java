8661ce106544070f4745d53dd50eaad5a166b464
hiren <hiren.patel@globant.com>
2024-09-23 13:35:35 +0530

    modify tapno of coordinate method with tap

--- src/main/java/com/disney/qa/disney/apple/pages/common/DisneyPlusVideoPlayerIOSPageBase.java
+++ src/main/java/com/disney/qa/disney/apple/pages/common/DisneyPlusVideoPlayerIOSPageBase.java
@@ -200,7 +200,8 @@ public class DisneyPlusVideoPlayerIOSPageBase extends DisneyPlusApplePageBase {
     public DisneyPlusVideoPlayerIOSPageBase displayVideoController() {
         LOGGER.info("Activating video player controls...");
         //Check is due to placement of PlayPause, which will pause the video if clicked
-        clickElementAtLocation(playerView, 35, 50);
+        Dimension size = getDriver().manage().window().getSize();
+        tap((size.width * 35), (size.height * 50), 1);
         fluentWait(getDriver(), FIFTEEN_SEC_TIMEOUT, FIVE_SEC_TIMEOUT, "Seek bar is present").until(it -> !seekBar.isPresent(ONE_SEC_TIMEOUT));
         int attempts = 0;
         do {
