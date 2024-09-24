904326723c2bae82920c2f16b1780bc1f99dbaeb
hiren <hiren.patel@globant.com>
2024-09-24 16:54:47 +0530

    Add step to verify episode without progress bar

--- src/test/java/com/disney/qa/tests/disney/apple/ios/downloads/DisneyPlusDownloadsTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/downloads/DisneyPlusDownloadsTest.java
@@ -31,6 +31,7 @@ public class DisneyPlusDownloadsTest extends DisneyBaseTest {
         int timeoutInSeconds = 90;
         String zero = "0";
         String one = "1";
+        String two = "2";
         DisneyPlusDetailsIOSPageBase detailsPage = initPage(DisneyPlusDetailsIOSPageBase.class);
         DisneyPlusVideoPlayerIOSPageBase videoPlayer = initPage(DisneyPlusVideoPlayerIOSPageBase.class);
         DisneyPlusDownloadsIOSPageBase downloads = initPage(DisneyPlusDownloadsIOSPageBase.class);
@@ -42,14 +43,14 @@ public class DisneyPlusDownloadsTest extends DisneyBaseTest {
         String episodeTitle = videoPlayer.getSubTitleLabel();
         videoPlayer.scrubToPlaybackPercentage(SCRUB_PERCENTAGE_THIRTY);
         videoPlayer.clickBackButton();
-
         if (PHONE.equalsIgnoreCase(DisneyConfiguration.getDeviceType())) {
             swipeUp(2500);
         }
+
         //Download episode
         detailsPage.getEpisodeToDownload(one, one).click();
+        detailsPage.getEpisodeToDownload(one, two).click();
         detailsPage.waitForOneEpisodeDownloadToComplete(timeoutInSeconds, pollingInSeconds);
-
         //Navigate to Download page
         navigateToTab(DisneyPlusApplePageBase.FooterTabs.DOWNLOADS);
         Assert.assertTrue(downloads.isOpened(), DOWNLOADS_PAGE_DID_NOT_OPEN);
@@ -57,6 +58,8 @@ public class DisneyPlusDownloadsTest extends DisneyBaseTest {
         sa.assertTrue(downloads.getStaticTextByLabel(episodeTitle).isPresent(), "Episode Title was not found");
         sa.assertTrue(downloads.isProgressbarDisplayedOnDownloads(one, one),
                 "Progress bar on series title not displayed");
+        sa.assertFalse(downloads.isProgressbarDisplayedOnDownloads(one, two),
+                "Progress bar on series title displayed on episode 2");
 
         navigateToTab(DisneyPlusApplePageBase.FooterTabs.SEARCH);
         detailsPage.clickCloseButton();
@@ -65,10 +68,10 @@ public class DisneyPlusDownloadsTest extends DisneyBaseTest {
         String movieTitle = videoPlayer.getTitleLabel();
         videoPlayer.scrubToPlaybackPercentage(SCRUB_PERCENTAGE_THIRTY);
         videoPlayer.clickBackButton();
-
         if (PHONE.equalsIgnoreCase(DisneyConfiguration.getDeviceType())) {
             swipeUp(2500);
         }
+
         detailsPage.startDownload();
         detailsPage.waitForMovieDownloadComplete(timeoutInSeconds, pollingInSeconds);
         navigateToTab(DisneyPlusApplePageBase.FooterTabs.DOWNLOADS);
