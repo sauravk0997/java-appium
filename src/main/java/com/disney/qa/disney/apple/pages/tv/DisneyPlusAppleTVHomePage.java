8e4c924ca3e37200c319e47f3c4af9561cc4db46
RUIZV035 <Victoria.Ruiz.Martinez.-ND@disney.com>
2024-09-25 10:49:13 -0600

    QAA-14944 changes to verify brands

--- src/main/java/com/disney/qa/disney/apple/pages/tv/DisneyPlusAppleTVHomePage.java
+++ src/main/java/com/disney/qa/disney/apple/pages/tv/DisneyPlusAppleTVHomePage.java
@@ -268,8 +268,9 @@ public class DisneyPlusAppleTVHomePage extends DisneyPlusHomeIOSPageBase {
             clickSelect();
         }
     }
-
+//
     public void traverseAndVerifyHomepageLayout(List<ContentSet> sets, List<String> brands, SoftAssert sa) {
+        LOGGER.info("** brands: " +  brands.toString());
         brands.forEach(item -> {
             sa.assertTrue(isFocused(getDynamicCellByLabel(item)), "The following brand tile was not focused: " + item);
             Screenshot.capture(getDriver(), ScreenshotType.EXPLICIT_VISIBLE);
