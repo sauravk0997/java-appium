f45ca8bfeeadd871fbe0ae3af37d800d085beb5e
RUIZV035 <Victoria.Ruiz.Martinez.-ND@disney.com>
2024-09-26 08:22:43 -0600

    QAA-14944 improvements

--- src/test/java/com/disney/qa/tests/disney/apple/tvos/regression/home/DisneyPlusAppleTVHomeTests.java
+++ src/test/java/com/disney/qa/tests/disney/apple/tvos/regression/home/DisneyPlusAppleTVHomeTests.java
@@ -2,14 +2,7 @@ package com.disney.qa.tests.disney.apple.tvos.regression.home;
 
 import com.disney.alice.AliceDriver;
 import com.disney.alice.labels.AliceLabels;
-import com.disney.qa.api.client.requests.content.CollectionRequest;
-import com.disney.qa.api.client.responses.content.ContentCollection;
-import com.disney.qa.api.client.responses.content.ContentSet;
 import com.disney.qa.api.explore.response.Container;
-import com.disney.qa.api.pojos.DisneyAccount;
-import com.disney.qa.api.pojos.DisneyOffer;
-import com.disney.qa.api.search.DisneySearchApi;
-import com.disney.qa.api.search.assets.DisneyStandardCollection;
 import com.disney.qa.api.utils.DisneySkuParameters;
 import com.disney.qa.common.constant.CollectionConstant;
 import com.disney.qa.disney.apple.pages.tv.DisneyPlusAppleTVHomePage;
@@ -18,8 +11,6 @@ import com.disney.qa.tests.disney.apple.tvos.DisneyPlusAppleTVBaseTest;
 import com.disney.util.TestGroup;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.zebrunner.agent.core.annotation.TestLabel;
-import com.zebrunner.carina.webdriver.Screenshot;
-import com.zebrunner.carina.webdriver.ScreenshotType;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
@@ -31,15 +22,14 @@ import java.util.stream.IntStream;
 import static com.disney.qa.api.disney.DisneyEntityIds.HOME_PAGE;
 
 public class DisneyPlusAppleTVHomeTests extends DisneyPlusAppleTVBaseTest {
-    private static final String PERSONALIZED_COLLECTION = "PersonalizedCollection";
-// this
+
     @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XCDQA-89521", "XCDQA-89523"})
     @Test(description = "Verify focus and home screen layout upon landing", groups = {TestGroup.HOME})
     public void verifyHomeScreenLayout() throws URISyntaxException, JsonProcessingException {
         DisneyBaseTest disneyBaseTest = new DisneyBaseTest();
         SoftAssert sa = new SoftAssert();
         AliceDriver aliceDriver = new AliceDriver(getDriver());
-        DisneyOffer offer = new DisneyOffer();
+
         setAccount(disneyBaseTest.createAccountWithSku(DisneySkuParameters.DISNEY_US_WEB_YEARLY_PREMIUM, getLocalizationUtils().getLocale(), getLocalizationUtils().getUserLanguage()));
 
         DisneyPlusAppleTVHomePage disneyPlusAppleTVHomePage = new DisneyPlusAppleTVHomePage(getDriver());
@@ -62,6 +52,8 @@ public class DisneyPlusAppleTVHomeTests extends DisneyPlusAppleTVBaseTest {
             sa.assertTrue(disneyPlusAppleTVHomePage.getTypeCellNameContains(titles.get(i)).isElementPresent(),
                     String.format("%s asset of %s not found on first row", titles, titles.get(i)));
         });
+        sa.assertTrue(disneyPlusAppleTVHomePage.getStaticTextByLabelContains(CollectionConstant.Collection.RECOMMENDED_FOR_YOU.name()).isPresent(),
+                "Recommended for you collection is not present");
 
         sa.assertAll();
     }
