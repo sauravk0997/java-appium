62785a85c3b9f7ca03650942a636741aba3256ca
Garima <garima.krishna@disneystreaming.com>
2024-10-01 14:58:36 -0700

    update OTP code retrieval logic

--- src/test/java/com/disney/qa/tests/disney/apple/ios/DisneyBaseTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/DisneyBaseTest.java
@@ -637,4 +637,18 @@ public class DisneyBaseTest extends DisneyAppleBaseTest {
         long minutes = timeInMinutes % 60;
         return String.format("%dh %dm", hours, minutes);
     }
+
+    public String getOTPFromApi(Date startTime, DisneyAccount testAccount) {
+        int emailAPILatency = 10;
+        String firstOTP = getEmailApi().getDisneyOTP(testAccount.getEmail(), startTime);
+        pause(emailAPILatency);
+        String secondOTP = getEmailApi().getDisneyOTP(testAccount.getEmail(), startTime);
+
+        if (!secondOTP.equals(firstOTP)) {
+            LOGGER.info("First and second OTP doesn't match, firstOTP: {}, secondOTP: {}", firstOTP, secondOTP);
+            return secondOTP;
+        } else {
+            return firstOTP;
+        }
+    }
 }
