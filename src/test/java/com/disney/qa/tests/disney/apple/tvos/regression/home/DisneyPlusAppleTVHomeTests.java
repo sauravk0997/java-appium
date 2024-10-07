493002590d7946d85e802e4e9153ca6968e801b2
RUIZV035 <Victoria.Ruiz.Martinez.-ND@disney.com>
2024-10-07 14:24:39 -0600

    QAA-14944 improvements

--- src/test/java/com/disney/qa/tests/disney/apple/tvos/regression/home/DisneyPlusAppleTVHomeTests.java
+++ src/test/java/com/disney/qa/tests/disney/apple/tvos/regression/home/DisneyPlusAppleTVHomeTests.java
@@ -1,66 +1,101 @@
+
+
 package com.disney.qa.tests.disney.apple.tvos.regression.home;
 
-import com.disney.alice.AliceDriver;
-import com.disney.alice.labels.AliceLabels;
-import com.disney.qa.api.explore.response.Container;
-import com.disney.qa.api.utils.DisneySkuParameters;
+import com.disney.qa.api.explore.response.*;
+import com.disney.qa.api.utils.*;
 import com.disney.qa.disney.apple.pages.tv.DisneyPlusAppleTVHomePage;
-import com.disney.qa.tests.disney.apple.ios.DisneyBaseTest;
+import com.disney.qa.disney.apple.pages.tv.DisneyPlusAppleTVWelcomeScreenPage;
 import com.disney.qa.tests.disney.apple.tvos.DisneyPlusAppleTVBaseTest;
 import com.disney.util.TestGroup;
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.*;
 import com.zebrunner.agent.core.annotation.TestLabel;
+import com.zebrunner.carina.webdriver.*;
+import org.slf4j.*;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
-import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.List;
+import java.lang.invoke.*;
+import java.net.*;
+import java.util.*;
 import java.util.stream.IntStream;
 
 import static com.disney.qa.api.disney.DisneyEntityIds.HOME_PAGE;
 
 public class DisneyPlusAppleTVHomeTests extends DisneyPlusAppleTVBaseTest {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
     @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XCDQA-89521"})
     @Test(groups = {TestGroup.HOME})
-    public void verifyHomeScreenLayout() throws URISyntaxException, JsonProcessingException {
-        DisneyPlusAppleTVHomePage disneyPlusAppleTVHomePage = new DisneyPlusAppleTVHomePage(getDriver());
-        DisneyBaseTest disneyBaseTest = new DisneyBaseTest();
-        AliceDriver aliceDriver = new AliceDriver(getDriver());
+    public void verifyHomeScreenLayout() {
+        DisneyPlusAppleTVHomePage homePage = new DisneyPlusAppleTVHomePage(getDriver());
+        DisneyPlusAppleTVWelcomeScreenPage welcomeScreenPage = new DisneyPlusAppleTVWelcomeScreenPage(getDriver());
         SoftAssert sa = new SoftAssert();
-
-        setAccount(disneyBaseTest.createAccountWithSku(DisneySkuParameters.DISNEY_US_WEB_YEARLY_PREMIUM,
-                getLocalizationUtils().getLocale(), getLocalizationUtils().getUserLanguage()));
-
+        ArrayList<Container> homeCollections;
+        setAccount(createAccountWithSku(DisneySkuParameters.DISNEY_US_WEB_YEARLY_PREMIUM,
+               getLocalizationUtils().getLocale(), getLocalizationUtils().getUserLanguage()));
         logInTemp(getAccount());
 
+        Screenshot.capture(getDriver(), ScreenshotType.EXPLICIT_VISIBLE);
+        sa.assertTrue(welcomeScreenPage.isOpened(), "Welcome screen did not launch");
         //stop hero carousel
-        disneyPlusAppleTVHomePage.moveRight(2, 2);
+        homePage.moveRight(2, 2);
 
-        aliceDriver.screenshotAndRecognize().isLabelNotPresent(sa, AliceLabels.HOME_BUTTON_IS_SELECTED.getText())
-                .isLabelPresent(sa, AliceLabels.BANNER_HOVERED.getText());
-        disneyPlusAppleTVHomePage.clickDown();
-        ArrayList<Container> collectionsHome = disneyBaseTest.getDisneyAPIPage(HOME_PAGE.getEntityId());
+        homePage.clickDown();
 
-        List<String> titles = disneyBaseTest.getContainerTitlesFromApi(collectionsHome.get(1).getId(), 50);
-        disneyPlusAppleTVHomePage.moveDown(2,1);
-        // Items related to brands are visible on the screen
-        IntStream.range(0, titles.size()).forEach(i -> {
-            sa.assertTrue(disneyPlusAppleTVHomePage.getTypeCellNameContains(titles.get(i)).isElementPresent(),
-                    String.format("%s asset of %s not found", titles, titles.get(i)));
-        });
-        disneyPlusAppleTVHomePage.moveDown(1,1);
+        try {
+            homeCollections = getDisneyAPIPage(HOME_PAGE.getEntityId(),
+                    getLocalizationUtils().getLocale(),
+                    getLocalizationUtils().getUserLanguage());
+        } catch (URISyntaxException | JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
 
-        // Validate first containers
-        IntStream.range(2, 4).forEach(i -> {
-            List<String> container = disneyBaseTest.getContainerTitlesFromApi(collectionsHome.get(i).getId(), 5);
-            IntStream.range(0, 5).forEach(j -> {
-                sa.assertTrue(disneyPlusAppleTVHomePage.getTypeCellNameContains(container.get(j)).isElementPresent(),
-                        "Title not found");});
-            if(i == 3) { disneyPlusAppleTVHomePage.moveDown(1, 1); }
-        });
+        verifyBrandDetails(homeCollections.get(1).getId(), sa);
 
+        homePage.moveDown(1, 1);
+        homePage.moveLeft(4, 1);
+        homePage.moveRight(2, 1);
+
+        verifyHomeCollectionsAndContent(homeCollections, sa);
         sa.assertAll();
+    }
+
+
+    private void verifyBrandDetails(String brandCollectionID, SoftAssert sa) {
+        DisneyPlusAppleTVHomePage homePage = new DisneyPlusAppleTVHomePage(getDriver());
+        List<Item> brandsCollection = getExploreAPIItemsFromSet(brandCollectionID, 10);
+        brandsCollection.forEach(item -> {
+            sa.assertTrue(homePage.isFocused(homePage.getTypeCellNameContains(item.getVisuals().getTitle())),
+                    "The following brand tile was not focused: " + item);
+            Screenshot.capture(getDriver(), ScreenshotType.EXPLICIT_VISIBLE);
+            homePage.moveRight(1, 1);
+        });
+    }
+
+    private void verifyHomeCollectionsAndContent(ArrayList<Container> homeCollections, SoftAssert sa) {
+        DisneyPlusAppleTVHomePage homePage = initPage(DisneyPlusAppleTVHomePage.class);
+
+        //This removes first 2 collections from the home collection
+        homeCollections.subList(0, 2).clear();
+        LOGGER.info("Home collection size: {}", homeCollections.size());
+
+        homeCollections.forEach(homeCollectionId -> {
+            //Verify shelf title
+            String shelfTitle = homeCollectionId.getVisuals().getName();
+            sa.assertTrue(homePage.getStaticTextByLabelContains(shelfTitle).isPresent(SHORT_TIMEOUT),
+                    "Shelf title not found: " + shelfTitle);
+
+            //Get content for the shelf
+            List<Item> collectionItems = getExploreAPIItemsFromSet(homeCollectionId.getId(), 10);
+            if (!collectionItems.isEmpty() && collectionItems.size() > 0) {
+                String firstContentTitle = collectionItems.get(0).getVisuals().getTitle();
+                LOGGER.info("Content Title: {} for Shelf: {}", firstContentTitle, shelfTitle);
+                //Verify content title
+                sa.assertTrue(homePage.getTypeCellNameContains(firstContentTitle).isPresent(),
+                        "Content title not found: " + firstContentTitle);
+            }
+        });
     }
 }
