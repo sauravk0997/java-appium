84c9e127ea2c0851852db19234c698b8a2cfb9d8
csolmaz <cristina.solmaz@hulu.com>
2024-10-16 16:45:01 -0700

    remove unused

--- src/main/java/com/disney/qa/disney/apple/pages/tv/DisneyPlusAppleTVSearchPage.java
+++ src/main/java/com/disney/qa/disney/apple/pages/tv/DisneyPlusAppleTVSearchPage.java
@@ -12,10 +12,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.invoke.MethodHandles;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.stream.IntStream;
 
 @SuppressWarnings("squid:MaximumInheritanceDepth")
 @DeviceType(pageType = DeviceType.Type.APPLE_TV, parentClass = DisneyPlusSearchIOSPageBase.class)
