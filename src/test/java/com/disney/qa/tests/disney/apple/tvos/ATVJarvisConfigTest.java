047657ab957c82ad47e04211a10d649db6c611cf
Garima <garima.krishna@disneystreaming.com>
2024-10-28 16:59:19 -0700

    add test tag

--- src/test/java/com/disney/qa/tests/disney/apple/tvos/ATVJarvisConfigTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/tvos/ATVJarvisConfigTest.java
@@ -1,13 +1,17 @@
 package com.disney.qa.tests.disney.apple.tvos;
 
-import com.disney.jarvisutils.pages.apple.*;
+import com.disney.jarvisutils.parameters.apple.*;
+import com.disney.qa.tests.disney.apple.*;
+import com.zebrunner.carina.core.*;
 import org.testng.annotations.*;
 
-public class ATVJarvisConfigTest extends DisneyPlusAppleTVBaseTest {
+
+public class ATVJarvisConfigTest extends AbstractTest {
     @Test
     public void disableCompanionConfig() {
-        jarvisOverrideDisableCompanionConfig();
-        terminateApp(sessionBundles.get(JarvisAppleBase.JARVIS));
-        startApp(sessionBundles.get(DISNEY));
+        DisneyPlusAppleTVBaseTest appleTVBaseTest = new DisneyPlusAppleTVBaseTest();
+        appleTVBaseTest.jarvisOverrideDisableCompanionConfig();
+        appleTVBaseTest.terminateApp(JarvisAppleParameters.getEnterpriseBundle());
+        appleTVBaseTest.startApp(DisneyAppleBaseTest.BuildType.ENTERPRISE.toString());
     }
 }
