4ba43ef48d75855fbfeabb3ca5336fd6703d50f3
hiren <hiren.patel@globant.com>
2024-10-28 12:11:27 +0530

    change language for SG locale TC

--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/ratings/DisneyPlusRatingsBase.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/ratings/DisneyPlusRatingsBase.java
@@ -43,7 +43,7 @@ public class DisneyPlusRatingsBase extends DisneyBaseTest {
     static final String KOREAN_LANG = "ko";
     static final String NETHERLANDS_LANG = "en-GB";
     static final String NEW_ZEALAND_LANG = "en";
-    public static final String SINGAPORE_LANG = "en";
+    public static final String SINGAPORE_LANG = "en-GB";
     static final String TURKEY_LANG = "tr";
     static final String LATAM_LANG = "es";
 
