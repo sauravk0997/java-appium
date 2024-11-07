49a59f4b0cedcff1a57c92ead3b8610bd28e8965
RUIZV035 <Victoria.Ruiz.Martinez.-ND@disney.com>
2024-11-07 15:22:44 -0600

    cleaning code

--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/onboarding/DisneyPlusRalphProfileTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/onboarding/DisneyPlusRalphProfileTest.java
@@ -17,7 +17,6 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
