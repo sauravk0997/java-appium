0061305bd4e58aa31eacb1f0ca20814af92d3bd7
hiren <hiren.patel@globant.com>
2024-11-15 17:38:50 +0530

    Add TC to verify Hulu series videoplayer deeplink

--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/deeplinks/DisneyPlusDeepLinksTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/deeplinks/DisneyPlusDeepLinksTest.java
@@ -1,6 +1,7 @@
 package com.disney.qa.tests.disney.apple.ios.regression.deeplinks;
 
 import com.disney.qa.api.client.requests.CreateDisneyProfileRequest;
+import com.disney.qa.api.pojos.explore.ExploreContent;
 import com.disney.qa.api.utils.DisneySkuParameters;
 import com.disney.qa.disney.apple.pages.common.*;
 import com.disney.qa.tests.disney.apple.ios.DisneyBaseTest;
@@ -204,5 +205,32 @@ public class DisneyPlusDeepLinksTest extends DisneyBaseTest {
 
         homePage.getUnavailableOkButton().click();
         Assert.assertTrue(homePage.isOpened(), HOME_PAGE_NOT_DISPLAYED);
+    }
+
+    @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-75302"})
+    @Test(groups = {TestGroup.DEEPLINKS, TestGroup.HULK, TestGroup.PRE_CONFIGURATION, US})
+    public void verifyHuluSeriesVideoPlayerDeepLink() {
+        int seasonNumber = 0;
+        int episodeNumber = 0;
+        DisneyPlusVideoPlayerIOSPageBase videoPlayer = initPage(DisneyPlusVideoPlayerIOSPageBase.class);
+        setAccount(createAccountWithSku(DisneySkuParameters.DISNEY_HULU_NO_ADS_ESPN_WEB,
+                getLocalizationUtils().getLocale(), getLocalizationUtils().getUserLanguage()));
+        setAppToHomeScreen(getAccount());
+
+        ExploreContent seriesApiContent = getDisneyApiSeries(R.TESTDATA.get(
+                "disney_prod_hulu_series_detail_only_murders_in_the_building_entity_id"));
+        String episodeTitle =
+                seriesApiContent.getSeasons().get(seasonNumber).getItems().get(episodeNumber).getVisuals().getEpisodeTitle();
+        String episodeDeeplinkId =
+                seriesApiContent.getSeasons().get(seasonNumber).getItems().get(episodeNumber).getId();
+
+        String contentDeeplink = String.format("%s/%s",
+                R.TESTDATA.get("disney_prod_content_playback_deeplink"),
+                episodeDeeplinkId);
+
+        launchDeeplink(contentDeeplink);
+        videoPlayer.isOpened();
+        Assert.assertTrue(videoPlayer.getSubTitleLabel().contains(episodeTitle),
+                "Video player deeplink is not playing correct series episode");
     }
 }
