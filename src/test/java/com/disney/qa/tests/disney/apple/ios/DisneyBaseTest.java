a2bee5a2fc1024919acedd80f7f9043eb04bf0e4
RUIZV035 <Victoria.Ruiz.Martinez.-ND@disney.com>
2024-11-20 12:44:11 -0600

    changed rewind steps

--- src/test/java/com/disney/qa/tests/disney/apple/ios/DisneyBaseTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/DisneyBaseTest.java
@@ -200,11 +200,13 @@ public class DisneyBaseTest extends DisneyAppleBaseTest {
             restart();
             handleAlert();
             loginToHome(account, profileName);
+            homePage.dismissAppTrackingPopUp();
         } else {
             disneyPlusWelcomeScreenIOSPageBase.clickHomeIcon();
         }
         homePage.dismissAppTrackingPopUp();
         pause(3);
+
     }
 
     /**
