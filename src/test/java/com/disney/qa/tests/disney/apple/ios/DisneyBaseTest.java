0831297443b400999889e110cfad8ec84179800e
csolmaz <cristina.solmaz@hulu.com>
2024-11-20 14:53:19 -0800

    revert temp dismiss app tracking fix

--- src/test/java/com/disney/qa/tests/disney/apple/ios/DisneyBaseTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/DisneyBaseTest.java
@@ -166,8 +166,7 @@ public class DisneyBaseTest extends DisneyAppleBaseTest {
         initPage(DisneyPlusWelcomeScreenIOSPageBase.class).clickLogInButton();
         login(entitledUser);
         pause(5);
-//        initPage(DisneyPlusApplePageBase.class).dismissAppTrackingPopUp(5);
-        handleSystemAlert(AlertButtonCommand.DISMISS, 1);
+        initPage(DisneyPlusApplePageBase.class).dismissAppTrackingPopUp(5);
         if (profileName.length > 0 && !(initPage(DisneyPlusHomeIOSPageBase.class).isOpened())) {
             initPage(DisneyPlusWhoseWatchingIOSPageBase.class).clickProfile(String.valueOf(profileName[0]), true);
         }
