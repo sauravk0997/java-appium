4c9b319e222752a0ca36868f5c8e0b851fe7466e
hiren <hiren.patel@globant.com>
2024-11-21 17:56:04 +0530

    modify percentage

--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/videoplayer/DisneyPlusVideoUpNextTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/videoplayer/DisneyPlusVideoUpNextTest.java
@@ -264,7 +264,7 @@ public class DisneyPlusVideoUpNextTest extends DisneyBaseTest {
 
         //Enable autoplay
         toggleAutoPlay("ON");
-        initiatePlaybackAndScrubOnPlayer(seriesContentTitle, PLAYER_PERCENTAGE_FOR_AUTO_PLAY);
+        initiatePlaybackAndScrubOnPlayer(seriesContentTitle, PLAYER_PERCENTAGE_FOR_UP_NEXT);
         upNextIOSPageBase.waitForUpNextUIToAppear();
         sa.assertTrue(upNextIOSPageBase.verifyUpNextUI(), "Up Next UI was not displayed");
         sa.assertTrue(upNextIOSPageBase.getStaticTextByLabel(upNextTitlePlaceHolder).isPresent(),
