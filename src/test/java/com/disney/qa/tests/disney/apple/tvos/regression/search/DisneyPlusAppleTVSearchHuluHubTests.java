f4d2a1ae580a52d08caf694796bfba56e9756107
RUIZV035 <Victoria.Ruiz.Martinez.-ND@disney.com>
2024-11-26 11:49:40 -0600

    scenario added XCDQA-121511

--- src/test/java/com/disney/qa/tests/disney/apple/tvos/regression/search/DisneyPlusAppleTVSearchHuluHubTests.java
+++ src/test/java/com/disney/qa/tests/disney/apple/tvos/regression/search/DisneyPlusAppleTVSearchHuluHubTests.java
@@ -1,5 +1,6 @@
 package com.disney.qa.tests.disney.apple.tvos.regression.search;
 
+import com.disney.qa.disney.apple.pages.common.DisneyPlusOneTrustConsentBannerIOSPageBase;
 import com.disney.qa.disney.apple.pages.tv.*;
 import com.disney.qa.tests.disney.apple.tvos.DisneyPlusAppleTVBaseTest;
 import com.disney.util.TestGroup;
@@ -32,4 +33,31 @@ public class DisneyPlusAppleTVSearchHuluHubTests extends DisneyPlusAppleTVBaseTe
         searchPage.typeInSearchField(HULU_CONTENT);
         Assert.assertTrue(searchPage.getStaticTextByLabelContains(HULU_CONTENT).isPresent(), "Hulu content is not present");
     }
+
+    @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XCDQA-121511"})
+    @Test(groups = {TestGroup.HULU_HUB, TestGroup.SEARCH, US})
+    public void verifyHuluHubSearchContentInNonEligibleCountry() {
+        DisneyPlusAppleTVSearchPage searchPage = new DisneyPlusAppleTVSearchPage(getDriver());
+        DisneyPlusAppleTVHomePage home = new DisneyPlusAppleTVHomePage(getDriver());
+        DisneyPlusAppleTVWelcomeScreenPage welcomeScreenPage = new DisneyPlusAppleTVWelcomeScreenPage(getDriver());
+        DisneyPlusOneTrustConsentBannerIOSPageBase bannerIOSPageBase =
+                new DisneyPlusOneTrustConsentBannerIOSPageBase(getDriver());
+        String standaloneAccount = "alekhya.rallapalli+6745f17f@disneyplustesting.com";
+        selectAppleUpdateLaterAndDismissAppTracking();
+        Assert.assertTrue(welcomeScreenPage.isOpened(), "Welcome screen did not launch");
+
+        loginATVHuluHub(standaloneAccount);
+        if (bannerIOSPageBase.isAllowAllButtonPresent()) {
+            bannerIOSPageBase.tapAcceptAllButton();
+        }
+        Assert.assertTrue(home.isOpened(), "Home page did not open");
+        home.moveDownFromHeroTileToBrandTile();
+        home.openGlobalNavAndSelectOneMenu(SEARCH.getText());
+        Assert.assertTrue(searchPage.isOpened(), "Search page did not open");
+        searchPage.typeInSearchField(HULU_CONTENT);
+        Assert.assertFalse(searchPage.getStaticTextByLabelContains(HULU_CONTENT).isPresent(),
+                "Hulu content is present");
+    }
+
+
 }
