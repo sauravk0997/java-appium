6c0d49659a8e0569fa3a03ec13aa0c1e6d819862
csolmaz <cristina.solmaz@hulu.com>
2024-12-02 11:34:07 -0800

    fix

--- src/test/java/com/disney/qa/tests/disney/apple/tvos/regression/onboarding/DisneyPlusAppleTVLoginTests.java
+++ src/test/java/com/disney/qa/tests/disney/apple/tvos/regression/onboarding/DisneyPlusAppleTVLoginTests.java
@@ -475,9 +475,7 @@ public class DisneyPlusAppleTVLoginTests extends DisneyPlusAppleTVBaseTest {
                 "Add profile is not in focus");
 
         whoIsWatchingPage.moveDown(1, 1);
-        pause(5);
-        System.out.println(getDriver().getPageSource());
-        sa.assertTrue(whoIsWatchingPage.isFocused(whoIsWatchingPage.getTypeCellLabelContains(editProfileBtn)),
+        sa.assertTrue(whoIsWatchingPage.isFocused(whoIsWatchingPage.getTypeButtonContainsLabel(editProfileBtn)),
                 "Edit profile button is not in focus");
         sa.assertAll();
     }
