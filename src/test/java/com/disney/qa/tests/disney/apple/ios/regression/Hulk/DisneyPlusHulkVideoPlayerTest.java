a94a62e8becc0fd3ae402d8bb093e6d4f700e218
Juan Castiblanco <Juan.X.Castiblanco.-ND@disney.com>
2024-12-09 14:14:38 -0500

    Add some screen print temporal tests

--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/Hulk/DisneyPlusHulkVideoPlayerTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/Hulk/DisneyPlusHulkVideoPlayerTest.java
@@ -149,7 +149,9 @@ public class DisneyPlusHulkVideoPlayerTest extends DisneyBaseTest {
 
         launchDeeplink(R.TESTDATA.get("disney_prod_hulu_series_survivor_episode_playback"));
         videoPlayer.waitForVideoToStart();
+        System.out.println(getDriver().getPageSource());
         Assert.assertTrue(videoPlayer.getSkipRecapButton().isPresent(),"Skip Recap button is not present");
+        System.out.println(getDriver().getPageSource());
         Assert.assertFalse(videoPlayer.getNetworkWatermarkLogo(contentNetwork).isElementPresent(1),
                 String.format("Network (%s) Watermark logo is present at the same time that Skip Recap button",
                         contentNetwork));
