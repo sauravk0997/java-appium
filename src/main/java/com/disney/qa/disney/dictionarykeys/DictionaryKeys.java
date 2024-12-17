7711c62956ef6dea16a9cd47020250bb6e56cd04
Juan Castiblanco <Juan.X.Castiblanco.-ND@disney.com>
2024-12-17 11:05:07 -0500

    Remove unused key

--- src/main/java/com/disney/qa/disney/dictionarykeys/DictionaryKeys.java
+++ src/main/java/com/disney/qa/disney/dictionarykeys/DictionaryKeys.java
@@ -411,7 +411,6 @@ public enum DictionaryKeys {
     PLAN_SWITCH_IAP_ANNUAL("plan_switch_iap_cta_annual"),
     PLAY("play"),
     PLAYBACK_LANGUAGE_HEADER("profile_settings_playback_language_header"),
-    POSTPLAY_BECAUSEYOUWATCHED("postplay_becauseyouwatched"),
     POSTPLAY_NEXTEPISODE_HEADER("btn_upnextlite_play"),
     POSTPLAY_SNEAK_PEEK("postplay_sneakpeek"),
     POSTPLAY_UPNEXT("postplay_upnext"),
