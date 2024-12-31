f5d64fec491b672f4f63c4cd254ca127e91cb11d
RUIZV035 <Victoria.Ruiz.Martinez.-ND@disney.com>
2024-12-30 18:20:06 -0600

    added alice validation

--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/videoplayer/DisneyPlusVideoPlayerLockScreenTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/videoplayer/DisneyPlusVideoPlayerLockScreenTest.java
@@ -1,12 +1,13 @@
 package com.disney.qa.tests.disney.apple.ios.regression.videoplayer;
 
+import com.disney.alice.AliceAssertion;
+import com.disney.alice.AliceDriver;
 import com.disney.qa.disney.apple.pages.common.DisneyPlusDetailsIOSPageBase;
 import com.disney.qa.disney.apple.pages.common.DisneyPlusVideoPlayerIOSPageBase;
 import com.disney.qa.tests.disney.apple.ios.DisneyBaseTest;
 import com.disney.util.TestGroup;
 import com.zebrunner.agent.core.annotation.TestLabel;
 import com.zebrunner.carina.utils.R;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
@@ -20,23 +21,27 @@ public class DisneyPlusVideoPlayerLockScreenTest extends DisneyBaseTest {
     public void verifyPlaybackLockScreen() {
         DisneyPlusDetailsIOSPageBase detailsPage = initPage(DisneyPlusDetailsIOSPageBase.class);
         DisneyPlusVideoPlayerIOSPageBase videoPlayer = initPage(DisneyPlusVideoPlayerIOSPageBase.class);
+        AliceDriver aliceDriver = new AliceDriver(getDriver());
         SoftAssert sa = new SoftAssert();
+        String overlayLocked = "closed_captioning";
 
         // Login and open deeplink to movie
         setAppToHomeScreen(getAccount());
         launchDeeplink(R.TESTDATA.get("disney_prod_movie_detail_dr_strange_deeplink"));
-        Assert.assertTrue(detailsPage.isOpened(), "Details page did not open");
+        sa.assertTrue(detailsPage.isOpened(), "Details page did not open");
         detailsPage.clickPlayButton();
-        Assert.assertTrue(videoPlayer.isOpened(), "Video player did not open");
+        sa.assertTrue(videoPlayer.isOpened(), "Video player did not open");
         videoPlayer.waitForVideoToStart();
-        Assert.assertTrue(isLockScreenButtonPresent(),
-                "Lock icon is not present");
-        pause(5);
+        sa.assertTrue(isLockScreenButtonPresent(),"Lock icon is not present");
+        pause(3);
         clickLockScreenButton();
-        System.out.println(getDriver().getPageSource());
-        Assert.assertTrue(getScreenLockedText(),"Screen is not locked");
 
-
+        // Look for locked overlay
+        clickElementAtLocation(videoPlayer.getPlayerView(), 10, 50);
+        AliceAssertion aliceAssertion = aliceDriver.screenshotAndRecognize();
+        aliceAssertion.getMetaData().forEach(item -> item.getCaption());
+        aliceAssertion.isLabelPresent(sa, overlayLocked);
+        aliceAssertion.isLabelPresent(sa, "CLOSED_CAPTIONING");
         sa.assertAll();
     }
 
@@ -50,15 +55,5 @@ public class DisneyPlusVideoPlayerLockScreenTest extends DisneyBaseTest {
         DisneyPlusVideoPlayerIOSPageBase videoPlayer = initPage(DisneyPlusVideoPlayerIOSPageBase.class);
         clickElementAtLocation(videoPlayer.getPlayerView(), 10, 50);
         videoPlayer.getElementFor(PlayerControl.LOCK_ICON).click();
-    }
-
-    private boolean getScreenLockedText() {
-        DisneyPlusVideoPlayerIOSPageBase videoPlayer = initPage(DisneyPlusVideoPlayerIOSPageBase.class);
-        // videoPlayer.displayVideoController();
-        clickElementAtLocation(videoPlayer.getPlayerView(), 50, 50);
-        return videoPlayer.getPlayerControlScreenLockedPresent().isElementPresent();
-        //.getStaticTextByLabelContains
-        // ("SCREEN
-        // LOCKED").isPresent();
     }
 }
