ae96edd47eefe7309a302db09b5187eb517eacef
RUIZV035 <Victoria.Ruiz.Martinez.-ND@disney.com>
2025-01-17 14:33:55 -0600

    xml updated

--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/concurrency/DisneyPlusConcurrencyTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/concurrency/DisneyPlusConcurrencyTest.java
@@ -5,6 +5,7 @@ import com.disney.qa.tests.disney.apple.ios.DisneyBaseTest;
 import com.disney.util.TestGroup;
 import com.zebrunner.agent.core.annotation.TestLabel;
 import com.zebrunner.carina.utils.R;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
@@ -16,36 +17,29 @@ public class DisneyPlusConcurrencyTest extends DisneyBaseTest {
     @Test(groups = {TestGroup.CONCURRENCY, TestGroup.VIDEO_PLAYER, TestGroup.PRE_CONFIGURATION, US})
     public void verifyConcurrency() {
         DisneyPlusVideoPlayerIOSPageBase videoPlayer = initPage(DisneyPlusVideoPlayerIOSPageBase.class);
-        DisneyPlusWelcomeScreenIOSPageBase welcomePage = initPage(DisneyPlusWelcomeScreenIOSPageBase.class);
-        DisneyPlusLoginIOSPageBase loginPage = new DisneyPlusLoginIOSPageBase(getDriver());
-        DisneyPlusPasswordIOSPageBase passwordPage = initPage(DisneyPlusPasswordIOSPageBase.class);
         DisneyPlusHomeIOSPageBase homePage = initPage(DisneyPlusHomeIOSPageBase.class);
         SoftAssert sa = new SoftAssert();
-      //  setAppToHomeScreen(getAccount());
-/*
-        String email = getAccount().getEmail();
-        String password = getAccount().getUserPass();
-        LOGGER.info("* email** " + email);
-        LOGGER.info("* password ** " + password);
-        */
-        String email = System.getenv("accountEmail");
-        String password = System.getenv("accountPassword");
 
-        welcomePage.clickLogInButton();
-        loginPage.submitEmail(email);
-        passwordPage.submitPasswordForLogin(password);
-    //    loginPage.submitEmail(getAccount().getEmail());
-       // loginPage.submitEmail("qaittestguid+173402350387805e8@gsuite.disneyplustesting.com");
-     //   passwordPage.submitPasswordForLogin(getAccount().getUserPass());
-       // passwordPage.submitPasswordForLogin("M1ck3yM0us3#");
+        loginIn(System.getenv("accountEmail"), System.getenv("accountPassword"));
 
-
-        //  passwordPage.clickPrimaryButton();
-       // pause(5);
         homePage.waitForHomePageToOpen();
         sa.assertTrue(homePage.isOpened(), "Home page did not open");
         launchDeeplink(R.TESTDATA.get("disney_prod_movie_deadpool_rated_r_deeplink"));
         sa.assertTrue(videoPlayer.isOpened(), "Video player did not open");
 
     }
+
+    public void loginIn(String email, String password) {
+        DisneyPlusWelcomeScreenIOSPageBase welcomePage = initPage(DisneyPlusWelcomeScreenIOSPageBase.class);
+        DisneyPlusLoginIOSPageBase loginPage = new DisneyPlusLoginIOSPageBase(getDriver());
+        DisneyPlusPasswordIOSPageBase passwordPage = initPage(DisneyPlusPasswordIOSPageBase.class);
+        DisneyPlusHomeIOSPageBase homePage = initPage(DisneyPlusHomeIOSPageBase.class);
+
+        welcomePage.clickLogInButton();
+        loginPage.submitEmail(email);
+        passwordPage.submitPasswordForLogin(password);
+        homePage.waitForHomePageToOpen();
+    }
+
+
 }
