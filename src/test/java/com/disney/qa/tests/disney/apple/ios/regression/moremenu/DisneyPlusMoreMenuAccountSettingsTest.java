36b2c4d1014f5ea64b0e70e4c0488984fe6c0e7b
hiren <hiren.patel@globant.com>
2025-01-28 22:21:03 +0530

    remove sa

--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/moremenu/DisneyPlusMoreMenuAccountSettingsTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/moremenu/DisneyPlusMoreMenuAccountSettingsTest.java
@@ -157,10 +157,8 @@ public class DisneyPlusMoreMenuAccountSettingsTest extends DisneyBaseTest {
                 getLocalizationUtils().getUserLanguage()));
         DisneyPlusAccountIOSPageBase disneyPlusAccountIOSPageBase = new DisneyPlusAccountIOSPageBase(getDriver());
         setAppToAccountSettings();
-        SoftAssert sa = new SoftAssert();
-        sa.assertTrue(disneyPlusAccountIOSPageBase.isSubscriptionMessageDisplayed(),
+        Assert.assertTrue(disneyPlusAccountIOSPageBase.isSubscriptionMessageDisplayed(),
                 "Direct Billing Web subscription message was not displayed");
-        sa.assertAll();
     }
 
     @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-75401"})
