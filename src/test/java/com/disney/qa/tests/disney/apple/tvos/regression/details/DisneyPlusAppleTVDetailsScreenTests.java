bc6e99ff980b2e7f0c8637ea41371d7a8ed6887f
hiren <hiren.patel@globant.com>
2025-01-28 14:00:09 +0530

    modify content

--- src/test/java/com/disney/qa/tests/disney/apple/tvos/regression/details/DisneyPlusAppleTVDetailsScreenTests.java
+++ src/test/java/com/disney/qa/tests/disney/apple/tvos/regression/details/DisneyPlusAppleTVDetailsScreenTests.java
@@ -152,7 +152,7 @@ public class DisneyPlusAppleTVDetailsScreenTests extends DisneyPlusAppleTVBaseTe
     @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XCDQA-122531"})
     @Test(groups = {TestGroup.HULU_HUB, US})
     public void verifyHuluLicenseAttributeForStandAloneUser() {
-        String contentTitle = "Shogun";
+        String contentTitle = "Under The Bridge";
         String licenseAttributionText = "Provided by Hulu";
         DisneyPlusAppleTVSearchPage searchPage = new DisneyPlusAppleTVSearchPage(getDriver());
         DisneyPlusAppleTVHomePage home = new DisneyPlusAppleTVHomePage(getDriver());
