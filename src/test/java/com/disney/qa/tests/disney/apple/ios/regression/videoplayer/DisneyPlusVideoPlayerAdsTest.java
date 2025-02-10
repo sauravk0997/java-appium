4830925f7c1e36ba1afb00b741802602ed56ef2b
csolmaz <cristina.solmaz@hulu.com>
2025-02-10 12:26:59 -0800

    add wait for invisibility of seekbar

--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/videoplayer/DisneyPlusVideoPlayerAdsTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/videoplayer/DisneyPlusVideoPlayerAdsTest.java
@@ -127,6 +127,7 @@ public class DisneyPlusVideoPlayerAdsTest extends DisneyBaseTest {
         DisneyPlusVideoPlayerIOSPageBase videoPlayer = initPage(DisneyPlusVideoPlayerIOSPageBase.class);
         SoftAssert sa = new SoftAssert();
         loginAndStartPlayback(SPIDERMAN_THREE);
+        videoPlayer.waitUntil(ExpectedConditions.invisibilityOfElementLocated(videoPlayer.getSeekbar().getBy()), SHORT_TIMEOUT);
         Assert.assertTrue(videoPlayer.isAdBadgeLabelPresent(FIFTEEN_SEC_TIMEOUT), AD_BADGE_NOT_PRESENT_ERROR_MESSAGE);
         sa.assertTrue(videoPlayer.isElementPresent(DisneyPlusVideoPlayerIOSPageBase.PlayerControl.RESTART),
                 "Restart button is not visible on ad player overlay");
