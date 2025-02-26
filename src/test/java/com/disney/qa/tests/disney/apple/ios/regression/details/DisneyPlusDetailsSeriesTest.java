59c3fa8c08985d7418a8a1f1f86307593e1d2286
Blake Watson <blake.watson@bamtechmedia.com>
2025-02-26 08:57:48 -0500

    remove duplicate click detail tab

--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/details/DisneyPlusDetailsSeriesTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/details/DisneyPlusDetailsSeriesTest.java
@@ -402,8 +402,7 @@ public class DisneyPlusDetailsSeriesTest extends DisneyBaseTest {
         if (DisneyConfiguration.getDeviceType().equalsIgnoreCase(PHONE)) {
             detailsPage.swipeTabBar(Direction.LEFT, 1000);
         }
-
-        detailsPage.clickDetailsTab();
+        
         sa.assertTrue(detailsPage.metadataLabelCompareDetailsTab(0, detailsPage.getReleaseDate(), 1),
                 "Release date from metadata label does not match release date from details tab");
         sa.assertTrue(detailsPage.metadataLabelCompareDetailsTab(2, detailsPage.getGenre(), 1),
