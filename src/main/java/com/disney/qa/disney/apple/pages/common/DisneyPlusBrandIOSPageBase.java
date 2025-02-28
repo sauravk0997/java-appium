ca256f01ce687575832fce8ee258b68309c16f8d
Blake Watson <blake.watson@bamtechmedia.com>
2025-02-28 11:35:40 -0500

    line break

--- src/main/java/com/disney/qa/disney/apple/pages/common/DisneyPlusBrandIOSPageBase.java
+++ src/main/java/com/disney/qa/disney/apple/pages/common/DisneyPlusBrandIOSPageBase.java
@@ -12,7 +12,8 @@ import java.util.*;
 public class DisneyPlusBrandIOSPageBase extends DisneyPlusApplePageBase {
     private static final String IMAGES_ARE_THE_SAME_ERROR_MESSAGE = "Images are the same";
 
-    @ExtendedFindBy(iosClassChain = "**/XCUIElementTypeOther[name == \"highEmphasisView\"]/XCUIElementTypeOther[3]/XCUIElementTypeOther/XCUIElementTypeImage")
+    @ExtendedFindBy(iosClassChain = "**/XCUIElementTypeOther[name == \"highEmphasisView\"]/XCUIElementTypeOther[3]" +
+            "/XCUIElementTypeOther/XCUIElementTypeImage")
     protected ExtendedWebElement collectionBrandImageExpanded;
 
     @ExtendedFindBy(iosClassChain = "**/XCUIElementTypeOther[$name = 'buttonBack'$]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeImage[3]")
