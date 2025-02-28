a21c34d4b02ff0312c8f13bc7bf735b141f53841
Juan Castiblanco <Juan.X.Castiblanco.-ND@disney.com>
2025-02-27 21:41:06 -0500

    add ESPN+ constant

--- src/test/java/com/disney/qa/tests/disney/apple/ios/DisneyBaseTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/DisneyBaseTest.java
@@ -94,6 +94,7 @@ public class DisneyBaseTest extends DisneyAppleBaseTest {
     public static final String DEEPLINKURL = "disneyplus://www.disneyplus.com/browse/";
     public static final String JARVIS_PLAYBACK = "Playback";
     public static final String JARVIS_OFFLINE_EXPIRED_LICENSE_OVERRIDE = "Offline Expired License Override";
+    public static final String ESPN_PLUS = "ESPN+";
 
     @BeforeMethod(alwaysRun = true, onlyForGroups = TestGroup.PRE_CONFIGURATION)
     public void beforeAnyAppActions(ITestContext context) {
