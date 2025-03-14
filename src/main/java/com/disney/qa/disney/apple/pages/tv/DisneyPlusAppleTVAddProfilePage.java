356a3c3d75f7caf0abdb375d6a96e3be876ada5c
Juan Castiblanco <Juan.X.Castiblanco.-ND@disney.com>
2025-03-14 13:14:32 -0500

    reorder class and add elements/methods related to ATV Add Profile flow

--- src/main/java/com/disney/qa/disney/apple/pages/tv/DisneyPlusAppleTVAddProfilePage.java
+++ src/main/java/com/disney/qa/disney/apple/pages/tv/DisneyPlusAppleTVAddProfilePage.java
@@ -10,16 +10,50 @@ import com.zebrunner.carina.webdriver.locator.ExtendedFindBy;
 import org.openqa.selenium.InvalidArgumentException;
 import org.openqa.selenium.WebDriver;
 
+import static com.disney.qa.disney.dictionarykeys.DictionaryKeys.*;
+
 
 @SuppressWarnings("squid:MaximumInheritanceDepth")
 @DeviceType(pageType = DeviceType.Type.APPLE_TV, parentClass = DisneyPlusApplePageBase.class)
 public class DisneyPlusAppleTVAddProfilePage extends DisneyPlusAddProfileIOSPageBase {
+
+    @ExtendedFindBy(accessibilityId = "skipAvatarSelectionBarButton")
+    private ExtendedWebElement skipAvatarSelectionBtn;
+
+    ExtendedWebElement enterProfileNameTitle = getStaticTextByLabel(getLocalizationUtils().getDictionaryItem(
+            DisneyDictionaryApi.ResourceKeys.APPLICATION, ADD_PROFILE_ENTER_PROFILE_NAME_TITLE.getText()));
+    ExtendedWebElement enterProfileNameContinueButton = getTypeButtonByLabel(getLocalizationUtils().getDictionaryItem(
+            DisneyDictionaryApi.ResourceKeys.APPLICATION, BTN_ADD_PROFILE_ENTER_NAME_CONTINUE.getText()));
+    ExtendedWebElement enterYourBirthdateTitle = getStaticTextByLabel(getLocalizationUtils().getDictionaryItem(
+            DisneyDictionaryApi.ResourceKeys.APPLICATION, ADD_PROFILE_DATE_OF_BIRTH_TITLE.getText()));
+    ExtendedWebElement enterDateOfBirthContinueButton = getTypeButtonByLabel(getLocalizationUtils().getDictionaryItem(
+            DisneyDictionaryApi.ResourceKeys.APPLICATION, BTN_ADD_PROFILE_DATE_OF_BIRTH_CONTINUE.getText()));
+    ExtendedWebElement selectGenderTitle = getStaticTextByLabel(getLocalizationUtils().getDictionaryItem(
+            DisneyDictionaryApi.ResourceKeys.APPLICATION, ADD_PROFILE_SELECT_GENDER_TITLE.getText()));
+
     public DisneyPlusAppleTVAddProfilePage(WebDriver driver) {
         super(driver);
     }
 
-    @ExtendedFindBy(accessibilityId = "skipAvatarSelectionBarButton")
-    private ExtendedWebElement skipAvatarSelectionBtn;
+    public ExtendedWebElement getEnterProfileNameTitle() {
+        return enterProfileNameTitle;
+    }
+
+    public ExtendedWebElement getEnterProfileNameContinueButton() {
+        return enterProfileNameContinueButton;
+    }
+
+    public ExtendedWebElement getEnterYourBirthdateTitle() {
+        return enterYourBirthdateTitle;
+    }
+
+    public ExtendedWebElement getEnterDateOfBirthContinueButton() {
+        return enterDateOfBirthContinueButton;
+    }
+
+    public ExtendedWebElement getSelectGenderTitle() {
+        return selectGenderTitle;
+    }
 
     public void clickSelectAvatarSkipBtn() { skipAvatarSelectionBtn.click(); }
 
