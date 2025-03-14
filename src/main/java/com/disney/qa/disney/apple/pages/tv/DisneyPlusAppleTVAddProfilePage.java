fbb7800c129f19e767c3563c3ef974a5c4947254
Juan Castiblanco <Juan.X.Castiblanco.-ND@disney.com>
2025-03-14 12:51:34 -0500

    add method to input given DOB

--- src/main/java/com/disney/qa/disney/apple/pages/tv/DisneyPlusAppleTVAddProfilePage.java
+++ src/main/java/com/disney/qa/disney/apple/pages/tv/DisneyPlusAppleTVAddProfilePage.java
@@ -1,10 +1,13 @@
 package com.disney.qa.disney.apple.pages.tv;
 
+import com.disney.qa.api.dictionary.DisneyDictionaryApi;
+import com.disney.qa.common.utils.helpers.DateHelper;
 import com.disney.qa.disney.apple.pages.common.DisneyPlusAddProfileIOSPageBase;
 import com.disney.qa.disney.apple.pages.common.DisneyPlusApplePageBase;
 import com.zebrunner.carina.utils.factory.DeviceType;
 import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
 import com.zebrunner.carina.webdriver.locator.ExtendedFindBy;
+import org.openqa.selenium.InvalidArgumentException;
 import org.openqa.selenium.WebDriver;
 
 
@@ -19,4 +22,17 @@ public class DisneyPlusAppleTVAddProfilePage extends DisneyPlusAddProfileIOSPage
     private ExtendedWebElement skipAvatarSelectionBtn;
 
     public void clickSelectAvatarSkipBtn() { skipAvatarSelectionBtn.click(); }
+
+    @Override
+    public void enterDOB(DateHelper.Month month, String day, String year) {
+        String monthNumber = month.getNum();
+        String fullDate = String.join("", monthNumber, day, year);
+
+        if (fullDate.length() != 8) {
+            throw new InvalidArgumentException("Given Birthdate was invalid");
+        }
+        for (char number : fullDate.toCharArray()) {
+            dynamicBtnFindByLabel.format(number).click();
+        }
+    }
 }
