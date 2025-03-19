2457c10ab6e89e28e9645a0f3b7e884d432b7ebf
csolmaz <cristina.solmaz@hulu.com>
2025-03-19 10:56:28 -0700

    update validation to take no screenshot

--- src/test/java/com/disney/qa/tests/disney/apple/tvos/regression/watchlist/DisneyPlusAppleTVWatchlistTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/tvos/regression/watchlist/DisneyPlusAppleTVWatchlistTest.java
@@ -55,10 +55,11 @@ public class DisneyPlusAppleTVWatchlistTest extends DisneyPlusAppleTVBaseTest {
 
         sa.assertTrue(disneyPlusAppleTVWatchListPage.isOpened(), WATCHLIST_NOT_OPEN);
 
-        sa.assertTrue(disneyPlusAppleTVWatchListPage.isAIDElementPresentWithScreenshot(getLocalizationUtils().getDictionaryItem(DisneyDictionaryApi.ResourceKeys.APPLICATION, DictionaryKeys.WATCHLIST_COPY.getText())), "Empty watchlist text is not present");
+        sa.assertTrue(disneyPlusAppleTVWatchListPage.isDynamicAccessibilityIDElementPresent(getLocalizationUtils().getDictionaryItem(
+                DisneyDictionaryApi.ResourceKeys.APPLICATION, DictionaryKeys.WATCHLIST_COPY.getText())), "Empty watchlist text is not present");
         String subtext = getLocalizationUtils().getDictionaryItem(DisneyDictionaryApi.ResourceKeys.APPLICATION, DictionaryKeys.WATCHLIST_COPY.getText())
                 + ". " +getLocalizationUtils().getDictionaryItem(DisneyDictionaryApi.ResourceKeys.APPLICATION, DictionaryKeys.WATCHLIST_SUBCOPY.getText());
-        sa.assertTrue(disneyPlusAppleTVWatchListPage.isAIDElementPresentWithScreenshot(subtext), "Empty watchlist subtext is not present");
+        sa.assertTrue(disneyPlusAppleTVWatchListPage.isDynamicAccessibilityIDElementPresent(subtext), "Empty watchlist subtext is not present");
         sa.assertAll();
     }
 
