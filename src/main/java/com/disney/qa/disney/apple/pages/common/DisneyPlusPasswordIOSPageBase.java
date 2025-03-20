e18e08dea86b21ba9a90e0f73f655aadb3d8868c
hiren <hiren.patel@globant.com>
2025-03-20 21:43:57 +0530

    remove unused import

--- src/main/java/com/disney/qa/disney/apple/pages/common/DisneyPlusPasswordIOSPageBase.java
+++ src/main/java/com/disney/qa/disney/apple/pages/common/DisneyPlusPasswordIOSPageBase.java
@@ -1,16 +1,12 @@
 package com.disney.qa.disney.apple.pages.common;
 
 import com.disney.qa.api.dictionary.DisneyDictionaryApi;
-import com.disney.qa.common.utils.IOSUtils;
 import com.disney.qa.disney.dictionarykeys.DictionaryKeys;
 import com.zebrunner.carina.webdriver.Screenshot;
 import com.zebrunner.carina.webdriver.ScreenshotType;
 import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
 import com.zebrunner.carina.webdriver.locator.ExtendedFindBy;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.testng.Assert;
-
 import java.util.Map;
 import static com.disney.qa.disney.dictionarykeys.DictionaryKeys.*;
 
