7202562f6b550fd0d6824fa6530e7e34a423a907
csolmaz <cristina.solmaz@hulu.com>
2025-03-25 09:39:24 -0700

    remove unused imports

--- src/main/java/com/disney/qa/disney/apple/pages/tv/DisneyPlusAppleTVWatchListPage.java
+++ src/main/java/com/disney/qa/disney/apple/pages/tv/DisneyPlusAppleTVWatchListPage.java
@@ -1,19 +1,10 @@
 package com.disney.qa.disney.apple.pages.tv;
 
 import com.disney.qa.api.dictionary.DisneyDictionaryApi;
-import com.disney.qa.common.constant.CollectionConstant;
 import com.disney.qa.disney.apple.pages.common.DisneyPlusWatchlistIOSPageBase;
-import com.disney.qa.disney.apple.pages.phone.DisneyPlusMoreMenuIOSPage;
 import com.disney.qa.disney.dictionarykeys.DictionaryKeys;
 import com.zebrunner.carina.utils.factory.DeviceType;
-import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
 import org.openqa.selenium.WebDriver;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import static com.zebrunner.carina.utils.mobile.IMobileUtils.Direction.LEFT;
 
 @SuppressWarnings("squid:MaximumInheritanceDepth")
 @DeviceType(pageType = DeviceType.Type.APPLE_TV, parentClass = DisneyPlusWatchlistIOSPageBase.class)
