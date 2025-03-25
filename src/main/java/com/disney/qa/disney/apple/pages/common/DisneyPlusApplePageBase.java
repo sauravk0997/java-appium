28aab820c42650c9f433bca621f9bd0891dbabc0
csolmaz <cristina.solmaz@hulu.com>
2025-03-25 10:01:23 -0700

    revert

--- src/main/java/com/disney/qa/disney/apple/pages/common/DisneyPlusApplePageBase.java
+++ src/main/java/com/disney/qa/disney/apple/pages/common/DisneyPlusApplePageBase.java
@@ -70,7 +70,6 @@ public class DisneyPlusApplePageBase extends DisneyAbstractPage implements IRemo
     protected static final String DEVICE = "DEVICE";
     public static final String HULU_SERVICE_ATTRIBUTION_MESSAGE = "Included with your Hulu subscription";
     public static final String VALUE = "value";
-    public static final String RECOMMENDED_FOR_YOU = "Recommended For You";
 
     @FindBy(xpath = "%s")
     protected ExtendedWebElement dynamicXpath;
@@ -871,7 +870,7 @@ public class DisneyPlusApplePageBase extends DisneyAbstractPage implements IRemo
         DisneyLocalizationUtils localizationUtils = (R.CONFIG.get(DEVICE_TYPE).equals(DEVICE_TYPE_TVOS)) ?
                 getAppleTVLocalizationUtils() : getLocalizationUtils();
         dynamicBtnFindByLabel.format(localizationUtils.getDictionaryItem(
-                DisneyDictionaryApi.ResourceKeys.APPLICATION, BTN_ADD_PROFILE_SAVE.getText()))
+                        DisneyDictionaryApi.ResourceKeys.APPLICATION, BTN_ADD_PROFILE_SAVE.getText()))
                 .click();
         Screenshot.capture(getDriver(), ScreenshotType.EXPLICIT_VISIBLE);
     }
@@ -1578,13 +1577,5 @@ public class DisneyPlusApplePageBase extends DisneyAbstractPage implements IRemo
 
     public ExtendedWebElement getThumbnailView() {
         return thumbnailView;
-    }
-
-    public void findCollection(String collection, int countNum) {
-        int count = countNum;
-        while (!getStaticTextByLabel(collection).isPresent(THREE_SEC_TIMEOUT) && count >= 0) {
-            moveDown(1, 2);
-            count--;
-        }
     }
 }
