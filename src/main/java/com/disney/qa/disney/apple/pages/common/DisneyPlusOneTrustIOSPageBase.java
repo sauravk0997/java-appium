ae400b5963623ec0e9d66df5d88bb9575f793127
Saurav <saurav.kumar.-nd@disney.com>
2025-03-26 15:06:49 +0530

    tapping with dimension

--- src/main/java/com/disney/qa/disney/apple/pages/common/DisneyPlusOneTrustIOSPageBase.java
+++ src/main/java/com/disney/qa/disney/apple/pages/common/DisneyPlusOneTrustIOSPageBase.java
@@ -97,7 +97,7 @@ public class DisneyPlusOneTrustIOSPageBase extends DisneyPlusApplePageBase {
     }
 
     public void clickYourCaliforniaPrivacyRightsLink(){
-        clickElementAtLocation(customHyperlinkByLabel.format(californiaPrivacyRights), 10, 75);
+//        clickElementAtLocation(customHyperlinkByLabel.format(californiaPrivacyRights), 10, 75);
 //        ExtendedWebElement element = customHyperlinkByLabel.format(californiaPrivacyRights);
 //        int heightValue = 60;
 //        Dimension dimension = element.getSize();
@@ -109,6 +109,10 @@ public class DisneyPlusOneTrustIOSPageBase extends DisneyPlusApplePageBase {
 //        } else {
 //            element.click();
 //        }
+        ExtendedWebElement element = customHyperlinkByLabel.format(californiaPrivacyRights);
+        var dimension = element.getSize();
+        Point location = element.getLocation();
+        tap(location.getX() , location.getY() + dimension.getHeight());
     }
 
     public boolean isYourCaliforniaPrivacyRightsPageOpened(int timeout){
