a6c869ce3e6eacb4f02b4a65f9164919d3b07bc1
Garima <garima.krishna@disneystreaming.com>
2025-03-27 09:35:45 -0700

    update test

--- src/test/java/com/disney/qa/tests/disney/apple/tvos/regression/search/DisneyPlusAppleTVSearchTests.java
+++ src/test/java/com/disney/qa/tests/disney/apple/tvos/regression/search/DisneyPlusAppleTVSearchTests.java
@@ -40,7 +40,7 @@ public class DisneyPlusAppleTVSearchTests extends DisneyPlusAppleTVBaseTest {
         Assert.assertTrue(searchPage.isOpened(), SEARCH_PAGE_ERROR_MESSAGE);
         searchPage.typeInSearchField(UNENTITLED_HULU_CONTENT);
         System.out.println(searchPage.getSearchBarText());
-        Assert.assertEquals(searchPage.getSearchBarText().equals(UNENTITLED_HULU_CONTENT),
+        Assert.assertEquals(searchPage.getSearchBar().getAttribute("value").equals(UNENTITLED_HULU_CONTENT),
                 "Search text was not displayed in the search field");
     }
 
