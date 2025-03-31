2f3e09457736e10a72c3e800f0f428c5191112fa
hiren <hiren.patel@globant.com>
2025-03-31 11:08:24 +0530

    Add group for AccountSharing, Add TC for plan name verify

---
+++ src/test/java/com/disney/qa/tests/disney/apple/tvos/regression/settings/DisneyPlusAppleTVAccountTests.java
@@ -0,0 +1,29 @@
+package com.disney.qa.tests.disney.apple.tvos.regression.settings;
+
+import com.disney.dmed.productivity.jocasta.JocastaCarinaAdapter;
+import com.disney.qa.disney.apple.pages.tv.DisneyPlusAppleTVHomePage;
+import com.disney.qa.tests.disney.apple.tvos.DisneyPlusAppleTVBaseTest;
+import com.disney.util.TestGroup;
+import com.zebrunner.agent.core.annotation.TestLabel;
+import org.testng.Assert;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import static com.disney.qa.common.constant.IConstantHelper.US;
+import static com.disney.qa.disney.apple.pages.tv.DisneyPlusAppleTVHomePage.globalNavigationMenu.SETTINGS;
+
+@Listeners(JocastaCarinaAdapter.class)
+public class DisneyPlusAppleTVAccountTests extends DisneyPlusAppleTVBaseTest {
+
+    @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XCDQA-118407"})
+    @Test(groups = {TestGroup.ACCOUNT_SHARING, US})
+    public void verifyPlanNameForExtraMemberAddOnUser() {
+        DisneyPlusAppleTVHomePage homePage = new DisneyPlusAppleTVHomePage(getDriver());
+
+        setAccount(createAccountSharingUnifiedAccounts().getReceivingAccount());
+        logInWithoutHomeCheck(getUnifiedAccount());
+        homePage.moveDownFromHeroTileToBrandTile();
+        homePage.openGlobalNavAndSelectOneMenu(SETTINGS.getText());
+
+    }
+}
