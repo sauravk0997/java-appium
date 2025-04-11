6c2e30af67ed0a3fe5b03c24fce32a26d9279d0b
RUIZV035 <Victoria.Ruiz.Martinez.-ND@disney.com>
2025-04-11 11:28:38 -0600

    added scenario XCDQA-66732

--- src/test/java/com/disney/qa/tests/disney/apple/tvos/regression/deeplinks/DisneyPlusAppleTVDeepLinksTests.java
+++ src/test/java/com/disney/qa/tests/disney/apple/tvos/regression/deeplinks/DisneyPlusAppleTVDeepLinksTests.java
@@ -3,6 +3,7 @@ package com.disney.qa.tests.disney.apple.tvos.regression.deeplinks;
 import com.disney.dmed.productivity.jocasta.JocastaCarinaAdapter;
 import com.disney.qa.api.client.requests.CreateUnifiedAccountProfileRequest;
 import com.disney.qa.disney.apple.pages.tv.DisneyPlusAppleTVCollectionPage;
+import com.disney.qa.disney.apple.pages.tv.DisneyPlusAppleTVHomePage;
 import com.disney.qa.disney.apple.pages.tv.DisneyPlusAppleTVWhoIsWatchingPage;
 import com.disney.qa.tests.disney.apple.tvos.DisneyPlusAppleTVBaseTest;
 import com.disney.util.TestGroup;
@@ -13,8 +14,7 @@ import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import static com.disney.qa.common.constant.DisneyUnifiedOfferPlan.DISNEY_BUNDLE_TRIO_PREMIUM_MONTHLY;
-import static com.disney.qa.common.constant.IConstantHelper.US;
-import static com.disney.qa.common.constant.IConstantHelper.WHOS_WATCHING_NOT_DISPLAYED;
+import static com.disney.qa.common.constant.IConstantHelper.*;
 import static com.disney.qa.disney.apple.pages.common.DisneyPlusApplePageBase.RAYA;
 
 @Listeners(JocastaCarinaAdapter.class)
@@ -55,5 +55,16 @@ public class DisneyPlusAppleTVDeepLinksTests extends DisneyPlusAppleTVBaseTest {
         terminateApp(sessionBundles.get(DISNEY));
         launchDeeplink(R.TESTDATA.get("disney_prod_home_page_deeplink"));
         Assert.assertTrue(whoIsWatchingPage.isOpened(), WHOS_WATCHING_NOT_DISPLAYED);
+    }
+
+    @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XCDQA-66732"})
+    @Test(groups = {TestGroup.DEEPLINKS, US})
+    public void verifySingleProfileDeeplinkToHome() {
+        DisneyPlusAppleTVHomePage homePage = new DisneyPlusAppleTVHomePage(getDriver());
+
+        logIn(getUnifiedAccount());
+
+        launchDeeplink(R.TESTDATA.get("disney_prod_home_page_deeplink"));
+        Assert.assertTrue(homePage.isOpened(), HOME_PAGE_NOT_DISPLAYED);
     }
 }
