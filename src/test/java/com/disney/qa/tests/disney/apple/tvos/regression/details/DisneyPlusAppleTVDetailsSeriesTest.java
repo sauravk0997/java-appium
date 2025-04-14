a60e533b92f4025ca79c9e720534e1800f651d92
hiren <hiren.patel@globant.com>
2025-04-14 14:18:12 +0530

    Add step to display controller

--- src/test/java/com/disney/qa/tests/disney/apple/tvos/regression/details/DisneyPlusAppleTVDetailsSeriesTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/tvos/regression/details/DisneyPlusAppleTVDetailsSeriesTest.java
@@ -150,6 +150,7 @@ public class DisneyPlusAppleTVDetailsSeriesTest extends DisneyPlusAppleTVBaseTes
         detailsPage.getTrailerButton().click();
         Assert.assertTrue(videoPlayer.isOpened(), VIDEO_PLAYER_NOT_DISPLAYED);
         videoPlayer.waitForVideoToStart();
+        videoPlayer.clickDown();
         String title = videoPlayer.getTitleLabel();
         Assert.assertTrue(title.contains(trailer) || title.contains(visualsResponse.getTitle()),
                 "Expected Trailer not playing");
