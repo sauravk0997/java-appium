c36601eb545eca9efd7ae2e71786b49f7d975e1d
Saurav <saurav.kumar.-nd@disney.com>
2025-04-15 15:36:23 +0530

    added new condition

--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/ratings/DisneyPlusRatingsBase.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/ratings/DisneyPlusRatingsBase.java
@@ -191,7 +191,7 @@ public class DisneyPlusRatingsBase extends DisneyBaseTest implements IAPIHelper 
         for (String disneyCollectionsID : disneyCollectionsIDs) {
             List<Item> disneyCollectionItems = getExploreAPIItemsFromSet(disneyCollectionsID, locale, language);
             for (Item item : disneyCollectionItems) {
-                if (! item.getVisuals().getPromoLabel().getHeader().contains(comingSoon)) {
+                if ((item.getVisuals().getPromoLabel() == null) || !(item.getVisuals().getPromoLabel().getHeader().contains(comingSoon))) {
                     if (item.getVisuals().getMetastringParts() != null) {
                         if (item.getVisuals().getMetastringParts().getRatingInfo() != null) {
                             if (item.getVisuals().getMetastringParts().getRatingInfo().getRating().getText().equals(rating)) {
