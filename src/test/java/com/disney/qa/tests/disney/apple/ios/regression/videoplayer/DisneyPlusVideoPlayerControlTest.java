d25f88657889db595eb60d4adda057403806dc4f
Blake Watson <blake.watson@bamtechmedia.com>
2025-04-15 08:31:59 -0400

    remainingTimeAfterFastForwardTapInPauseMode

--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/videoplayer/DisneyPlusVideoPlayerControlTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/videoplayer/DisneyPlusVideoPlayerControlTest.java
@@ -70,11 +70,11 @@ public class DisneyPlusVideoPlayerControlTest extends DisneyBaseTest {
                 "Remaining time in pause mode time after Rewind tap " + remainingTimeAfterRwdTapInPauseMode +
                         " is not greater than remaining time before Rewind tap " + remainingTimeInPauseMode);
 
-        videoPlayer.scrubToPlaybackPercentage(SCRUB_PERCENTAGE_TEN);
+        videoPlayer.clickPlayButton();
         remainingTimeInPauseMode = videoPlayer.clickPauseButton().getRemainingTime();
         int remainingTimeAfterFastForwardTapInPauseMode = videoPlayer.tapRewindButton(4).getRemainingTime();
         sa.assertTrue(remainingTimeInPauseMode > remainingTimeAfterFastForwardTapInPauseMode,
-                "Remaining time in pause mode time after Forward tap " + remainingTimeAfterRwdTapInPauseMode +
+                "Remaining time in pause mode time after Forward tap " + remainingTimeAfterFastForwardTapInPauseMode +
                         " is not less than remaining time before Forward tap " + remainingTimeInPauseMode);
 
         sa.assertAll();
