ac8c04f570eaaf271ecc91c5311e095e7b005316
Saurav <saurav.kumar.-nd@disney.com>
2025-04-23 13:06:37 +0530

    added new offer

--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/ratings/DisneyPlusLATAMRatingsTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/ratings/DisneyPlusLATAMRatingsTest.java
@@ -7,6 +7,7 @@ import org.testng.annotations.Test;
 import org.testng.annotations.Listeners;
 
 import static com.disney.qa.common.constant.DisneyUnifiedOfferPlan.DISNEY_PLUS_PREMIUM;
+import static com.disney.qa.common.constant.DisneyUnifiedOfferPlan.DISNEY_PLUS_PREMIUM_YEARLY;
 import static com.disney.qa.common.constant.RatingConstant.Rating.FOURTEEN_PLUS;
 import static com.disney.qa.common.constant.RatingConstant.Rating.SEVEN_PLUS;
 import static com.disney.qa.common.constant.RatingConstant.Rating.SIXTEEN_PLUS;
@@ -22,49 +23,49 @@ public class DisneyPlusLATAMRatingsTest extends DisneyPlusRatingsBase {
     @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-68362"})
     @Test(groups = {TestGroup.RATINGS, TestGroup.RATING_SYSTEM_LATAM, LATAM})
     public void verifyRatingSystemLATAM0() {
-        ratingsSetup(DISNEY_PLUS_PREMIUM, ZERO_PLUS.getContentRating(), getCountry());
+        ratingsSetup(DISNEY_PLUS_PREMIUM_YEARLY, ZERO_PLUS.getContentRating(), getCountry());
         confirmRegionalRatingsDisplays(ZERO_PLUS.getContentRating());
     }
 
     @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-73172"})
     @Test(groups = {TestGroup.RATINGS, TestGroup.RATING_SYSTEM_LATAM, LATAM})
     public void verifyRatingSystemLATAM7() {
-        ratingsSetup(DISNEY_PLUS_PREMIUM, SEVEN_PLUS.getContentRating(), getCountry());
+        ratingsSetup(DISNEY_PLUS_PREMIUM_YEARLY, SEVEN_PLUS.getContentRating(), getCountry());
         confirmRegionalRatingsDisplays(SEVEN_PLUS.getContentRating());
     }
 
     @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-73173"})
     @Test(groups = {TestGroup.RATINGS, TestGroup.RATING_SYSTEM_LATAM, LATAM})
     public void verifyRatingSystemLATAM10() {
-        ratingsSetup(DISNEY_PLUS_PREMIUM, TEN_PLUS.getContentRating(), getCountry());
+        ratingsSetup(DISNEY_PLUS_PREMIUM_YEARLY, TEN_PLUS.getContentRating(), getCountry());
         confirmRegionalRatingsDisplays(TEN_PLUS.getContentRating());
     }
 
     @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-73174"})
     @Test(groups = {TestGroup.RATINGS, TestGroup.RATING_SYSTEM_LATAM, LATAM})
     public void verifyRatingSystemLATAM12() {
-        ratingsSetup(DISNEY_PLUS_PREMIUM, TWELVE_PLUS.getContentRating(), getCountry());
+        ratingsSetup(DISNEY_PLUS_PREMIUM_YEARLY, TWELVE_PLUS.getContentRating(), getCountry());
         confirmRegionalRatingsDisplays(TWELVE_PLUS.getContentRating());
     }
 
     @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-73175"})
     @Test(groups = {TestGroup.RATINGS, TestGroup.RATING_SYSTEM_LATAM, LATAM})
     public void verifyRatingSystemLATAM13() {
-        ratingsSetup(DISNEY_PLUS_PREMIUM, THIRTEEN_PLUS.getContentRating(), getCountry());
+        ratingsSetup(DISNEY_PLUS_PREMIUM_YEARLY, THIRTEEN_PLUS.getContentRating(), getCountry());
         confirmRegionalRatingsDisplays(THIRTEEN_PLUS.getContentRating());
     }
 
     @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-73176"})
     @Test(groups = {TestGroup.RATINGS, TestGroup.RATING_SYSTEM_LATAM, LATAM})
     public void verifyRatingSystemLATAM14() {
-        ratingsSetup(DISNEY_PLUS_PREMIUM, FOURTEEN_PLUS.getContentRating(), getCountry());
+        ratingsSetup(DISNEY_PLUS_PREMIUM_YEARLY, FOURTEEN_PLUS.getContentRating(), getCountry());
         confirmRegionalRatingsDisplays(FOURTEEN_PLUS.getContentRating());
     }
 
     @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-73177"})
     @Test(groups = {TestGroup.RATINGS, TestGroup.RATING_SYSTEM_LATAM, LATAM})
     public void verifyRatingSystemLATAM16() {
-        ratingsSetup(DISNEY_PLUS_PREMIUM, SIXTEEN_PLUS.getContentRating(), getCountry());
+        ratingsSetup(DISNEY_PLUS_PREMIUM_YEARLY, SIXTEEN_PLUS.getContentRating(), getCountry());
         confirmRegionalRatingsDisplays(SIXTEEN_PLUS.getContentRating());
     }
 }
