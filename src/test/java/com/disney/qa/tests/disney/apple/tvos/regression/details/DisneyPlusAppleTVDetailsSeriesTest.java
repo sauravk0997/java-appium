d2553293b44c80b2c0eb1acefbd76d5b3b493efa
hiren <hiren.patel@globant.com>
2025-04-25 15:15:16 +0530

    Add TC to verify see all episode button

--- src/test/java/com/disney/qa/tests/disney/apple/tvos/regression/details/DisneyPlusAppleTVDetailsSeriesTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/tvos/regression/details/DisneyPlusAppleTVDetailsSeriesTest.java
@@ -339,6 +339,32 @@ public class DisneyPlusAppleTVDetailsSeriesTest extends DisneyPlusAppleTVBaseTes
                 "Playback is not initiated for expected episode");
     }
 
+    @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XCDQA-112653"})
+    @Test(groups = {TestGroup.VIDEO_PLAYER, TestGroup.UP_NEXT, US})
+    public void verifySeriesUpNextSeeAllEpisodesCTA() {
+        DisneyPlusAppleTVHomePage homePage = new DisneyPlusAppleTVHomePage(getDriver());
+        DisneyPlusAppleTVVideoPlayerPage videoPlayer = new DisneyPlusAppleTVVideoPlayerPage(getDriver());
+        DisneyPlusAppleTVCommonPage commonPage = new DisneyPlusAppleTVCommonPage(getDriver());
+        DisneyPlusAppleTVUpNextPage upNextPage = new DisneyPlusAppleTVUpNextPage(getDriver());
+        DisneyPlusAppleTVDetailsPage detailsPage = new DisneyPlusAppleTVDetailsPage(getDriver());
+        logIn(getUnifiedAccount());
+
+        // Play first episode
+        homePage.waitForHomePageToOpen();
+        launchDeeplink(R.TESTDATA.get("disney_prod_series_bluey_mini_episodes_playback_deeplink"));
+        Assert.assertTrue(videoPlayer.isOpened(), VIDEO_PLAYER_NOT_DISPLAYED);
+        videoPlayer.waitForVideoToStart();
+        // Scrub to the end and start next episode
+        commonPage.clickRight(6, 1, 1);
+        videoPlayer.waitForPresenceOfAnElement(upNextPage.getSeeDetailsButton());
+        LOGGER.info("Debugger " + getDriver().getPageSource());
+        commonPage.moveRightUntilElementIsFocused(upNextPage.getSeeDetailsButton(), 6);
+        Assert.assertTrue(upNextPage.getSeeDetailsButton().isPresent(), "See All Episode CTA is not displayed");
+        upNextPage.getSeeDetailsButton().click();
+        detailsPage.waitForDetailsPageToOpen();
+        Assert.assertTrue(detailsPage.isOpened(), DETAILS_PAGE_NOT_DISPLAYED);
+    }
+
     @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XCDQA-64885"})
     @Test(groups = {TestGroup.DETAILS_PAGE, TestGroup.SERIES, US})
     public void verifyBookmarkSeriesDetailsAppearance() {
