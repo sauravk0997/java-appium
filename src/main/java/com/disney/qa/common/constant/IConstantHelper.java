d8e6f9ba57569206ea0106a233f71aea46d79653
Juan Castiblanco <Juan.X.Castiblanco.-ND@disney.com>
2025-04-29 17:21:05 -0500

    add warning suppression

--- src/main/java/com/disney/qa/common/constant/IConstantHelper.java
+++ src/main/java/com/disney/qa/common/constant/IConstantHelper.java
@@ -4,6 +4,7 @@ import com.disney.qa.api.explore.request.ExploreSearchRequest;
 
 import java.util.EnumSet;
 
+@SuppressWarnings("squid:S1214")
 public interface IConstantHelper {
     EnumSet<ExploreSearchRequest.ContentEntitlement> CONTENT_ENTITLEMENT_DISNEY =
             EnumSet.of(ExploreSearchRequest.ContentEntitlement.DISNEY_PLUS_BASE);
