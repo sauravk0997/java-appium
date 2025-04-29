8df6448c2d95a58e005b8bc0968bb250b7c02cc0
csolmaz <cristina.solmaz@hulu.com>
2025-04-29 14:58:13 -0700

    revert

--- src/main/java/com/disney/qa/disney/apple/pages/tv/DisneyPlusAppleTVChangePasswordPage.java
+++ src/main/java/com/disney/qa/disney/apple/pages/tv/DisneyPlusAppleTVChangePasswordPage.java
@@ -7,15 +7,10 @@ import com.zebrunner.carina.webdriver.ScreenshotType;
 import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
 import com.zebrunner.carina.webdriver.locator.ExtendedFindBy;
 import org.openqa.selenium.WebDriver;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.lang.invoke.MethodHandles;
 
 @SuppressWarnings("squid:MaximumInheritanceDepth")
 @DeviceType(pageType = DeviceType.Type.APPLE_TV, parentClass = DisneyPlusChangePasswordIOSPageBase.class)
 public class DisneyPlusAppleTVChangePasswordPage extends DisneyPlusChangePasswordIOSPageBase {
-    protected static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     public DisneyPlusAppleTVChangePasswordPage(WebDriver driver) {super(driver);}
 
     @ExtendedFindBy(accessibilityId = "secureTextFieldPassword")
@@ -35,8 +30,6 @@ public class DisneyPlusAppleTVChangePasswordPage extends DisneyPlusChangePasswor
     }
 
     public void clickSave() {
-        LOGGER.info("Print out:" + getDriver().getPageSource());
-        waitForPresenceOfAnElement(primaryButton);
         primaryButton.click();
     }
 }
