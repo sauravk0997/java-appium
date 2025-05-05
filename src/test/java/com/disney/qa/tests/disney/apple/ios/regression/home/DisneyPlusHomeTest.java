b7b7da0ce4d31c9748175a5c298f7d14b82d5f9e
maguilera <maxe.aguilera.-nd@disney.com>
2025-05-05 09:17:45 -0300

    increasing the expected remaining time value

--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/home/DisneyPlusHomeTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/home/DisneyPlusHomeTest.java
@@ -666,7 +666,7 @@ public class DisneyPlusHomeTest extends DisneyBaseTest {
     @Test(groups = {TestGroup.HOME, TestGroup.PRE_CONFIGURATION, US})
     public void verifyContinueWatchingWhenBookmarkLessThanOneMin() {
         int swipeCount = 5;
-        int expectedRemainingTimeInSec = 30;
+        int expectedRemainingTimeInSec = 70;
         String lessThanOneMinMessage = "Less than 1m remaining";
         DisneyPlusHomeIOSPageBase homePage = initPage(DisneyPlusHomeIOSPageBase.class);
         DisneyPlusWhoseWatchingIOSPageBase whoIsWatching = initPage(DisneyPlusWhoseWatchingIOSPageBase.class);
