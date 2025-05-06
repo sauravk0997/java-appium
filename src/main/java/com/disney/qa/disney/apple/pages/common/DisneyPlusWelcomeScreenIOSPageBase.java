7ef73178c142923919c74777030ac1b5ff0de8a1
Blake Watson <blake.watson@bamtechmedia.com>
2025-05-06 09:49:49 -0400

    code cleanup

--- src/main/java/com/disney/qa/disney/apple/pages/common/DisneyPlusWelcomeScreenIOSPageBase.java
+++ src/main/java/com/disney/qa/disney/apple/pages/common/DisneyPlusWelcomeScreenIOSPageBase.java
@@ -4,7 +4,6 @@ import com.disney.qa.api.dictionary.DisneyDictionaryApi;
 import com.disney.qa.disney.dictionarykeys.DictionaryKeys;
 import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
 import com.zebrunner.carina.webdriver.locator.ExtendedFindBy;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 @SuppressWarnings("squid:MaximumInheritanceDepth")
@@ -17,33 +16,6 @@ public class DisneyPlusWelcomeScreenIOSPageBase extends DisneyPlusApplePageBase 
 
     @ExtendedFindBy(accessibilityId = "loginButton")
     protected ExtendedWebElement loginButton;
-
-    @ExtendedFindBy(accessibilityId = "dismissButton")
-    private ExtendedWebElement logOutButton;
-
-    @ExtendedFindBy(accessibilityId = "customButton")
-    private ExtendedWebElement completeSubscriptionButton;
-
-    @ExtendedFindBy(accessibilityId = "Cancel")
-    private ExtendedWebElement paywallCancelButton;
-
-    @ExtendedFindBy(iosClassChain = "**/XCUIElementTypeButton[`name CONTAINS \"monthly\"`]")
-    private ExtendedWebElement monthlySubButton;
-
-    @ExtendedFindBy(iosClassChain = "**/XCUIElementTypeButton[`name CONTAINS \"yearly\"`]")
-    private ExtendedWebElement yearlySubButton;
-
-    @ExtendedFindBy(accessibilityId = "restoreButton")
-    private ExtendedWebElement restoreButton;
-
-    @ExtendedFindBy(accessibilityId = "imageLogo")
-    private ExtendedWebElement backgroundImage;
-
-    @ExtendedFindBy(accessibilityId = "Disney Plus Logo")
-    private ExtendedWebElement disneyPlusLogo;
-
-    @ExtendedFindBy(accessibilityId = "Don’t Allow")
-    private ExtendedWebElement dontAllowbtn;
 
     private ExtendedWebElement forceUpdateTitle = getStaticTextByLabel(getLocalizationUtils().getDictionaryItem(
                 DisneyDictionaryApi.ResourceKeys.APPLICATION,
@@ -77,34 +49,9 @@ public class DisneyPlusWelcomeScreenIOSPageBase extends DisneyPlusApplePageBase 
         return loginButton.isPresent();
     }
 
-    public boolean isCompleteSubscriptionButtonDisplayed() {
-        return completeSubscriptionButton.isElementPresent();
-    }
-
     public void clickLogInButton() {
         handleSystemAlert(AlertButtonCommand.DISMISS, 2);
         loginButton.click();
-    }
-
-    public void clickSignUpButton() {
-        handleSystemAlert(AlertButtonCommand.DISMISS, 2);
-        signUpButton.click();
-    }
-
-    public void clickCompleteSubscriptionButton() {
-        completeSubscriptionButton.click();
-    }
-
-    public By getSignUpButtonBy() {
-        return signUpButton.getBy();
-    }
-
-    public ExtendedWebElement getSignupButton() {
-        return signUpButton;
-    }
-
-    public void clickDontAllowBtn() {
-        dontAllowbtn.clickIfPresent();
     }
 
     public void clickForceUpdateTitle() {
