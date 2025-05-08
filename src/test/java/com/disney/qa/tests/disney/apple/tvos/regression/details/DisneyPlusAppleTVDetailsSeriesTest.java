d712bb51ecf062e65009cc04df53ffb4cfe6d9f4
hiren <hiren.patel@globant.com>
2025-05-08 13:45:40 +0530

    Add step to background app by presing menu

--- src/test/java/com/disney/qa/tests/disney/apple/tvos/regression/details/DisneyPlusAppleTVDetailsSeriesTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/tvos/regression/details/DisneyPlusAppleTVDetailsSeriesTest.java
@@ -804,8 +804,8 @@ public class DisneyPlusAppleTVDetailsSeriesTest extends DisneyPlusAppleTVBaseTes
         logIn(getUnifiedAccount());
         homePage.waitForHomePageToOpen();
         navigateToUpNextOnVideoPlayer(R.TESTDATA.get("disney_prod_series_bluey_mini_episodes_playback_deeplink"));
-        commonPage.clickHome();
-        commonPage.clickMenu();
+        //commonPage.clickHome();
+        commonPage.clickMenu(5);
         LOGGER.info("Launching app again");
         startApp(sessionBundles.get(DISNEY));
         Assert.assertTrue(detailsPage.isOpened(), DETAILS_PAGE_NOT_DISPLAYED);
