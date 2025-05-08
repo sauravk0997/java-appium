4f8d9ae4e5f5596e1e372f25508edbb6da5f2f66
Saurav <saurav.kumar.-nd@disney.com>
2025-05-08 09:23:08 +0530

    addressed review comments

--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/details/DisneyPlusDetailsSeriesTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/details/DisneyPlusDetailsSeriesTest.java
@@ -42,7 +42,6 @@ public class DisneyPlusDetailsSeriesTest extends DisneyBaseTest {
     private static final String DISNEY_JUNIOR_ARIEL = "Disney Junior Ariel";
     private static final String PLAY = "Play";
     private static final String DOWNLOAD_MODAL_IS_VISIBLE = "Download Modal was still visible";
-    private static final String BLUEY = "Bluey";
 
     @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-67401"})
     @Test(groups = {TestGroup.DETAILS_PAGE, TestGroup.SERIES, TestGroup.PRE_CONFIGURATION, US})
@@ -895,8 +894,8 @@ public class DisneyPlusDetailsSeriesTest extends DisneyBaseTest {
 
         setAppToHomeScreen(getUnifiedAccount(), JUNIOR_PROFILE);
         homePage.clickSearchIcon();
-        searchPage.searchForMedia(BLUEY);
-        searchPage.getDynamicAccessibilityId(BLUEY).click();
+        searchPage.searchForMedia(SERIES_BLUEY);
+        searchPage.getDynamicAccessibilityId(SERIES_BLUEY).click();
         Assert.assertTrue(detailsPage.waitForDetailsPageToOpen(), DETAILS_PAGE_NOT_DISPLAYED);
 
         String entityID = R.TESTDATA.get("disney_prod_series_bluey_entity_id");
