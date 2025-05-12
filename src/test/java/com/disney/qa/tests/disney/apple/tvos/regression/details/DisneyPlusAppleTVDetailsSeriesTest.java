5a72fcc44434b00a08f19b5bf450faa0d3f3dae8
RUIZV035 <Victoria.Ruiz.Martinez.-ND@disney.com>
2025-05-12 16:24:01 -0600

    changed wait

--- src/test/java/com/disney/qa/tests/disney/apple/tvos/regression/details/DisneyPlusAppleTVDetailsSeriesTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/tvos/regression/details/DisneyPlusAppleTVDetailsSeriesTest.java
@@ -835,8 +835,7 @@ public class DisneyPlusAppleTVDetailsSeriesTest extends DisneyPlusAppleTVBaseTes
         // Play first episode
         homePage.waitForHomePageToOpen();
         navigateToUpNextOnVideoPlayer(R.TESTDATA.get("disney_prod_series_bluey_mini_episodes_playback_deeplink"));
-        videoPlayer.waitForElementToDisappear(upNextPage.getSeeAllEpisodesButton(), SIXTY_SEC_TIMEOUT);
-        Assert.assertTrue(videoPlayer.isOpened(), VIDEO_PLAYER_NOT_DISPLAYED);
+        videoPlayer.waitForVideoToStart();
         videoPlayer.clickDown();
         Assert.assertTrue(videoPlayer.getStaticTextByLabelContains(nextEpisodeTitle).isPresent(),
                 "Playback is not initiated for expected episode");
