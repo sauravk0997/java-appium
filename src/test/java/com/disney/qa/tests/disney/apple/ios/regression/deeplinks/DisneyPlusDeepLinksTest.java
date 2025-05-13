cedea3d6f5ffa55e7be487f7f42fd3be2e8f9f35
Juan Castiblanco <Juan.X.Castiblanco.-ND@disney.com>
2025-05-13 15:52:48 -0500

    add test for XMOBQA-67539

--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/deeplinks/DisneyPlusDeepLinksTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/deeplinks/DisneyPlusDeepLinksTest.java
@@ -644,4 +644,18 @@ public class DisneyPlusDeepLinksTest extends DisneyBaseTest {
                 String.format("Expected editorial/franchise collection page '%s' did not open",
                         expectedCollectionPageTitle));
     }
+
+    @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-67539"})
+    @Test(groups = {TestGroup.DEEPLINKS, TestGroup.PRE_CONFIGURATION, US})
+    public void verifyDeepLinkToOriginals() {
+        String originalsPageTitle = "Originals";
+        DisneyPlusHomeIOSPageBase homePage = initPage(DisneyPlusHomeIOSPageBase.class);
+        DisneyPlusCollectionIOSPageBase collectionPage = initPage(DisneyPlusCollectionIOSPageBase.class);
+
+        setAppToHomeScreen(getUnifiedAccount());
+        homePage.waitForHomePageToOpen();
+
+        launchDeeplink(R.TESTDATA.get("disney_prod_originals_deeplink"));
+        Assert.assertTrue(collectionPage.isOpened(originalsPageTitle), "Originals page did not open");
+    }
 }
