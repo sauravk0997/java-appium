7448e76843dca527d9eb08d89dfcb7a66c362f00
RUIZV035 <Victoria.Ruiz.Martinez.-ND@disney.com>
2025-05-12 18:57:50 -0600

    added steps to validate profile and home

--- src/test/java/com/disney/qa/tests/disney/apple/tvos/regression/details/DisneyPlusAppleTVDetailsSeriesTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/tvos/regression/details/DisneyPlusAppleTVDetailsSeriesTest.java
@@ -853,8 +853,7 @@ public class DisneyPlusAppleTVDetailsSeriesTest extends DisneyPlusAppleTVBaseTes
         editProfilePage.toggleAutoplayButton(toogleValue);
         homePage.clickMenuTimes(1, 1);
         Assert.assertTrue(whoIsWatchingPage.isOpened(), WHOS_WATCHING_NOT_DISPLAYED);
-        homePage.clickUp();
-        homePage.clickSelect();
+        homePage.clickMenuTimes(1, 1);
         Assert.assertTrue(homePage.isOpened(), HOME_PAGE_NOT_DISPLAYED);
     }
 
