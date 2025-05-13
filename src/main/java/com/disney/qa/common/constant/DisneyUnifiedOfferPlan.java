5e5d67bbcdfa26e38f8d61eedc306c363fd71f14
Blake Watson <blake.watson@bamtechmedia.com>
2025-05-13 08:52:23 -0400

    edit

--- src/main/java/com/disney/qa/common/constant/DisneyUnifiedOfferPlan.java
+++ src/main/java/com/disney/qa/common/constant/DisneyUnifiedOfferPlan.java
@@ -9,7 +9,7 @@ public enum DisneyUnifiedOfferPlan {
     DISNEY_BUNDLE_TRIO_PREMIUM_MONTHLY("Disney Bundle Trio Premium - 26.99 USD - Monthly"),
     DISNEY_PREMIUM_MONTHLY_AMAZON("Disney Plus Premium - Monthly - Amazon"),
     DISNEY_PREMIUM_MONTHLY_ROKU("Promotion - Disney+ Premium Monthly - 13.99 USD - 7 Day FT Roku ISU"),
-    
+
     //Disney plans in non US countries
     DISNEY_PLUS_STANDARD("Disney+ Standard"),
     DISNEY_PLUS_STANDARD_WITH_ADS_DE("Disney+ Standard with Ads"),
