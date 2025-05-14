c12a8dd1302adf78c1a6c51437699397a08aa51a
Saurav <saurav.kumar.-nd@disney.com>
2025-05-14 20:38:20 +0530

    removed some step

--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/videoplayer/DisneyPlusVideoPlayerControlTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/videoplayer/DisneyPlusVideoPlayerControlTest.java
@@ -74,9 +74,7 @@ public class DisneyPlusVideoPlayerControlTest extends DisneyBaseTest {
         videoPlayer.clickPlayButton();
         videoPlayer.waitForVideoToStart();
         videoPlayer.waitForVideoControlToDisappear();
-        videoPlayer.clickPauseButton();
-        videoPlayer.waitForVideoControlToDisappear();
-        remainingTimeInPauseMode = videoPlayer.getRemainingTime();
+        remainingTimeInPauseMode = videoPlayer.clickPauseButton().getRemainingTime();
         int remainingTimeAfterForwardTapInPauseMode = videoPlayer.tapForwardButton(2).getRemainingTime();
         videoPlayer.waitForVideoControlToDisappear();
         sa.assertTrue(remainingTimeAfterForwardTapInPauseMode <= remainingTimeInPauseMode - 20,
