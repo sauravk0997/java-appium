03c89c388a96631b7d209359940c8dfafb99a128
Saurav <saurav.kumar.-nd@disney.com>
2025-05-15 15:48:32 +0530

    added new forward func

--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/videoplayer/DisneyPlusVideoPlayerControlTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/videoplayer/DisneyPlusVideoPlayerControlTest.java
@@ -75,14 +75,15 @@ public class DisneyPlusVideoPlayerControlTest extends DisneyBaseTest {
         videoPlayer.waitForVideoToStart();
         videoPlayer.waitForVideoControlToDisappear();
         remainingTimeInPauseMode = videoPlayer.clickPauseButton().getRemainingTime();
-        videoPlayer.tapForwardButton(2);
-        videoPlayer.waitForVideoControlToDisappear();
+//        videoPlayer.tapForwardButton(2);
+        videoPlayer.tapPlayerScreen(PlayerControl.FAST_FORWARD,4);
+//        videoPlayer.waitForVideoControlToDisappear();
         int remainingTimeAfterForwardTapInPauseMode = videoPlayer.getRemainingTime();
         sa.assertTrue(remainingTimeAfterForwardTapInPauseMode <= remainingTimeInPauseMode - 20,
                 "Remaining time in pause mode after Forward tap " + remainingTimeAfterForwardTapInPauseMode +
                         " is not at least 20 seconds less than before " + remainingTimeInPauseMode);
 
-
+//        tapPlayerScreen
         sa.assertAll();
     }
 
