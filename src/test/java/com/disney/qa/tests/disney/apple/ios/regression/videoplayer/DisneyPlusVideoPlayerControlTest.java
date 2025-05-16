c7f6b8cc9ee51358bc9f4b952c5ca697ee4292ca
Saurav <saurav.kumar.-nd@disney.com>
2025-05-16 16:14:30 +0530

    fix

--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/videoplayer/DisneyPlusVideoPlayerControlTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/videoplayer/DisneyPlusVideoPlayerControlTest.java
@@ -418,10 +418,10 @@ public class DisneyPlusVideoPlayerControlTest extends DisneyBaseTest {
         // Wait for video controls to disappear and click on the screen to validate FX logo is not present
         videoPlayer.waitForVideoControlToDisappear();
         videoPlayer.clickElementAtLocation(videoPlayer.getPlayerView(), 10, 50);
-        Assert.assertFalse(videoPlayer.getTypeOtherContainsLabel(network).isPresent(5), "Network watermark is present");
+        sa.assertFalse(videoPlayer.getTypeOtherContainsLabel(network).isElementPresent(2), "Network watermark is present");
         // Wait for network watermark to disappear and validate time after stop timer
         videoPlayer.waitForVideoControlToDisappear();
-        Assert.assertTrue(videoPlayer.waitForNetworkWatermarkLogoToDisappear(network), "Watermark network is present");
+        sa.assertTrue(videoPlayer.waitForNetworkWatermarkLogoToDisappear(network), "Watermark network is present");
         stopWatch.stop();
         long totalTime = stopWatch.getTime(TimeUnit.SECONDS);
         LOGGER.info("totalTime {}, minimumNetworkEpisodeLogoDuration {}", totalTime, minimumNetworkEpisodeLogoDuration);
