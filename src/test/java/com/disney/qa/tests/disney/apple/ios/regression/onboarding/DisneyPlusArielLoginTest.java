79dd8cdc679a9790eb8812db508c7b88e1b3273d
Juan Castiblanco <Juan.X.Castiblanco.-ND@disney.com>
2025-05-16 21:47:00 -0500

    add test for XMOBQA-72170

--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/onboarding/DisneyPlusArielLoginTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/onboarding/DisneyPlusArielLoginTest.java
@@ -6,15 +6,16 @@ import com.disney.qa.disney.apple.pages.common.*;
 import com.disney.qa.tests.disney.apple.ios.DisneyBaseTest;
 import com.disney.util.TestGroup;
 import com.zebrunner.agent.core.annotation.TestLabel;
+import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
 import org.testng.Assert;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
-import java.util.*;
-
 import static com.disney.qa.common.constant.DisneyUnifiedOfferPlan.*;
 import static com.disney.qa.common.constant.IConstantHelper.*;
+import static com.disney.qa.tests.disney.apple.ios.regression.onboarding.DisneyPlusLoginTest.COMPLETE_PROFILE_PAGE_NOT_DISPLAYED;
+import static com.disney.qa.disney.apple.pages.common.DisneyPlusEditGenderIOSPageBase.GenderOption;
 
 @Listeners(JocastaCarinaAdapter.class)
 public class DisneyPlusArielLoginTest extends DisneyBaseTest {
@@ -116,5 +117,69 @@ public class DisneyPlusArielLoginTest extends DisneyBaseTest {
         oneTimePasscodePage.clickPrimaryButton();
         handleGenericPopup(5,1);
         Assert.assertTrue(homePage.isOpened(), HOME_PAGE_NOT_DISPLAYED);
+    }
+
+    @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XMOBQA-72170"})
+    @Test(groups = {TestGroup.ONBOARDING, TestGroup.LOG_IN, TestGroup.PRE_CONFIGURATION, US})
+    public void testProfileGenderSelection() {
+        SoftAssert sa = new SoftAssert();
+        DisneyPlusWelcomeScreenIOSPageBase welcomeScreenPage = initPage(DisneyPlusWelcomeScreenIOSPageBase.class);
+        DisneyPlusLoginIOSPageBase loginPage = initPage(DisneyPlusLoginIOSPageBase.class);
+        DisneyPlusPasswordIOSPageBase passwordPage = initPage(DisneyPlusPasswordIOSPageBase.class);
+        DisneyPlusEdnaDOBCollectionPageBase ednaDobCollectionPage = initPage(DisneyPlusEdnaDOBCollectionPageBase.class);
+        DisneyPlusAddProfileIOSPageBase addProfilePage = initPage(DisneyPlusAddProfileIOSPageBase.class);
+        DisneyPlusEditGenderIOSPageBase editGenderPage = initPage(DisneyPlusEditGenderIOSPageBase.class);
+        DisneyPlusHomeIOSPageBase homePage = initPage(DisneyPlusHomeIOSPageBase.class);
+        DisneyPlusMoreMenuIOSPageBase moreMenuPage = initPage(DisneyPlusMoreMenuIOSPageBase.class);
+        DisneyPlusEditProfileIOSPageBase editProfile = initPage(DisneyPlusEditProfileIOSPageBase.class);
+
+        // Create Disney account without DOB and Gender
+        setAccount(getUnifiedAccountApi().createAccount(
+                getDefaultCreateUnifiedAccountRequest()
+                        .setDateOfBirth(null)
+                        .setGender(null)
+                        .setPartner(Partner.DISNEY)
+                        .setCountry(getLocalizationUtils().getLocale())
+                        .setAddDefaultEntitlement(true)
+                        .setLanguage(getLocalizationUtils().getUserLanguage())));
+
+        welcomeScreenPage.clickLogInButton();
+        loginPage.submitEmail(getUnifiedAccount().getEmail());
+        passwordPage.submitPasswordForLogin(getUnifiedAccount().getUserPass());
+
+        Assert.assertTrue(ednaDobCollectionPage.isOpened(), EDNA_DOB_COLLECTION_PAGE_NOT_DISPLAYED);
+        ednaDobCollectionPage.enterDOB(Person.ADULT.getMonth(), Person.ADULT.getDay(), Person.ADULT.getYear());
+        ednaDobCollectionPage.tapSaveAndContinueButton();
+
+        Assert.assertTrue(addProfilePage.isGenderFieldPresent(), COMPLETE_PROFILE_PAGE_NOT_DISPLAYED);
+        addProfilePage.tapSaveButton();
+        Assert.assertTrue(addProfilePage.isInlineErrorForGenderFieldPresent(),
+                "Inline error for Gender field is not present");
+
+        GenderOption[] genders = GenderOption.values();
+        for (GenderOption gender : genders) {
+            addProfilePage.getGenderDropdown().click();
+            String currentGenderValue = editGenderPage.selectGender(gender);
+            ExtendedWebElement genderButton = editGenderPage.getTypeButtonByLabel(currentGenderValue);
+            sa.assertTrue(genderButton.isPresent(),
+                    String.format("Expected '%s' gender option is not present", gender));
+            genderButton.click();
+            String dropdownSelectedGenderValue = addProfilePage.getDropdownSelectedGender();
+            sa.assertEquals(addProfilePage.getDropdownSelectedGender(), currentGenderValue,
+                    String.format("Dropdown selected gender '%s' didn't match expected gender '%s'",
+                            dropdownSelectedGenderValue, currentGenderValue));
+        }
+        addProfilePage.tapSaveButton();
+
+        // Dismiss 'Add another profile' banner
+        homePage.clickSecondaryButton();
+        Assert.assertTrue(homePage.isOpened(), HOME_PAGE_NOT_DISPLAYED);
+
+        homePage.clickMoreTab();
+        moreMenuPage.clickEditProfilesBtn();
+        editProfile.clickEditModeProfile(getUnifiedAccount().getFirstName());
+        String lastGenderValue = editGenderPage.selectGender(genders[genders.length - 1]);
+        Assert.assertEquals(editProfile.getGenderValue(), lastGenderValue,
+                "Profile gender didn't match selected gender");
     }
 }
