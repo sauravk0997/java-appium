5c164a2c446c11315182d1b0f4e5a48b222a2169
hiren <hiren.patel@globant.com>
2025-05-26 12:44:40 +0530

    Add TC to verify ESPN Brand tile

--- src/test/java/com/disney/qa/tests/disney/apple/tvos/regression/home/DisneyPlusAppleTVHomeTests.java
+++ src/test/java/com/disney/qa/tests/disney/apple/tvos/regression/home/DisneyPlusAppleTVHomeTests.java
@@ -37,6 +37,25 @@ import static com.disney.qa.common.constant.IConstantHelper.*;
 @Listeners(JocastaCarinaAdapter.class)
 public class DisneyPlusAppleTVHomeTests extends DisneyPlusAppleTVBaseTest {
 
+    @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XCDQA-121942"})
+    @Test(groups = {TestGroup.HOME, US})
+    public void verifyESPNBrandTiles() {
+        DisneyPlusAppleTVHomePage homePage = new DisneyPlusAppleTVHomePage(getDriver());
+        DisneyPlusAppleTVBrandsPage brandPage = new DisneyPlusAppleTVBrandsPage(getDriver());
+        setAccount(getUnifiedAccountApi().createAccount(getCreateUnifiedAccountRequest(DISNEY_BUNDLE_TRIO_PREMIUM_MONTHLY)));
+        logIn(getUnifiedAccount());
+
+        homePage.waitForHomePageToOpen();
+        Assert.assertTrue(homePage.getBrandCell(brandPage.getBrand(DisneyPlusAppleTVBrandsPage.Brand.ESPN)).isPresent(),
+                "ESPN brand tile was not present on home page screen");
+
+        homePage.moveDownFromHeroTileToBrandTile();
+        homePage.clickBrandTile(brandPage.getBrand(DisneyPlusAppleTVBrandsPage.Brand.ESPN));
+        Assert.assertTrue(
+                brandPage.isBrandScreenDisplayed(brandPage.getBrand(DisneyPlusAppleTVBrandsPage.Brand.ESPN)),
+                ESPN_PAGE_DID_NOT_OPEN);
+    }
+
     @TestLabel(name = ZEBRUNNER_XRAY_TEST_KEY, value = {"XCDQA-121503"})
     @Test(groups = {TestGroup.HOME, TestGroup.HULU, US})
     public void verifyStandaloneESPNAndHuluBrandTiles() {
