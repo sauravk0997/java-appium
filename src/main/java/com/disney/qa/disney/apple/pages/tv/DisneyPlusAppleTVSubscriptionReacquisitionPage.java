d7a71d4c590746ba07783b97fe95262b8776678c
Juan Castiblanco <Juan.X.Castiblanco.-ND@disney.com>
2025-05-30 12:41:02 -0500

    apply suggested changes

--- src/main/java/com/disney/qa/disney/apple/pages/tv/DisneyPlusAppleTVSubscriptionReacquisitionPage.java
+++ src/main/java/com/disney/qa/disney/apple/pages/tv/DisneyPlusAppleTVSubscriptionReacquisitionPage.java
@@ -1,18 +1,38 @@
 package com.disney.qa.disney.apple.pages.tv;
 
 import com.disney.qa.api.dictionary.DisneyDictionaryApi;
-import com.disney.qa.disney.apple.pages.common.DisneyPlusSubscriptionReacquisitionIOSPageBase;
+import com.disney.qa.disney.apple.pages.common.DisneyPlusApplePageBase;
 import com.disney.qa.disney.dictionarykeys.DictionaryKeys;
 import com.zebrunner.carina.utils.factory.DeviceType;
 import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
+import com.zebrunner.carina.webdriver.locator.ExtendedFindBy;
 import org.openqa.selenium.WebDriver;
 
 @SuppressWarnings("squid:MaximumInheritanceDepth")
-@DeviceType(pageType = DeviceType.Type.APPLE_TV, parentClass = DisneyPlusSubscriptionReacquisitionIOSPageBase.class)
-public class DisneyPlusAppleTVSubscriptionReacquisitionPage extends DisneyPlusSubscriptionReacquisitionIOSPageBase {
+@DeviceType(pageType = DeviceType.Type.APPLE_TV, parentClass = DisneyPlusApplePageBase.class)
+public class DisneyPlusAppleTVSubscriptionReacquisitionPage extends DisneyPlusApplePageBase {
+
+    @ExtendedFindBy(iosClassChain = "**/XCUIElementTypeImage[`name == 'paywallLandingComplete'`][2]")
+    private ExtendedWebElement disneyPlusLogo;
 
     public DisneyPlusAppleTVSubscriptionReacquisitionPage(WebDriver driver) {
         super(driver);
+    }
+
+    public ExtendedWebElement getDisneyPlusLogo() {
+        return disneyPlusLogo;
+    }
+
+    public ExtendedWebElement getTitleTextElement() {
+        return staticTextByLabel.format(getLocalizationUtils().getDictionaryItem(
+                DisneyDictionaryApi.ResourceKeys.UNIFIED_COMMERCE_ONBOARDING,
+                DictionaryKeys.CO_ACCOUNT_NO_ENTITLEMENT_HEADER.getText()));
+    }
+
+    public ExtendedWebElement getDescriptionTextElement() {
+        return staticTextByLabel.format(getLocalizationUtils().getDictionaryItem(
+                DisneyDictionaryApi.ResourceKeys.UNIFIED_COMMERCE_ONBOARDING,
+                DictionaryKeys.CO_ACCOUNT_NO_ENTITLEMENT.getText()));
     }
 
     public ExtendedWebElement getJoinNowButton() {
