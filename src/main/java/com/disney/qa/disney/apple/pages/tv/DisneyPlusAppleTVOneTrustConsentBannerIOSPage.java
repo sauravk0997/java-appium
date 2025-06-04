3334f71f4e3fbb236c0a774e35f27a71f159be67
Juan Castiblanco <Juan.X.Castiblanco.-ND@disney.com>
2025-06-03 20:37:06 -0500

    add tvOS "Accept All" button locator

--- src/main/java/com/disney/qa/disney/apple/pages/tv/DisneyPlusAppleTVOneTrustConsentBannerIOSPage.java
+++ src/main/java/com/disney/qa/disney/apple/pages/tv/DisneyPlusAppleTVOneTrustConsentBannerIOSPage.java
@@ -2,11 +2,17 @@ package com.disney.qa.disney.apple.pages.tv;
 
 import com.disney.qa.disney.apple.pages.common.DisneyPlusOneTrustConsentBannerIOSPageBase;
 import com.zebrunner.carina.utils.factory.DeviceType;
+import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
+import com.zebrunner.carina.webdriver.locator.ExtendedFindBy;
 import org.openqa.selenium.WebDriver;
 
 @SuppressWarnings("squid:MaximumInheritanceDepth")
 @DeviceType(pageType = DeviceType.Type.APPLE_TV, parentClass = DisneyPlusOneTrustConsentBannerIOSPageBase.class)
 public class DisneyPlusAppleTVOneTrustConsentBannerIOSPage extends DisneyPlusOneTrustConsentBannerIOSPageBase {
+
+    @ExtendedFindBy(accessibilityId = "bannerAllowAllButton")
+    private ExtendedWebElement tvOsBannerAllowAllButton;
+
     public DisneyPlusAppleTVOneTrustConsentBannerIOSPage (WebDriver driver) {
         super(driver);
     }
