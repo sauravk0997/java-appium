7d6004c58ab41c2b73841111224a3998f8b80e33
RUIZV035 <Victoria.Ruiz.Martinez.-ND@disney.com>
2025-06-10 16:50:56 -0600

    clean code

--- src/test/java/com/disney/qa/tests/disney/apple/tvos/regression/settings/DisneyPlusAppleTVProfilesTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/tvos/regression/settings/DisneyPlusAppleTVProfilesTest.java
@@ -18,8 +18,6 @@ import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
