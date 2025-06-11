5b0dec82da51ec49ac4f7089bff4ad12b5ec121f
RUIZV035 <Victoria.Ruiz.Martinez.-ND@disney.com>
2025-06-11 12:12:12 -0600

    testing AU

--- src/test/java/com/disney/qa/tests/disney/apple/DisneyAppleBaseTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/DisneyAppleBaseTest.java
@@ -720,7 +720,7 @@ public class DisneyAppleBaseTest extends AbstractTest implements IOSUtils, IAPIH
                 ARGENTINA, BOLIVIA, CHILE, COLOMBIA, COSTA_RICA, DOMINICAN_REPUBLIC, ECUADOR, EL_SALVADOR, GUATEMALA,
                 HONDURAS, MEXICO, NICARAGUA, PANAMA, PARAGUAY, PERU, URUGUAY, AU, NZ);
         LOGGER.info("Selecting random Country code");
-        return NZ;
+        return AU;
     }
 
     private String getEMEAOrCanadaCountryCode() {
