e7afa0e1917179ead0a5ad94c5762598b80655c2
RUIZV035 <Victoria.Ruiz.Martinez.-ND@disney.com>
2025-06-24 14:40:33 -0600

    change reverted

--- src/test/java/com/disney/qa/tests/disney/apple/ios/regression/details/DisneyPlusDetailsTest.java
+++ src/test/java/com/disney/qa/tests/disney/apple/ios/regression/details/DisneyPlusDetailsTest.java
@@ -999,13 +999,6 @@ public class DisneyPlusDetailsTest extends DisneyBaseTest {
         DisneyPlusHomeIOSPageBase homePage = initPage(DisneyPlusHomeIOSPageBase.class);
         DisneyPlusDetailsIOSPageBase detailsPage = initPage(DisneyPlusDetailsIOSPageBase.class);
 
-     //   getSeriesApi2("entity-4a004490-8d31-4c46-9ab2-5b68b1820a41");
-<<<<<<< Updated upstream
-        getExploreAPIPageVisuals("entity-b3563311-3e65-42d5-921c-9c9c3a43cb0f");
-=======
-     //   getExploreAPIPageVisuals("entity-b3563311-3e65-42d5-921c-9c9c3a43cb0f");
->>>>>>> Stashed changes
-
         setAppToHomeScreen(getUnifiedAccount());
         homePage.waitForHomePageToOpen();
 
