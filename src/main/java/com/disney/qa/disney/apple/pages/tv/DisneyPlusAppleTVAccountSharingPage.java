a2cc0f70c2cf734ef65cf88e15595bdadd3731df
Saurav <saurav.kumar.-nd@disney.com>
2025-07-03 01:02:32 +0530

    address review comments

--- src/main/java/com/disney/qa/disney/apple/pages/tv/DisneyPlusAppleTVAccountSharingPage.java
+++ src/main/java/com/disney/qa/disney/apple/pages/tv/DisneyPlusAppleTVAccountSharingPage.java
@@ -121,7 +121,7 @@ public class DisneyPlusAppleTVAccountSharingPage extends DisneyPlusApplePageBase
                         DictionaryKeys.OOH_HARD_BLOCK_SUBCOPY_2.getText())).isPresent();
     }
 
-    public boolean isOOHHardBlockSubcopy3Present() {
+    public boolean isOOHHardBlockSubcopyTitlePresent() {
         return getStaticTextByLabel(getLocalizationUtils()
                 .getDictionaryItem(DisneyDictionaryApi.ResourceKeys.UNIFIED_COMMERCE,
                         DictionaryKeys.OOH_HARD_BLOCK_SUBCOPY.getText())).isPresent();
