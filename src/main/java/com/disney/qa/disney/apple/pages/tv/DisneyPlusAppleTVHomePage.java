556f2aff9850c0e9dc41aabd624b7e1bfbfaa097
Saurav <saurav.kumar.-nd@disney.com>
2025-07-08 12:10:39 +0530

    added conditon

--- src/main/java/com/disney/qa/disney/apple/pages/tv/DisneyPlusAppleTVHomePage.java
+++ src/main/java/com/disney/qa/disney/apple/pages/tv/DisneyPlusAppleTVHomePage.java
@@ -209,10 +209,14 @@ public class DisneyPlusAppleTVHomePage extends DisneyPlusHomeIOSPageBase {
     }
 
     public void hiddenNavStateOnInnerPages(String globalNavMenu) {
+        DisneyPlusAppleTVSearchPage searchPage = new DisneyPlusAppleTVSearchPage(getDriver());
         pause(THREE_SEC_TIMEOUT); //if no pause, selecting menu back goes to native home outside of app
         LOGGER.info("Navigating to global nav menu: {}", globalNavMenu);
         if (globalNavMenu.equalsIgnoreCase(globalNavigationMenu.MOVIES.getText())){
             pause(FIVE_SEC_TIMEOUT);
+            if (searchPage.isGlobalNavExpanded()) {
+                clickMenu();
+            }
             clickLeft();
         } else {
             clickMenuTimes(1, 2);
