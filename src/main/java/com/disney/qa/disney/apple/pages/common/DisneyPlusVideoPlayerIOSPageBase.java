fdb0c52ed5948738cb119e1618d5ced63e000ddc
csolmaz <cristina.solmaz@hulu.com>
2025-07-10 15:25:18 -0700

    reduce timeout

--- src/main/java/com/disney/qa/disney/apple/pages/common/DisneyPlusVideoPlayerIOSPageBase.java
+++ src/main/java/com/disney/qa/disney/apple/pages/common/DisneyPlusVideoPlayerIOSPageBase.java
@@ -564,7 +564,7 @@ public class DisneyPlusVideoPlayerIOSPageBase extends DisneyPlusApplePageBase {
 
     public boolean isAdBadgeLabelPresent(int...timeout) {
         int waitTime = timeout.length > 0 ? timeout[0] : FIFTEEN_SEC_TIMEOUT;
-        return fluentWait(getDriver(), waitTime, THREE_SEC_TIMEOUT, "Ad badge label not present")
+        return fluentWait(getDriver(), waitTime, ONE_SEC_TIMEOUT, "Ad badge label not present")
                 .until(it -> isAdBadgePresent(ONE_SEC_TIMEOUT));
     }
 
